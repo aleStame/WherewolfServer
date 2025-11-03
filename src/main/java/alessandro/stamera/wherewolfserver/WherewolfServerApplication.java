6db0b0bfe0ae103850e101774d457e47be526fe7
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2025-11-03 22:25:02 +0100

    Commit iniziale

--- src/main/java/alessandro/stamera/wherewolfserver/WherewolfServerApplication.java
+++ src/main/java/alessandro/stamera/wherewolfserver/WherewolfServerApplication.java
@@ -3,9 +3,7 @@ package alessandro.stamera.wherewolfserver;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import static org.springframework.boot.SpringApplication.run;
 
-@SpringBootApplication public final class WherewolfServerApplication
+@SpringBootApplication public class WherewolfServerApplication
 {
-    public static void main(String[] args) {
-        run(WherewolfServerApplication.class, args);
-    }
+    public static void main(String[] args) { run(WherewolfServerApplication.class, args); }
 }
