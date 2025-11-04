a36a725577ffea58d2c4e6da71e4b18fed5e796f
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2025-11-04 14:46:31 +0100

    VERDE: registrazione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/controller/UtenteController.java
+++ src/main/java/alessandro/stamera/wherewolfserver/controller/UtenteController.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.controller;
 
+import alessandro.stamera.wherewolfserver.entity.Utente;
 import alessandro.stamera.wherewolfserver.repository.UtenteRepository;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +18,18 @@ import org.springframework.web.bind.annotation.*;
         String risultato;
         if(repo.findAll().stream().anyMatch(utente -> utente.login(username, password))) risultato = "Login eseguito correttamente";
         else risultato = "ERRORE!!! Username o password errate";
+        return risultato;
+    }
+
+    @PostMapping("/registrazione") public String registrazione(@RequestParam String username, @RequestParam String password)
+    {
+        String risultato;
+        if(repo.findById(username).isPresent())risultato = "ERRORE!!! Nome utente già inserito";
+        else
+        {
+            repo.save(new Utente(username, password));
+            risultato = "Registrazione avvenuta correttamente";
+        }
         return risultato;
     }
 
