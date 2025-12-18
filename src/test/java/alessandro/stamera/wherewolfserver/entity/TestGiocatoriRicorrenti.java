4f8b9589b077fda4d64bfac791ddbb0d26be7bf3
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2025-12-18 15:38:18 +0100

    ROSSO: inserimentoGiocatoreNonRiuscito non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/entity/TestGiocatoriRicorrenti.java
+++ src/test/java/alessandro/stamera/wherewolfserver/entity/TestGiocatoriRicorrenti.java
@@ -2,6 +2,7 @@ package alessandro.stamera.wherewolfserver.entity;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public final class TestGiocatoriRicorrenti
 {
@@ -12,6 +13,13 @@ public final class TestGiocatoriRicorrenti
         giocatori.aggiungi("Aldo");
         giocatori.aggiungi("Giovanni");
         assertThat(giocatori.getNumeroGiocatori()).isEqualTo(2);
+    }
+
+    @Test public void inserimentoGiocatoreNonRiuscito()
+    {
+        GiocatoriRicorrenti giocatori = new GiocatoriRicorrenti();
+        giocatori.aggiungi("Aldo");
+        assertThatIllegalArgumentException().isThrownBy(() -> giocatori.aggiungi("Aldo")).withMessage("ERRORE!!! Utente già inserito.");
     }
 
 }
