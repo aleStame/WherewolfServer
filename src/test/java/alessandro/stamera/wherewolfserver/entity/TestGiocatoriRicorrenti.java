19c385e80f1b8841101fc5a97a781e99e52d8ffa
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2025-12-18 15:51:56 +0100

    REFACTORING: definizione e utilizzo della costante ESEMPIO_GIOCATORE

--- src/test/java/alessandro/stamera/wherewolfserver/entity/TestGiocatoriRicorrenti.java
+++ src/test/java/alessandro/stamera/wherewolfserver/entity/TestGiocatoriRicorrenti.java
@@ -8,21 +8,25 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 public final class TestGiocatoriRicorrenti
 {
 
+    private static final String ESEMPIO_GIOCATORE = "Aldo";
+
     private GiocatoriRicorrenti giocatori;
 
-    @BeforeEach public void setUp() { giocatori = new GiocatoriRicorrenti(); }
+    @BeforeEach public void setUp()
+    {
+        giocatori = new GiocatoriRicorrenti();
+        aggiungiGiocatore(ESEMPIO_GIOCATORE);
+    }
 
     @Test public void inserimentoGiocatoreRiuscito()
     {
-        aggiungiGiocatore("Aldo");
         aggiungiGiocatore("Giovanni");
         assertThat(giocatori.getNumeroGiocatori()).isEqualTo(2);
     }
 
     @Test public void inserimentoGiocatoreNonRiuscito()
     {
-        aggiungiGiocatore("Aldo");
-        assertThatIllegalArgumentException().isThrownBy(() -> aggiungiGiocatore("Aldo")).withMessage("ERRORE!!! Utente già inserito");
+        assertThatIllegalArgumentException().isThrownBy(() -> aggiungiGiocatore(ESEMPIO_GIOCATORE)).withMessage("ERRORE!!! Utente già inserito");
     }
 
     private void aggiungiGiocatore(String nomeGiocatore) { giocatori.aggiungi(nomeGiocatore); }
