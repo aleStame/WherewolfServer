2dcc446af8a4a0e0215d0888ad96c55b75cbae32
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-12 22:09:44 +0100

    ROSSO: testRicerca non riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/TipoContadino.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/TipoContadino.java
@@ -3,12 +3,15 @@ package alessandro.stamera.wherewolfserver.classi;
 public enum TipoContadino
 {
 
-    NORMALE("Contadino normale"), EROE("Contadino eroe"), MOSTRO("Contadino mostro"), LUPO("Contadino discendente dei lupi");
+    NORMALE("Contadino normale"), EROE("Contadino eroe"), MOSTRO("Contadino mostro"),
+    LUPO("Contadino discendente dei lupi");
 
     private final String descrizione;
 
     TipoContadino(String descrizione) { this.descrizione = descrizione; }
 
     @Override public String toString() { return descrizione; }
+
+    public static TipoContadino getTipoContadino(String descrizione) { return null; }
 
 }
--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestTipoContadino.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestTipoContadino.java
@@ -2,6 +2,7 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import static alessandro.stamera.wherewolfserver.classi.TipoContadino.getTipoContadino;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestTipoContadino
@@ -12,5 +13,11 @@ public final class TestTipoContadino
         { "NORMALE, Contadino normale", "EROE, Contadino eroe", "MOSTRO, Contadino mostro", "LUPO, Contadino discendente dei lupi" }
     )
     public void testStringa(TipoContadino tipo, String descrizione) { assertThat(tipo.toString()).isEqualTo(descrizione); }
+
+    @ParameterizedTest @CsvSource
+    (
+        { "Contadino normale, NORMALE", "Contadino eroe, EROE", "Contadino mostro, MOSTRO", "Contadino discendente dei lupi, LUPO" }
+    )
+    public void testRicerca(String descrizione, TipoContadino tipo) { assertThat(getTipoContadino(descrizione)).isEqualTo(tipo); }
 
 }
