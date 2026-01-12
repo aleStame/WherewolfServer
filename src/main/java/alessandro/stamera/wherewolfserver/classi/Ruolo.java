96ea2ca734841a70e85b10c6007409e52452d319
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-12 11:58:08 +0100

    ROSSO: testCategoria non riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Ruolo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Ruolo.java
@@ -1,3 +1,3 @@
 package alessandro.stamera.wherewolfserver.classi;
 
-public record Ruolo(String nome, Fazione fazione, Aura aura, int lune) { }
+public record Ruolo(String nome, Fazione fazione, Aura aura, int lune) { public Categoria getCategoria() { return null; } }
--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestRuolo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestRuolo.java
@@ -2,7 +2,7 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+import static alessandro.stamera.wherewolfserver.classi.Aura.*;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.NESSUNA;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,16 +10,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class TestRuolo
 {
 
-    @ParameterizedTest @CsvSource({ "1, 2, 3" }) public void testLune(int lune)
-    {
-        Ruolo ruolo = getRuolo("Contadino", VILLAGGIO, BIANCA, lune);
-        assertThat(ruolo.lune()).isEqualTo(lune);
-    }
+    @ParameterizedTest @CsvSource({ "1, 2, 3" })
+    public void testLune(int lune) { assertThat(getRuolo("Contadino", VILLAGGIO, BIANCA, lune).lune()).isEqualTo(lune); }
 
-    @ParameterizedTest @CsvSource({ "BIANCA, NERA" }) public void testAura(Aura aura)
+    @ParameterizedTest @CsvSource({ "BIANCA, NERA" })
+    public void testAura(Aura aura) { assertThat(getRuolo("Ruolo", NESSUNA, aura, 1).aura()).isEqualTo(aura); }
+
+    @ParameterizedTest @CsvSource
+    (
+        {
+            "NESSUNA, NESSUNA", "LUPO_BRANCO, CREATURE_OMBRA", "LUPO_SOLITARIO, CREATURE_OMBRA", "VAMPIRO, CREATURE_OMBRA",
+            "NOSFERATU, CREATURE_OMBRA", "NEGROMANTE, CREATURE_OMBRA", "POSSEDUTO, CREATURE_OMBRA", "VILLAGGIO, UOMINI", "CITTA, UOMINI",
+            "CRIMINALI, UOMINI", "AMANTI, UOMINI", "INQUISIZIONE, UOMINI"
+        }
+    )
+    public void testCategoria(Fazione fazione, Categoria categoria)
     {
-        Ruolo ruolo = getRuolo("Ruolo", NESSUNA, aura, 1);
-        assertThat(ruolo.aura()).isEqualTo(aura);
+        assertThat(getRuolo("Alessandro", fazione, NERA, 2).getCategoria()).isEqualTo(categoria);
     }
 
     private Ruolo getRuolo(String nome, Fazione fazione, Aura aura, int lune) { return new Ruolo(nome, fazione, aura, lune); }
