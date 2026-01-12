bfe72d7fe86ebc465d848743b65cea3e1fa188e7
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-12 18:54:17 +0100

    REFACTORING: definizione del metodo getNumeroFazioni

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Fazione.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Fazione.java
@@ -31,9 +31,11 @@ public enum Fazione
     public static Fazione getFazione(String descrizione)
     {
         int posizione = -1;
-        for(int i = 0; i < values().length && posizione == -1; i++) if(descrizione.equals(getFazione(i).toString())) posizione = i;
+        for(int i = 0; i < getNumeroFazioni() && posizione == -1; i++) if(descrizione.equals(getFazione(i).toString())) posizione = i;
         return getFazione(posizione);
     }
+
+    private static int getNumeroFazioni() { return values().length; }
 
     private static Fazione getFazione(int posizione) { return values()[posizione]; }
 
