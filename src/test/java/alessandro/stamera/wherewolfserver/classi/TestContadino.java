4838813b0b78b2646177efe75f63ecb2b9a9f94f
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-12 22:43:13 +0100

    ROSSO: testFazione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadino.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadino.java
@@ -2,11 +2,14 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestContadino
 {
 
     @Test public void testNome() { assertThat(new Contadino(BIANCA).getNome()).isEqualTo("Contadino"); }
+
+    @Test public void testFazione() { assertThat(new Contadino(BIANCA).getFazione()).isEqualTo(VILLAGGIO); }
 
 }
