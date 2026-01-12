b9d6742f71b374c6d47929be63c88557e4a4a30a
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-12 17:15:36 +0100

    VERDE: testStringa riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Categoria.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Categoria.java
@@ -1,3 +1,14 @@
 package alessandro.stamera.wherewolfserver.classi;
 
-public enum Categoria { NESSUNA, CREATURE_OMBRA, UOMINI; }
+public enum Categoria
+{
+
+    NESSUNA("-"), CREATURE_OMBRA("Creature dell'ombra"), UOMINI("Uomini");
+
+    private final String descrizione;
+
+    Categoria(String descrizione) { this.descrizione = descrizione; }
+
+    @Override public String toString() { return descrizione; }
+
+}
