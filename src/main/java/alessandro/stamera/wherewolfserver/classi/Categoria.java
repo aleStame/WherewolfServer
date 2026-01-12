f55dcf885a164ecf8f1bee6410f6b81b3e72ddb7
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-12 17:49:53 +0100

    REFACTORING: definizione di una variante del metodo getCategoria che accetta come parametro la posizione della categoria richiesta

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Categoria.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Categoria.java
@@ -11,13 +11,15 @@ public enum Categoria
 
     @Override public String toString() { return descrizione; }
 
-    public static Categoria getCategoria(String descrizione) { return values()[getPosizioneCategoria(descrizione)]; }
+    public static Categoria getCategoria(String descrizione) { return getCategoria(getPosizioneCategoria(descrizione)); }
 
     private static int getPosizioneCategoria(String descrizione)
     {
         int posizione = -1;
-        for(int i = 0; i < values().length && posizione == -1; i++) if(descrizione.equals(values()[i].toString())) posizione = i;
+        for(int i = 0; i < values().length && posizione == -1; i++) if(descrizione.equals(getCategoria(i).toString())) posizione = i;
         return posizione;
     }
+
+    private static Categoria getCategoria(int posizione) { return values()[posizione]; }
 
 }
