8280908cc6882765b1a22ea8f45245d39c19d984
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-14 18:59:20 +0100

    REFACTORING: definizione della costante NON_TROVATO

--- src/main/java/alessandro/stamera/wherewolfserver/classi/TipoContadino.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/TipoContadino.java
@@ -6,18 +6,23 @@ public enum TipoContadino
     NORMALE("Contadino normale"), EROE("Contadino eroe"), MOSTRO("Contadino mostro"),
     LUPO("Contadino discendente dei lupi");
 
+    private static final int NON_TROVATO = -1;
+
     private final String descrizione;
 
     TipoContadino(String descrizione) { this.descrizione = descrizione; }
 
     @Override public String toString() { return descrizione; }
 
-    public static TipoContadino getTipoContadino(String descrizione) { return getTipoContadino(getPosizioneContadino(descrizione)); }
+    public static TipoContadino getTipoContadino(String descrizione)
+    {
+        return getTipoContadino(getPosizioneContadino(descrizione));
+    }
 
     private static int getPosizioneContadino(String descrizione)
     {
-        int posizione = -1;
-        for(int i = 0; i < values().length && posizione == -1; i++) if(descrizione.equals(getDescrizioneContadino(i))) posizione = i;
+        int posizione = NON_TROVATO;
+        for(int i = 0; i < values().length && posizione == NON_TROVATO; i++) if(descrizione.equals(getDescrizioneContadino(i))) posizione = i;
         return posizione;
     }
 
