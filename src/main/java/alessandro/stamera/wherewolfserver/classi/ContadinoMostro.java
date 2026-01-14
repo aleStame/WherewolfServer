fb7bc9957e5dae3bc34ea252e4e357acc3e926fd
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-14 11:38:24 +0100

    ROSSO: testAura non riuscito

---
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/ContadinoMostro.java
@@ -0,0 +1,8 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+public final class ContadinoMostro extends Contadino
+{
+
+    public ContadinoMostro() { super(null); }
+
+}
---
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadinoMostro.java
@@ -0,0 +1,12 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+import org.junit.jupiter.api.Test;
+import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class TestContadinoMostro
+{
+
+    @Test public void testAura() { assertThat(new ContadinoMostro().getAura()).isEqualTo(NERA); }
+
+}
