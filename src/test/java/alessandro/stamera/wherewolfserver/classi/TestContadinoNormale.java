8528ce5d176404b114ebd0474c4de2ca0f719065
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-14 18:26:10 +0100

    REFACTORING: definizione del metodo verificaVero

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadinoNormale.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadinoNormale.java
@@ -2,7 +2,6 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,8 +14,12 @@ public final class TestContadinoNormale
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
-    @Test public void testContadino() { assertThat(ruolo.isContadino()).isTrue(); }
+    @Test public void testContadino() { verificaVero(ruolo.isContadino()); }
 
-    @Test public void testContadinoNormale() { assertThat(ruolo.isContadinoNormale()).isTrue(); }
+    @Test public void testContadinoNormale() { verificaVero(ruolo.isContadinoNormale()); }
+
+    @Test public void testContadinoMostro() { assertThat(ruolo.isContadinoMostro()).isFalse(); }
+
+    private void verificaVero(boolean valore) { assertThat(valore).isTrue(); }
 
 }
