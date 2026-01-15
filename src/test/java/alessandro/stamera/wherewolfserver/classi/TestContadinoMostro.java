ada07026d5455f9c50339a954f81e191bff58d62
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-15 16:43:36 +0100

    VERDE DIRETTO: testFazione riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadinoMostro.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadinoMostro.java
@@ -3,6 +3,7 @@ package alessandro.stamera.wherewolfserver.classi;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
+import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestContadinoMostro
@@ -13,6 +14,8 @@ public final class TestContadinoMostro
     @BeforeEach public void setUp() { ruolo = new ContadinoMostro(); }
 
     @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Contadino"); }
+
+    @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(VILLAGGIO); }
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(NERA); }
 
