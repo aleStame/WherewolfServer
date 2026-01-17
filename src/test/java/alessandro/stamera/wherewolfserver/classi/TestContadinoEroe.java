7fe827bd04d8bf0eb8cd6b9e1dc38dc4d729a6af
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-17 10:51:54 +0100

    VERDE DIRETTO: testContadinoLupo riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadinoEroe.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadinoEroe.java
@@ -27,6 +27,8 @@ public final class TestContadinoEroe
 
     @Test public void testContadinoEroe() { verificaVero(ruolo.isContadinoEroe()); }
 
+    @Test public void testContadinoLupo() { verificaFalso(ruolo.isContadinoLupo()); }
+
     private void verificaVero(boolean valore) { assertThat(valore).isTrue(); }
 
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
