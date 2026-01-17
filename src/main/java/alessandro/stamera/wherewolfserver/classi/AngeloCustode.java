6f99bcb3e371842f675c4410f9f071137cad56b5
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-17 15:57:16 +0100

    VERDE: testNome riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/AngeloCustode.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/AngeloCustode.java
@@ -1,3 +1,8 @@
 package alessandro.stamera.wherewolfserver.classi;
 
-public final class AngeloCustode { public String getNome() { return null; } }
+public final class AngeloCustode extends Ruolo
+{
+
+    public AngeloCustode() { super("Angelo custode", null, null, null, -1); }
+
+}
