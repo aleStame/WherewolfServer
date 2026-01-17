0d5ec2c0f259c5d4bb49627651488a61c74fe9fe
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-17 16:26:32 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/AngeloCustode.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/AngeloCustode.java
@@ -1,10 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.AMANTI;
 
 public final class AngeloCustode extends Ruolo
 {
 
-    public AngeloCustode() { super("Angelo custode", AMANTI, null, null, -1); }
+    public AngeloCustode() { super("Angelo custode", AMANTI, BIANCA, null, -1); }
 
 }
