cbaea6d355e429948f69319435d38d1c13eb205c
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-19 16:03:14 +0100

    VERDE: assassino riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Assassino.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Assassino.java
@@ -3,6 +3,6 @@ package alessandro.stamera.wherewolfserver.classi;
 public final class Assassino extends Ruolo
 {
 
-    public Assassino() { super(null, null, null, null, -1); }
+    public Assassino() { super("Assassino", null, null, null, -1); }
 
 }
