caf6b632b9c02aa4a68d67d0269418e34c158638
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-19 15:21:00 +0100

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/AngeloCustode.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/AngeloCustode.java
@@ -6,6 +6,16 @@ import static alessandro.stamera.wherewolfserver.classi.Fazione.AMANTI;
 public final class AngeloCustode extends Ruolo
 {
 
-    public AngeloCustode() { super("Angelo custode", AMANTI, BIANCA, null, -1); }
+    public AngeloCustode()
+    {
+        super
+        (
+      "Angelo custode", AMANTI, BIANCA,
+  "La prima notte indica un giocatore, l'Amato, che viene avvisato. Se quel giocatore dovesse essere accusato, l'Angelo custode sarà " +
+            "accusato al suo posto. Se dovesse essere attaccato e ucciso durante la notte, sarà invece attaccato, avvisato e ucciso l'Angelo " +
+            "custode.",
+        -1
+        );
+    }
 
 }
