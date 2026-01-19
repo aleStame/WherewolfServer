7e9707cedc27aa0f57da6cf7fab77c80e570137b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-19 18:30:29 +0100

    VERDE: testFazione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Assassino.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Assassino.java
@@ -1,10 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+import static alessandro.stamera.wherewolfserver.classi.Fazione.CRIMINALI;
 
 public final class Assassino extends Ruolo
 {
 
-    public Assassino() { super("Assassino", null, BIANCA, null, -1); }
+    public Assassino() { super("Assassino", CRIMINALI, BIANCA, null, -1); }
 
 }
