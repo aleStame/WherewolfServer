bdf7664b68ee81f729a72f36b47292d55a4c93c3
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-20 18:28:23 +0100

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Becchino.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Becchino.java
@@ -6,6 +6,16 @@ import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 public class Becchino extends Ruolo
 {
 
-    public Becchino() { super("Becchino", VILLAGGIO, BIANCA, null, 3); }
+    public Becchino()
+    {
+        super
+        (
+    "Becchino", VILLAGGIO, BIANCA,
+"La prima notte scopre se il Negromante è in gioco. Durante il turno del Negromante sceglie se riconoscerlo. Se lo fa, la sua fazione " +
+          "diventa Negromante. Altrimenti, ogni mattino, se sono stati eliminati giocatori maledetti dal mattino precedente, il Moderatore lo " +
+          "annuncia pubblicamente",
+     3
+        );
+    }
 
 }
