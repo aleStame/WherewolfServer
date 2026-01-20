38c5b2e4b8cccad1c4828516069ff2bcd23d01b5
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-20 16:04:09 +0100

    ROSSO: testFazione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBecchino.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBecchino.java
@@ -1,11 +1,15 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+
+import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestBecchino
 {
 
     @Test public void testNome() { assertThat(new Becchino().getNome()).isEqualTo("Becchino"); }
+
+    @Test public void testFazione() { assertThat(new Becchino().getFazione()).isEqualTo(VILLAGGIO); }
 
 }
