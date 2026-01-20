2517f512e45858880ec793347e9b4070c444f589
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-20 16:19:18 +0100

    REFACTORING: definizione di un metodo di set up

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBecchino.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBecchino.java
@@ -1,15 +1,19 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestBecchino
 {
 
-    @Test public void testNome() { assertThat(new Becchino().getNome()).isEqualTo("Becchino"); }
+    private Ruolo ruolo;
 
-    @Test public void testFazione() { assertThat(new Becchino().getFazione()).isEqualTo(VILLAGGIO); }
+    @BeforeEach public void setUp() { ruolo = new Becchino(); }
+
+    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Becchino"); }
+
+    @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(VILLAGGIO); }
 
 }
