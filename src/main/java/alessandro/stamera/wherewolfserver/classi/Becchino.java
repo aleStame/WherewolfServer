96d54f611858928de1549faebf6753a292aa80cf
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-20 15:59:07 +0100

    VERDE: testNome riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Becchino.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Becchino.java
@@ -3,6 +3,6 @@ package alessandro.stamera.wherewolfserver.classi;
 public class Becchino extends Ruolo
 {
 
-    public Becchino() { super(null, null, null, null, -1); }
+    public Becchino() { super("Becchino", null, null, null, -1); }
 
 }
