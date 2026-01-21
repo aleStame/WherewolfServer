941d48287d2bb8d4ec181a4836a27c8ac7bb2cc9
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-21 12:24:52 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/BoccaDiRosa.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/BoccaDiRosa.java
@@ -1,10 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.CITTA;
 
 public final class BoccaDiRosa extends Ruolo
 {
 
-    public BoccaDiRosa() { super("Bocca di rosa", CITTA, null, null, -1); }
+    public BoccaDiRosa() { super("Bocca di rosa", CITTA, NERA, null, -1); }
 
 }
