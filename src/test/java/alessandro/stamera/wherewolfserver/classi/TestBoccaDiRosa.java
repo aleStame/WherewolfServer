a747f3fa57387281adb4714c2e09607aaab033ba
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-21 12:23:31 +0100

    ROSSO: testAura non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoccaDiRosa.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoccaDiRosa.java
@@ -2,7 +2,7 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.CITTA;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,5 +16,7 @@ public final class TestBoccaDiRosa
     @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Bocca di rosa"); }
 
     @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(CITTA); }
+
+    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(NERA); }
 
 }
