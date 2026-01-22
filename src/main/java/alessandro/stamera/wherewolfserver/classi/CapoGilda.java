ca6ea6606042278744300a2dfd85ca03409ebb1c
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-22 17:59:34 +0100

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/CapoGilda.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/CapoGilda.java
@@ -6,6 +6,16 @@ import static alessandro.stamera.wherewolfserver.classi.Fazione.CRIMINALI;
 public final class CapoGilda extends Ruolo
 {
 
-    public CapoGilda() { super("Capo gilda", CRIMINALI, BIANCA, null, 2); }
+    public CapoGilda()
+    {
+        super
+        (
+    "Capo gilda", CRIMINALI, BIANCA,
+"La prima notte riconosce gli altri criminali. Una volta per partita, dalla seconda notte, può aprire gli occhi nel turno di un " +
+          "mistico. Se quel mistico non è in gioco, indica un giocatore. Se è una Guardia o un lupo mannaro, il Capo gilda viene ucciso. Se la " +
+          "sua fazione è Città o Villaggio, riconosce il Capo gilda, altrimenti non accade nulla",
+    2
+        );
+    }
 
 }
