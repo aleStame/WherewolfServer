bf8b803e5cff281f0cd368ade3851d8164daf51b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-22 17:27:16 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/CapoGilda.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/CapoGilda.java
@@ -1,10 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.CRIMINALI;
 
 public final class CapoGilda extends Ruolo
 {
 
-    public CapoGilda() { super("Capo gilda", CRIMINALI, null, null, -1); }
+    public CapoGilda() { super("Capo gilda", CRIMINALI, BIANCA, null, -1); }
 
 }
