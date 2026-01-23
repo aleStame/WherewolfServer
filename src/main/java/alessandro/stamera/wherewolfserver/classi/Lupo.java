7ff6d655d2dee15c0c50e04f53195e6e5c7fec00
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-23 19:18:24 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Lupo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Lupo.java
@@ -1,8 +1,10 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
+
 public class Lupo extends Ruolo
 {
 
-    public Lupo(String nome, String descrizione, int lune) { super(nome, null, null, descrizione, lune); }
+    public Lupo(String nome, String descrizione, int lune) { super(nome, null, NERA, descrizione, lune); }
 
 }
