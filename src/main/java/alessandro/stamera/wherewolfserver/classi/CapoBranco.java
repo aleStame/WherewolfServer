4cd2839dd3b421bae3b8cf4a06db410b41159aac
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-24 17:26:54 +0100

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/CapoBranco.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/CapoBranco.java
@@ -3,7 +3,16 @@ package alessandro.stamera.wherewolfserver.classi;
 public final class CapoBranco extends Lupo
 {
 
-    public CapoBranco() { super("Capo branco", null, 1); }
+    public CapoBranco()
+    {
+        super
+        (
+    "Capo branco",
+"La prima notte individua il Traditore e riconosce i lupi del branco. Dalla seconda notte può indicare un giocatore, e questi " +
+          "viene ucciso.",
+     1
+        );
+    }
 
     @Override public boolean isCapoBranco() { return true; }
 
