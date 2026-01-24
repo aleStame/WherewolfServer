91cd1a634190822a239d4f0a098e8d99c39197c2
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-24 11:48:52 +0100

    ROSSO: testFazione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestLupo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestLupo.java
@@ -3,10 +3,16 @@ package alessandro.stamera.wherewolfserver.classi;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
+import static alessandro.stamera.wherewolfserver.classi.Fazione.LUPO_BRANCO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestLupo
 {
+
+    @ParameterizedTest @CsvSource({ "1, 2, 3" }) public void testFazione(int lune)
+    {
+        assertThat(new Lupo("Ruolo", "Descrizione generica", lune).getFazione()).isEqualTo(LUPO_BRANCO);
+    }
 
     @ParameterizedTest @CsvSource({ "1, 2, 3" })
     public void testAura(int lune) { assertThat(new Lupo("Ruolo", "Descrizione generica", lune).getAura()).isEqualTo(NERA); }
