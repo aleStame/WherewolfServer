620b75c31961efae90411ede2dfc1a84d0a1a2f8
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-24 18:09:30 +0100

    REFACTORING: definizione del metodo verificaStringa

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestCapoBranco.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestCapoBranco.java
@@ -11,18 +11,20 @@ public final class TestCapoBranco
 
     @BeforeEach public void setUp() { ruolo = new CapoBranco(); }
 
-    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Capo branco"); }
+    @Test public void testNome() { verificaStringa(ruolo.getNome(), "Capo branco"); }
 
     @Test public void testDescrizione()
     {
         String descrizione =
             "La prima notte individua il Traditore e riconosce i lupi del branco. Dalla seconda notte può indicare un giocatore, e questi " +
             "viene ucciso.";
-        assertThat(ruolo.getDescrizione()).isEqualTo(descrizione);
+        verificaStringa(ruolo.getDescrizione(), descrizione);
     }
 
     @Test public void testLune() { assertThat(ruolo.getLune()).isEqualTo(1); }
 
     @Test public void testCapoBranco() { assertThat(ruolo.isCapoBranco()).isTrue(); }
+
+    private void verificaStringa(String valore, String descrizione) { assertThat(valore).isEqualTo(descrizione); }
 
 }
