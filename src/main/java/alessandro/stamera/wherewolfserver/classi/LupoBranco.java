688778e98b00b820f4ffca1140422299bc6b6a2e
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-26 12:14:37 +0100

    VERDE: testLupoBranco riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/LupoBranco.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/LupoBranco.java
@@ -5,4 +5,6 @@ public final class LupoBranco extends Lupo
 
     public LupoBranco() { super("Lupo del branco", null, 1); }
 
+    @Override public boolean isLupoBranco() { return !super.isLupoBranco(); }
+
 }
