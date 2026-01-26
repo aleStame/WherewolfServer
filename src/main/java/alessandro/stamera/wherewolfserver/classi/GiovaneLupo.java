2dd6717a401f718289daace8512e2c2763fe9283
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-26 18:12:56 +0100

    VERDE: testGiovaneLupo riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/GiovaneLupo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/GiovaneLupo.java
@@ -5,4 +5,6 @@ public class GiovaneLupo extends Lupo
 
     public GiovaneLupo() { super("Giovane lupo", null, 0); }
 
+    @Override public boolean isGiovaneLupo() { return !super.isGiovaneLupo(); }
+
 }
