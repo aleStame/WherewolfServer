d4d76bbd21bfa7011a3ce5fc794c98a5e057f0ca
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-27 11:42:37 +0100

    ROSSO: testNome non riuscito

---
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/LupoReietto.java
@@ -0,0 +1,8 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+public final class LupoReietto extends Lupo
+{
+
+    public LupoReietto() { super(null, null, 0); }
+
+}
---
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestLupoReietto.java
@@ -0,0 +1,11 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class TestLupoReietto
+{
+
+    @Test public void testNome() { assertThat(new LupoReietto().getNome()).isEqualTo("Lupo reietto"); }
+
+}
