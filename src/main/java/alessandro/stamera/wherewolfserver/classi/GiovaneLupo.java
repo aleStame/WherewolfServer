79ef09df5f451d285490e3bbe616c7d4917fefb6
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-01-27 11:34:31 +0100

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/GiovaneLupo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/GiovaneLupo.java
@@ -3,7 +3,15 @@ package alessandro.stamera.wherewolfserver.classi;
 public class GiovaneLupo extends Lupo
 {
 
-    public GiovaneLupo() { super("Giovane lupo", null, 1); }
+    public GiovaneLupo()
+    {
+        super
+        (
+    "Giovane lupo",
+"La prima notte individua il Traditore e riconosce i lupi del branco e nelle altre apre gli occhi durante il turno dei lupi mannari. " +
+          "Se viene messo al rogo, la notte successiva i lupi del branco attaccheranno due volte",
+     1);
+    }
 
     @Override public boolean isGiovaneLupo() { return !super.isGiovaneLupo(); }
 
