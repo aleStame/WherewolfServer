554dd416e529945e42d8998f5912aaa39edab72b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-02 16:15:16 +0100

    ROSSO: testNome non riuscito

---
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Azzeccagarbugli.java
@@ -0,0 +1,8 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+public final class Azzeccagarbugli extends Ruolo
+{
+
+    public Azzeccagarbugli() { super(null, null, null, null, -1, true); }
+
+}
