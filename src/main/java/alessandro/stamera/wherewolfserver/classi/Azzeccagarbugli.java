858bc5b2ceb0b9a39d9141dfe4f888d10036d0b5
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-02 16:40:52 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Azzeccagarbugli.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Azzeccagarbugli.java
@@ -1,10 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.CITTA;
 
 public final class Azzeccagarbugli extends Ruolo
 {
 
-    public Azzeccagarbugli() { super("Azzeccagarbugli", CITTA, null, null, -1, true); }
+    public Azzeccagarbugli() { super("Azzeccagarbugli", CITTA, BIANCA, null, -1, true); }
 
 }
