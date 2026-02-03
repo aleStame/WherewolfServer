2bad50b519dca14778471875cb5d25526ad366fd
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-03 17:00:54 +0100

    VERDE: testFazione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Bardo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Bardo.java
@@ -1,10 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 
 public final class Bardo extends Ruolo
 {
 
-    public Bardo() { super("Bardo", null, BIANCA, null, 0, true); }
+    public Bardo() { super("Bardo", VILLAGGIO, BIANCA, null, 0, true); }
 
 }
