9cfe861d891971add2e96d7f51b1a5cb5178d94f
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-03 16:39:15 +0100

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Azzeccagarbugli.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Azzeccagarbugli.java
@@ -6,7 +6,16 @@ import static alessandro.stamera.wherewolfserver.classi.Fazione.CITTA;
 public final class Azzeccagarbugli extends Ruolo
 {
 
-    public Azzeccagarbugli() { super("Azzeccagarbugli", CITTA, BIANCA, null, 2, false); }
+    public Azzeccagarbugli()
+    {
+        super
+        (
+    "Azzeccagarbugli", CITTA, BIANCA,
+"Può votare al ballottaggio anche se è accusato e può segnalare un altro giocatore durante le accuse: se la sua fazione è Città o " +
+          "Criminali, i voti che riceve vengono azzerati, altrimenti sarà accusato a prescindere dai voti ricevuti.",
+     2, false
+        );
+    }
 
     @Override public boolean isAzzeccagarbugli() { return !super.isAzzeccagarbugli(); }
 
