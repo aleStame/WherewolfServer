ab9401afa6e32e9809c053d5492a992576fa78b7
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-03 16:52:31 +0100

    ROSSO: testBianca non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBardo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBardo.java
@@ -1,11 +1,15 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestBardo
 {
 
     @Test public void testNome() { assertThat(new Bardo().getNome()).isEqualTo("Bardo"); }
+
+    @Test public void testBianca() { assertThat(new Bardo().getAura()).isEqualTo(BIANCA); }
 
 }
