49706cc34c89acb8d0332abe4f420ea0fd36e704
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-03 16:57:54 +0100

    ROSSO: testFazione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBardo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBardo.java
@@ -2,8 +2,8 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestBardo
@@ -14,6 +14,8 @@ public final class TestBardo
     @BeforeEach public void setUp() { ruolo = new Bardo(); }
 
     @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Bardo"); }
+
+    @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(VILLAGGIO); }
 
     @Test public void testBianca() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
