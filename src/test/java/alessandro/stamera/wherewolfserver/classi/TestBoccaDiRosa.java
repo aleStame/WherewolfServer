03ec571a1a42ffad57b13af114bc9d415bdb668b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-04 18:15:52 +0100

    VERDE DIRETTO: testBardo riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoccaDiRosa.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoccaDiRosa.java
@@ -53,6 +53,8 @@ public final class TestBoccaDiRosa
 
     @Test public void testContadino() { verificaFalso(ruolo.isContadino()); }
 
+    @Test public void testBardo() { verificaFalso(ruolo.isBardo()); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
