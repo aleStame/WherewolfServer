2e7e5a860eccdf98fb4a79b44124c8270d2910eb
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-05 10:52:01 +0100

    VERDE DIRETTO: testBecchino riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestCapoGilda.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestCapoGilda.java
@@ -50,6 +50,8 @@ public final class TestCapoGilda
 
     @Test public void testBardo() { verificaFalso(ruolo.isBardo()); }
 
+    @Test public void testBecchino() { verificaFalso(ruolo.isBecchino()); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
