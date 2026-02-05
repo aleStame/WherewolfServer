dfb2cf3ef1f1f6c81c2341a106ae1daefbfadab5
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-05 15:45:10 +0100

    VERDE DIRETTO: testCategoria riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestAzzeccagarbugli.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestAzzeccagarbugli.java
@@ -2,8 +2,8 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+import static alessandro.stamera.wherewolfserver.classi.Categoria.UOMINI;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.CITTA;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,6 +45,8 @@ public final class TestAzzeccagarbugli
     @Test public void testBardo() { verificaFalso(ruolo.isBardo()); }
 
     @Test public void testBecchino() { verificaFalso(ruolo.isBecchino()); }
+
+    @Test public void testCategoria() { assertThat(ruolo.getCategoria()).isEqualTo(UOMINI); }
 
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
