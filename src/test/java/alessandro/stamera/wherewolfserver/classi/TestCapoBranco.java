6573727bda347b74f11058ad675b656f3580e384
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-06 18:23:26 +0100

    VERDE DIRETTO: testFazione riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestCapoBranco.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestCapoBranco.java
@@ -3,6 +3,7 @@ package alessandro.stamera.wherewolfserver.classi;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
+import static alessandro.stamera.wherewolfserver.classi.Fazione.LUPO_BRANCO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestCapoBranco
@@ -25,6 +26,8 @@ public final class TestCapoBranco
     @Test public void testLune() { assertThat(ruolo.getLune()).isEqualTo(1); }
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(NERA); }
+
+    @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(LUPO_BRANCO); }
 
     @Test public void testCapoBranco() { verificaVero(ruolo.isCapoBranco()); }
 
