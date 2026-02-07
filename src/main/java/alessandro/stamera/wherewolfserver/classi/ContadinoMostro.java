03a15e13872ee580cd3365ddec2ed8392087e8bf
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-07 15:59:59 +0100

    REFACTORING: sistemazione gerarchia contadini

--- src/main/java/alessandro/stamera/wherewolfserver/classi/ContadinoMostro.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/ContadinoMostro.java
@@ -7,7 +7,7 @@ public final class ContadinoMostro extends Contadino
 
     public ContadinoMostro() { super(NERA); }
 
-    @Override public boolean isContadinoMostro() { return !super.isContadinoMostro(); }
+    @Override public boolean isContadinoMostro() { return true; }
 
     @Override public boolean gildata() { return !super.gildata(); }
 
