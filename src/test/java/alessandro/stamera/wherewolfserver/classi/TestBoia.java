486b5e7ff60be9ecb4f549e27c5e43c24b0564d0
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-07 17:40:41 +0100

    ROSSO: testLune non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoia.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoia.java
@@ -19,4 +19,6 @@ public final class TestBoia
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
+    @Test public void testLune() { assertThat(ruolo.getLune()).isEqualTo(3); }
+
 }
