51503b3ebe3baedce98bba38b4de5385157a8b45
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-07 17:28:43 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Boia.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Boia.java
@@ -1,10 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.INQUISIZIONE;
 
 public final class Boia extends Ruolo
 {
 
-    public Boia() { super("Boia", INQUISIZIONE, null, null, 0, true); }
+    public Boia() { super("Boia", INQUISIZIONE, BIANCA, null, 0, true); }
 
 }
