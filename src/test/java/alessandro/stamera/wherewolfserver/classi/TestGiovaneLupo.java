462b416c4ac7de1fc1ab372786ef386459c6a862
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-07 11:19:20 +0100

    VERDE DIRETTO: testCategoria riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGiovaneLupo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGiovaneLupo.java
@@ -3,6 +3,7 @@ package alessandro.stamera.wherewolfserver.classi;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
+import static alessandro.stamera.wherewolfserver.classi.Categoria.CREATURE_OMBRA;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestGiovaneLupo
@@ -33,6 +34,8 @@ public final class TestGiovaneLupo
     @Test public void testLupoReietto() { verificaFalso(ruolo.isLupoReietto()); }
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(NERA); }
+
+    @Test public void testCategoria() { assertThat(ruolo.getCategoria()).isEqualTo(CREATURE_OMBRA); }
 
     private void verificaStringa(String valore, String soluzione) { assertThat(valore).isEqualTo(soluzione); }
 
