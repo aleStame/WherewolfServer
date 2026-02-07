b4ff0070c5dd9824f5ed6fcf777cc965ffa9f5aa
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-07 17:04:52 +0100

    ROSSO: testFazione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoia.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoia.java
@@ -1,11 +1,15 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+
+import static alessandro.stamera.wherewolfserver.classi.Fazione.INQUISIZIONE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestBoia
 {
 
     @Test public void testNome() { assertThat(new Boia().getNome()).isEqualTo("Boia"); }
+
+    @Test public void testFazione() { assertThat(new Boia().getFazione()).isEqualTo(INQUISIZIONE); }
 
 }
