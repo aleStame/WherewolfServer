3814710c4ab67800e4f4475acdae241a43bfe560
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-09 10:52:14 +0100

    VERDE DIRETTO: testBecchino riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoia.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoia.java
@@ -41,6 +41,8 @@ public final class TestBoia
 
     @Test public void testBardo() { verificaFalso(ruolo.isBardo()); }
 
+    @Test public void testBecchino() { verificaFalso(ruolo.isBecchino()); }
+
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
 
 }
