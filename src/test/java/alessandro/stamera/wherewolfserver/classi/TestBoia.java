e85116d59ff36d484a45c8ecc4d68a0a3d7eeb66
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-09 10:54:32 +0100

    VERDE DIRETTO: testCategoria riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoia.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoia.java
@@ -3,6 +3,7 @@ package alessandro.stamera.wherewolfserver.classi;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+import static alessandro.stamera.wherewolfserver.classi.Categoria.UOMINI;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.INQUISIZIONE;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,6 +43,8 @@ public final class TestBoia
     @Test public void testBardo() { verificaFalso(ruolo.isBardo()); }
 
     @Test public void testBecchino() { verificaFalso(ruolo.isBecchino()); }
+
+    @Test public void testCategoria() { assertThat(ruolo.getCategoria()).isEqualTo(UOMINI); }
 
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
 
