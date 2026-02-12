e6c4d3d01fded9a084e5817d5d246b59ff78c241
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-12 11:07:03 +0100

    VERDE DIRETTO: testCategoria riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestCitta.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestCitta.java
@@ -1,6 +1,8 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+
+import static alessandro.stamera.wherewolfserver.classi.Categoria.UOMINI;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.CITTA;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,5 +10,7 @@ public final class TestCitta
 {
 
     @Test public void testFazione() { assertThat(new Citta(null, null, null).getFazione()).isEqualTo(CITTA); }
+
+    @Test public void testCategoria() { assertThat(new Citta(null, null, null).getCategoria()).isEqualTo(UOMINI); }
 
 }
