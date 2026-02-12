69cced56622c9932a214ee1f5621c2d75aeb4cf3
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-12 14:49:54 +0100

    VERDE: testCitta riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/BoccaDiRosa.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/BoccaDiRosa.java
@@ -1,19 +1,17 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
-import static alessandro.stamera.wherewolfserver.classi.Fazione.CITTA;
 
-public final class BoccaDiRosa extends Ruolo
+public final class BoccaDiRosa extends Citta
 {
 
     public BoccaDiRosa()
     {
         super
         (
-    "Bocca di rosa", CITTA, NERA,
+    "Bocca di rosa", NERA,
 "Può votare al ballottaggio anche se è accusata. In ogni votazione i voti che Bocca di rosa riceve vengono dimezzati, arrotondando " +
-          "per eccesso.",
-     2, false
+          "per eccesso."
         );
     }
 
