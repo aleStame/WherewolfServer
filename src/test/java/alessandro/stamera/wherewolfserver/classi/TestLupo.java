92a42a4dad19691b45f8b0f8ff13fa4dff0559ff
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-12 16:51:39 +0100

    VERDE DIRETTO: testCitta riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestLupo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestLupo.java
@@ -41,6 +41,8 @@ public final class TestLupo
         verificaVero(isAmato());
     }
 
+    @Test public void testCitta() { verificaFalso(ruolo.isCitta()); }
+
     private void verificaVero(boolean valore) { assertThat(valore).isTrue(); }
 
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
