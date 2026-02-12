5d4bc8ccbcc49a3ffe02a077ddecdca3a7e515e9
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-12 17:29:08 +0100

    VERDE DIRETTO: testLupo riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBorgomastro.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBorgomastro.java
@@ -16,4 +16,6 @@ public final class TestBorgomastro
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
+    @Test public void testLupo() { assertThat(ruolo.isLupo()).isFalse(); }
+
 }
