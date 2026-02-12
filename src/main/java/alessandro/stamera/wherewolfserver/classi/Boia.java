6e9c770a75939ad88753a5e0804ccc66cca9c561
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-12 17:02:18 +0100

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Boia.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Boia.java
@@ -6,7 +6,15 @@ import static alessandro.stamera.wherewolfserver.classi.Fazione.INQUISIZIONE;
 public final class Boia extends Ruolo
 {
 
-    public Boia() { super("Boia", INQUISIZIONE, BIANCA, null, 3, false); }
+    public Boia()
+    {
+        super
+        (
+    "Boia", INQUISIZIONE, BIANCA,
+"La prima notte viene individuato dall'Inquisitore. Se non è accusato, può segnalare un giocatore durante il ballottaggio: se è un " +
+          "mistico o una creatura dell'ombra, i voti di tutti gli altri accusati vengono azzerati alla fine della votazione",
+    3, false);
+    }
 
     @Override public boolean isBoia() { return true; }
 
