412fe52bb223df0d62b91cb563e14e9e82364053
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-12 11:15:13 +0100

    VERDE: testMistico riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Citta.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Citta.java
@@ -5,6 +5,6 @@ import static alessandro.stamera.wherewolfserver.classi.Fazione.CITTA;
 public class Citta extends Ruolo
 {
 
-    public Citta(String nome, Aura aura, String descrizione) { super(nome, CITTA, aura, descrizione, 2, true); }
+    public Citta(String nome, Aura aura, String descrizione) { super(nome, CITTA, aura, descrizione, 2, false); }
 
 }
