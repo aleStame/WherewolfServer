0b9936544fbed5c2c18c708a9df854c597eb63d4
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-12 17:42:47 +0100

    ROSSO: testBorgomastro non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBorgomastro.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBorgomastro.java
@@ -18,8 +18,12 @@ public final class TestBorgomastro
 
     @Test public void testCitta() { assertThat(ruolo.isCitta()).isTrue(); }
 
-    @Test public void testAzzeccagarbugli() { assertThat(ruolo.isAzzeccagarbugli()).isFalse(); }
+    @Test public void testAzzeccagarbugli() { verificaFalso(ruolo.isAzzeccagarbugli()); }
 
-    @Test public void testBoccaDiRosa() { assertThat(ruolo.isBoccaDiRosa()).isFalse(); }
+    @Test public void testBoccaDiRosa() { verificaFalso(ruolo.isBoccaDiRosa()); }
+
+    @Test public void testBorgomastro() { assertThat(ruolo.isBorgomastro()).isTrue(); }
+
+    private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
 
 }
