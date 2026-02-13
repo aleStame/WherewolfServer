4b606d6346a2daaaffeba840dbbe4640783e0f8a
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-13 18:19:11 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Bracconiere.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Bracconiere.java
@@ -1,10 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 
 public final class Bracconiere extends Ruolo
 {
 
-    public Bracconiere() { super("Bracconiere", VILLAGGIO, null, null, 0, true); }
+    public Bracconiere() { super("Bracconiere", VILLAGGIO, BIANCA, null, 0, true); }
 
 }
