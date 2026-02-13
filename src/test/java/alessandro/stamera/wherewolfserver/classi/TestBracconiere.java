6caec908834bbb9564a50af4e11903777a52518c
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-13 18:11:37 +0100

    ROSSO: testFazione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBracconiere.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBracconiere.java
@@ -1,11 +1,14 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestBracconiere
 {
 
     @Test public void testNome() { assertThat(new Bracconiere().getNome()).isEqualTo("Bracconiere"); }
+
+    @Test public void testFazione() { assertThat(new Bracconiere().getFazione()).isEqualTo(VILLAGGIO); }
 
 }
