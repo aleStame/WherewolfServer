bd85291f99148a3f097bd612cfe9d202d38ba295
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-13 17:52:07 +0100

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Borgomastro.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Borgomastro.java
@@ -5,7 +5,15 @@ import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 public class Borgomastro extends Citta
 {
 
-    public Borgomastro() { super("Borgomastro", BIANCA, null); }
+    public Borgomastro()
+    {
+        super
+        (
+    "Borgomastro", BIANCA,
+"Può votare al ballottaggio anche se è accusato. Può segnalare un giocatore durante il ballottaggio: quel giocatore riceve un minimo " +
+          "di voti pari ai ruoli con fazione Città in gioco più uno."
+        );
+    }
 
     @Override public boolean isBorgomastro() { return true; }
 
