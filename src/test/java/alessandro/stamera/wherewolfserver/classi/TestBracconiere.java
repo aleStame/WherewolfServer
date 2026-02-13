3863295d1588d2a3f8f670dc0823d43658ffeb28
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-13 18:21:02 +0100

    ROSSO: testLune non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBracconiere.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBracconiere.java
@@ -19,4 +19,6 @@ public class TestBracconiere
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
+    @Test public void testLune() { assertThat(ruolo.getLune()).isEqualTo(3); }
+
 }
