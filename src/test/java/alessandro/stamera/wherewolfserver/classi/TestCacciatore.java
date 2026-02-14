c10a2c02926c6de46e273c84fdb96d2427b98e30
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-14 18:52:20 +0100

    ROSSO: testAura non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestCacciatore.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestCacciatore.java
@@ -1,11 +1,14 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestCacciatore
 {
 
     @Test public void testNome() { assertThat(new Cacciatore().getNome()).isEqualTo("Cacciatore"); }
+
+    @Test public void testAura() { assertThat(new Cacciatore().getAura()).isEqualTo(BIANCA); }
   
 }
