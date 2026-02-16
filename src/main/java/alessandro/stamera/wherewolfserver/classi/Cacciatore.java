0c94f4a172dc70a5ed8d0dff5c54e77272310844
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-16 18:17:35 +0100

    VERDE: testDescrizione non riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Cacciatore.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Cacciatore.java
@@ -6,7 +6,16 @@ import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 public class Cacciatore extends Ruolo
 {
 
-    public Cacciatore() { super("Cacciatore", VILLAGGIO, BIANCA, null, -1, false); }
+    public Cacciatore()
+    {
+        super
+        (
+    "Cacciatore", VILLAGGIO, BIANCA,
+"Se la Nonna si trasforma in lupo, il Cacciatore è protetto dal lupo ex Nonna. Se in gioco è rimasto l'ultimo lupo del branco (quindi " +
+          "senza contare l'eventuale presenza del Lupo solitario) o solo il LUPO SOLITARIO, il Cacciatore è protetto da questo lupo e se viene " +
+          "da questo attaccato, il lupo muore, anche se uno tra il Lupo ed il Cacciatore fosse Romeo, l'Amato o protetto dalla Strega.",
+     -1, false);
+    }
 
     @Override public boolean isCacciatore() { return true; }
 
