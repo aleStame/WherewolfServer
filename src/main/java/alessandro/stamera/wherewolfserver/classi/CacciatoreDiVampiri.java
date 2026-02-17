3640875ee06d74497b84c4b957e104e01802d7a4
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-17 12:10:31 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/CacciatoreDiVampiri.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/CacciatoreDiVampiri.java
@@ -1,10 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 
 public final class CacciatoreDiVampiri extends Ruolo
 {
 
-    public CacciatoreDiVampiri() { super("Cacciatore di vampiri", VILLAGGIO, null, null, -1, true); }
+    public CacciatoreDiVampiri() { super("Cacciatore di vampiri", VILLAGGIO, BIANCA, null, -1, true); }
 
 }
