17a83d66a9ca6f060c83b1ec530faaf6d3046819
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-17 17:22:44 +0100

    VERDE DIRETTO: testCriminale riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestCacciatoreDiVampiri.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestCacciatoreDiVampiri.java
@@ -36,6 +36,8 @@ public final class TestCacciatoreDiVampiri
 
     @Test public void testContadino() { verificaFalso(ruolo.isContadino()); }
 
+    @Test public void testCriminale() { verificaFalso(ruolo.isCriminale()); }
+
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
 
 }
