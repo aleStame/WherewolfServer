83688c33580f0d3a51170aa9cecbaf7053cdaaf7
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-17 17:01:15 +0100

    VERDE DIRETTO: testBecchino riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestCacciatoreDiVampiri.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestCacciatoreDiVampiri.java
@@ -27,6 +27,8 @@ public final class TestCacciatoreDiVampiri
 
     @Test public void testBardo() { verificaFalso(ruolo.isBardo()); }
 
+    @Test public void testBecchino() { verificaFalso(ruolo.isBecchino()); }
+
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
 
 }
