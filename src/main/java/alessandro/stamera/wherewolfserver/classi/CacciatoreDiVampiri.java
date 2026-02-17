3a116191597f35b7c8db0e516479648c1ab0e151
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-17 18:57:14 +0100

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/CacciatoreDiVampiri.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/CacciatoreDiVampiri.java
@@ -6,6 +6,14 @@ import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 public final class CacciatoreDiVampiri extends Ruolo
 {
 
-    public CacciatoreDiVampiri() { super("Cacciatore di vampiri", VILLAGGIO, BIANCA, null, 2, false); }
+    public CacciatoreDiVampiri()
+    {
+        super
+        (
+    "Cacciatore di vampiri", VILLAGGIO, BIANCA,
+"La prima notte scopre il Vampiro è in gioco. È protetto dal Vampiro e, se viene attaccato, viene avvisato e lo elimina", 2,
+    false
+        );
+    }
 
 }
