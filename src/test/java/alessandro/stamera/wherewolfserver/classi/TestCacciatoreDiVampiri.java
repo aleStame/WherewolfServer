f9772fd3530569197b02420a0566af8b278ebc5e
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-17 12:00:38 +0100

    REFACTORING: definizione di un metodo di set up

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestCacciatoreDiVampiri.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestCacciatoreDiVampiri.java
@@ -1,15 +1,19 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestCacciatoreDiVampiri
 {
 
-    @Test public void testNome() { assertThat(new CacciatoreDiVampiri().getNome()).isEqualTo("Cacciatore di vampiri"); }
+    private Ruolo ruolo;
 
-    @Test public void testFazione() { assertThat(new CacciatoreDiVampiri().getFazione()).isEqualTo(VILLAGGIO); }
+    @BeforeEach public void setUp() { ruolo = new CacciatoreDiVampiri(); }
+
+    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Cacciatore di vampiri"); }
+
+    @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(VILLAGGIO); }
 
 }
