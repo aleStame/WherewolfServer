d3c964b3fb7a28259cf8a81913c018c46e5cb740
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-18 14:02:30 +0100

    VERDE: testFazione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Villaggio.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Villaggio.java
@@ -1,11 +1,13 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Fazione.VILLAGGIO;
+
 public class Villaggio extends Ruolo
 {
 
     public Villaggio(String nome, Aura aura, String descrizione, int lune, boolean mistico)
     {
-        super(nome, null, aura, descrizione, lune, mistico);
+        super(nome, VILLAGGIO, aura, descrizione, lune, mistico);
     }
 
 }
