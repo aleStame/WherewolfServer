c9e147aa0437d561353cd580ff6bc5e2396f8bca
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-18 17:39:04 +0100

    VERDE: testAura riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/CappuccettoRosso.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/CappuccettoRosso.java
@@ -1,8 +1,10 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+
 public final class CappuccettoRosso extends Villaggio
 {
 
-    public CappuccettoRosso() { super("Cappuccetto rosso", null, null, 0, true); }
+    public CappuccettoRosso() { super("Cappuccetto rosso", BIANCA, null, 0, true); }
 
 }
