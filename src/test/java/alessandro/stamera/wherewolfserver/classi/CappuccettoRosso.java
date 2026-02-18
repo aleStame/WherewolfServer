7cbb4126a618ba233c432bc484759e56c679adc5
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-18 18:03:50 +0100

    VERDE: testMistico riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/CappuccettoRosso.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/CappuccettoRosso.java
@@ -13,7 +13,7 @@ public final class CappuccettoRosso extends Villaggio
 "Finché la Nonna è in gioco (anche se essa riceve il tratto Non morto o diventa il Posseduto) e non si è trasformata in Lupo, " +
           "Cappuccetto rosso è protetta dall'attacco dei Lupi. Se l'ultimo Lupo in gioco (sia esso l'ultimo Lupo del Branco o il Lupo solitario) " +
           "attacca Cappuccetto rosso, quest'ultima apre gli occhi e lo riconosce, anche se fosse Romeo o protetta dalla Strega",
-     1, true
+     1, false
         );
     }
 
