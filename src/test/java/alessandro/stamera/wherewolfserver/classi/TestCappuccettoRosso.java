7563d9608630afe92818b8c382f1fbf1b1826e31
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-18 18:01:19 +0100

    ROSSO: testMistico non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestCappuccettoRosso.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestCappuccettoRosso.java
@@ -27,6 +27,8 @@ public final class TestCappuccettoRosso
 
     @Test public void testLune() { assertThat(ruolo.getLune()).isEqualTo(1); }
 
+    @Test public void testMistico() { assertThat(ruolo.isMistico()).isFalse(); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
 }
