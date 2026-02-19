607d4ed8e1759d49574826da4b486b96a76b050e
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-19 17:38:21 +0100

    VERDE DIRETTO: testVillaggio riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestCacciatoreDiVampiri.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestCacciatoreDiVampiri.java
@@ -39,6 +39,8 @@ public final class TestCacciatoreDiVampiri
 
     @Test public void testCacciatoreDiVampiri() { assertThat(ruolo.isCacciatoreDiVampiri()).isTrue(); }
 
+    @Test public void testVillaggio() { assertThat(ruolo.isVillaggio()).isTrue(); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
