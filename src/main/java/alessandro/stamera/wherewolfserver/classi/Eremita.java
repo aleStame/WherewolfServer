d140b159a793ed929ee8f021fda21246afed22be
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-20 11:13:23 +0100

    VERDE: testNome riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Eremita.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Eremita.java
@@ -3,6 +3,6 @@ package alessandro.stamera.wherewolfserver.classi;
 public final class Eremita extends Villaggio
 {
 
-    public Eremita() { super(null, null, null, 0, true); }
+    public Eremita() { super("Eremita", null, null, 0, true); }
 
 }
