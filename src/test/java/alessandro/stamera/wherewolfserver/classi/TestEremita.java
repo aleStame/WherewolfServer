e8daa630cce54eeb734a6a2a0994a2906ae23a8d
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-20 11:15:07 +0100

    ROSSO: testAura non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestEremita.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestEremita.java
@@ -1,11 +1,15 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestEremita
 {
 
     @Test public void testNome() { assertThat(new Eremita().getNome()).isEqualTo("Eremita"); }
+
+    @Test public void testAura() { assertThat(new Eremita().getAura()).isEqualTo(BIANCA); }
 
 }
