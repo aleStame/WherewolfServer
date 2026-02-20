107cf1001c8387acb72e0b5e563d2c19c6316b9a
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-20 17:15:21 +0100

    VERDE DIRETTO: testVillaggio riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestEremita.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestEremita.java
@@ -35,6 +35,8 @@ public final class TestEremita
 
     @Test public void testCappuccettoRosso() { verificaFalso(ruolo.isCappuccettoRosso()); }
 
+    @Test public void testVillaggio() { assertThat(ruolo.isVillaggio()).isTrue(); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
