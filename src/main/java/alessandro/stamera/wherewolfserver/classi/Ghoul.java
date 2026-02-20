a83d6df241841fbad6bff65f3c1a1adabdb6e1de
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-20 18:21:21 +0100

    VERDE: testNome riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Ghoul.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Ghoul.java
@@ -3,6 +3,6 @@ package alessandro.stamera.wherewolfserver.classi;
 public class Ghoul extends Ruolo
 {
 
-    public Ghoul() { super(null, null, null, null, 0, true); }
+    public Ghoul() { super("Ghoul", null, null, null, 0, true); }
 
 }
