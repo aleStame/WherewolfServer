595dcae472cbedf1b7222e41b79325d92895c5de
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-21 10:45:13 +0100

    VERDE: testFazione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Ghoul.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Ghoul.java
@@ -1,8 +1,10 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Fazione.NESSUNA;
+
 public class Ghoul extends Ruolo
 {
 
-    public Ghoul() { super("Ghoul", null, null, null, 0, true); }
+    public Ghoul() { super("Ghoul", NESSUNA, null, null, 0, true); }
 
 }
