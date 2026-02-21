8859b828e0eb177f8be983c1e45b5f4b0d5b20dd
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-21 12:07:48 +0100

    VERDE DIRETTO: testLupo riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoia.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoia.java
@@ -35,9 +35,11 @@ public final class TestBoia
 
     @Test public void testCitta() { verificaFalso(ruolo.isCitta()); }
 
-    @Test public void testAngeloCustode() { verificaFalso(ruolo.isAngeloCustode()); }
-
     @Test public void testVillaggio() { verificaFalso(ruolo.isVillaggio()); }
+
+    @Test public void testLupo() { verificaFalso(ruolo.isLupo()); }
+
+    @Test public void testAngeloCustode() { verificaFalso(ruolo.isAngeloCustode()); }
 
     @Test public void testBoia() { assertThat(ruolo.isBoia()).isTrue(); }
 
