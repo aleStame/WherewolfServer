e141d7ed421a001b58aa684c61580b809f4b6443
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-21 10:42:01 +0100

    ROSSO: testFazione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGhoul.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGhoul.java
@@ -1,11 +1,15 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+
+import static alessandro.stamera.wherewolfserver.classi.Fazione.NESSUNA;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestGhoul
 {
 
     @Test public void testNome() { assertThat(new Ghoul().getNome()).isEqualTo("Ghoul"); }
+
+    @Test public void testFazione() { assertThat(new Ghoul().getFazione()).isEqualTo(NESSUNA); }
 
 }
