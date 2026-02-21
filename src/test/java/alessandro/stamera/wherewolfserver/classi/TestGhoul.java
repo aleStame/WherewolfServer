86a19dfd8789c814367c1d276f0a78d5ed310960
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-21 11:03:18 +0100

    VERDE DIRETTO: testCategoria riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGhoul.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGhoul.java
@@ -3,7 +3,6 @@ package alessandro.stamera.wherewolfserver.classi;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
-import static alessandro.stamera.wherewolfserver.classi.Fazione.NESSUNA;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestGhoul
@@ -15,7 +14,9 @@ public final class TestGhoul
 
     @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Ghoul"); }
 
-    @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(NESSUNA); }
+    @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(Fazione.NESSUNA); }
+
+    @Test public void testCategoria() { assertThat(ruolo.getCategoria()).isEqualTo(Categoria.NESSUNA); }
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
