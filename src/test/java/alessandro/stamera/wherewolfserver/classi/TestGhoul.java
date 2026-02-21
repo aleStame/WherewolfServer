266f4656cc5737c2ad313855c93f32ff5e321319
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-21 10:57:32 +0100

    ROSSO: testAura non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGhoul.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGhoul.java
@@ -1,15 +1,22 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.NESSUNA;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestGhoul
 {
 
-    @Test public void testNome() { assertThat(new Ghoul().getNome()).isEqualTo("Ghoul"); }
+    private Ruolo ruolo;
 
-    @Test public void testFazione() { assertThat(new Ghoul().getFazione()).isEqualTo(NESSUNA); }
+    @BeforeEach public void setUp() { ruolo = new Ghoul(); }
+
+    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Ghoul"); }
+
+    @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(NESSUNA); }
+
+    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
 }
