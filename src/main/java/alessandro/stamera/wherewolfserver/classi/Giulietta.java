d75f04c716b3448780fcda34c972e755bf55f88f
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-23 11:27:43 +0100

    VERDE: testLune riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Giulietta.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Giulietta.java
@@ -5,6 +5,6 @@ import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 public class Giulietta extends Villaggio
 {
 
-    public Giulietta() { super("Giulietta", BIANCA, null, 0, false); }
+    public Giulietta() { super("Giulietta", BIANCA, null, 2, false); }
 
 }
