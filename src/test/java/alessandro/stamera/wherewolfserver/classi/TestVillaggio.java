3cb4d155bc6c57ceb64a0ac9786b93746f36f8b9
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-24 11:20:47 +0100

    VERDE DIRETTO: testBoia riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestVillaggio.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestVillaggio.java
@@ -24,6 +24,8 @@ public final class TestVillaggio
         verificaFazione(CRIMINALI);
     }
 
+    @Test public void testBoia() { verificaFalso(ruolo.isBoia()); }
+
     @Test public void testCitta() { verificaFalso(ruolo.isCitta()); }
 
     @Test public void testCriminale() { verificaFalso(ruolo.isCriminale()); }
