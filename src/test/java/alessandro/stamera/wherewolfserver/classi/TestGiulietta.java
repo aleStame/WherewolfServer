a0005b6cc21df28a40cd66393d7b98e332aad0e1
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-24 17:09:19 +0100

    ROSSO: testGiulietta non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGiulietta.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGiulietta.java
@@ -13,6 +13,15 @@ public final class TestGiulietta
 
     @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Giulietta"); }
 
+    @Test public void testDescrizione()
+    {
+        String descrizione =
+            "La prima notte indica un giocatore, Romeo, che la riconosce. Quel giocatore diventa protetto dalle creature dell'ombra finché " +
+            "Giulietta è in gioco, e la sua fazione diventa Amanti. Se uno dei due viene ucciso di notte o messo al rogo, l'altro si uccide " +
+            "durante la notte.";
+        assertThat(ruolo.getDescrizione()).isEqualTo(descrizione);
+    }
+
     @Test public void testGiulietta() { assertThat(ruolo.isGiulietta()).isTrue(); }
 
     @Test public void testAngeloCustode() { assertThat(ruolo.isAngeloCustode()).isFalse(); }
