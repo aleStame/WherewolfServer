093f508b36fac3076297889e6bba07875ad0b433
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-25 15:07:56 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Giullare.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Giullare.java
@@ -1,10 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.NESSUNA;
 
 public final class Giullare extends Ruolo
 {
 
-    public Giullare() { super("Giullare", NESSUNA, null, null, 0, true); }
+    public Giullare() { super("Giullare", NESSUNA, BIANCA, null, 0, true); }
 
 }
