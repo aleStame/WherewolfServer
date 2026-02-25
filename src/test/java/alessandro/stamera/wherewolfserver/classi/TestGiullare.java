f53858893c5d61e319c52d23e32881b969174287
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-25 15:11:01 +0100

    ROSSO: testMistico non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGiullare.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGiullare.java
@@ -19,4 +19,6 @@ public final class TestGiullare
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
+    @Test public void testMistico() { assertThat(ruolo.isMistico()).isFalse(); }
+
 }
