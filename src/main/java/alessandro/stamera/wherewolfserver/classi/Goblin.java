acbb3e834774d69b26479c5c06e52a014a581800
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-26 15:21:31 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Goblin.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Goblin.java
@@ -1,10 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.NESSUNA;
 
 public class Goblin extends Ruolo
 {
 
-    public Goblin() { super("Goblin", NESSUNA, null, null, 0, false); }
+    public Goblin() { super("Goblin", NESSUNA, BIANCA, null, 0, false); }
 
 }
