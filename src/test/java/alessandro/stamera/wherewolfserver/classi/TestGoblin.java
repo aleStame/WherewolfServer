66bc303a73e191cbf60063b83cc9e47fcc8963d3
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-26 15:18:18 +0100

    VERDE DIRETTO: testCategoria riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGoblin.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGoblin.java
@@ -1,13 +1,20 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestGoblin
 {
 
-    @Test public void testNome() { assertThat(new Goblin().getNome()).isEqualTo("Goblin"); }
+    private Ruolo ruolo;
 
-    @Test public void testFazione() { assertThat(new Goblin().getFazione()).isEqualTo(Fazione.NESSUNA); }
+    @BeforeEach public void setUp() { ruolo = new Goblin(); }
+
+    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Goblin"); }
+
+    @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(Fazione.NESSUNA); }
+
+    @Test public void testCategoria() { assertThat(ruolo.getCategoria()).isEqualTo(Categoria.NESSUNA); }
 
 }
