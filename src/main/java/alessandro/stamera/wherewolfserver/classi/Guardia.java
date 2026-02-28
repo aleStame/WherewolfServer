df43dfd2bab2e0b168ae5ecce12b80888ec86b5f
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-28 15:27:41 +0100

    VERDE: testNome riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Guardia.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Guardia.java
@@ -3,6 +3,6 @@ package alessandro.stamera.wherewolfserver.classi;
 public final class Guardia extends Ruolo
 {
 
-    public Guardia() { super(null, null, null, null, 0, true); }
+    public Guardia() { super("Guardia", null, null, null, 0, true); }
 
 }
