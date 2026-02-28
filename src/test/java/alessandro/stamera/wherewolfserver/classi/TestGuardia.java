e6a6b1bfca26492f97090a4b97943539e2cf1b20
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-02-28 15:55:00 +0100

    ROSSO: testLune non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGuardia.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGuardia.java
@@ -22,6 +22,8 @@ public final class TestGuardia
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
+    @Test public void testLune() { assertThat(ruolo.getLune()).isEqualTo(2); }
+
     @Test public void testMistico() { assertThat(ruolo.isMistico()).isFalse(); }
 
 }
