0bbc5b28ff76e714eb739a3973718ab7d51c1e39
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-03 12:03:31 +0100

    VERDE DIRETTO: testGuardia riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadino.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadino.java
@@ -40,6 +40,8 @@ public final class TestContadino
 
     @Test public void testEremita() { verificaFalso(ruolo.isEremita()); }
 
+    @Test public void testGuardia() { verificaFalso(ruolo.isGuardia()); }
+
     @Test public void testVillaggio() { verificaVero(ruolo.isVillaggio()); }
 
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
