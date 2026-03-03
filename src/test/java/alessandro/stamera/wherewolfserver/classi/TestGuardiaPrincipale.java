7fd5f7307926edb8a17013428378e2d1dc227d89
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-03 18:22:34 +0100

    ROSSO: testAura non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGuardiaPrincipale.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGuardiaPrincipale.java
@@ -2,7 +2,6 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestGuardiaPrincipale
@@ -13,8 +12,6 @@ public final class TestGuardiaPrincipale
     @BeforeEach public void setUp() { ruolo = new GuardiaPrincipale(); }
 
     @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Guardia"); }
-
-    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
     @Test public void testLune() { assertThat(ruolo.getLune()).isEqualTo(2); }
 
