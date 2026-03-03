cf6677d50e0fd30eb890ccdf7ca66138cefb09db
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-03 18:23:50 +0100

    VERDE: testAura riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/Guardia.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/Guardia.java
@@ -1,8 +1,10 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+
 public class Guardia extends Villaggio
 {
 
-    public Guardia(String nome) { super(nome, null, null, 0, true); }
+    public Guardia(String nome) { super(nome, BIANCA, null, 0, true); }
 
 }
