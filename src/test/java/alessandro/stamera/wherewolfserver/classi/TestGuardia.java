d8caa1e31ddfb61f36955025642b485330070012
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-04 12:05:01 +0100

    ROSSO: testDescrizione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGuardia.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGuardia.java
@@ -20,6 +20,12 @@ public final class TestGuardia
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
+    @Test public void testDescrizione()
+    {
+        String descrizione = "La prima notte riconosce le altre guardie, poi scopre dal moderatore il numero di criminali presenti in gioco.";
+        assertThat(ruolo.getDescrizione()).isEqualTo(descrizione);
+    }
+
     @Test public void testLune() { assertThat(ruolo.getLune()).isEqualTo(2); }
 
     @Test public void testMistico() { verificaFalso(ruolo.isMistico()); }
