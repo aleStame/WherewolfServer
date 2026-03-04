3a53ac3153247b47bf9808cb151b7aee31416531
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-04 18:10:55 +0100

    VERDE: testFazione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/GuardiaCorrotta.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/GuardiaCorrotta.java
@@ -1,8 +1,14 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Fazione.CRIMINALI;
+
 public final class GuardiaCorrotta extends Guardia
 {
 
-    public GuardiaCorrotta() { super("Guardia corrotta"); }
+    public GuardiaCorrotta()
+    {
+        super("Guardia corrotta");
+        cambiaFazione(CRIMINALI);
+    }
 
 }
