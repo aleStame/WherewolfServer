07ad184d9154e25c632990ee42f3cac6d9dcefb5
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-04 12:07:34 +0100

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Guardia.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Guardia.java
@@ -5,7 +5,14 @@ import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 public class Guardia extends Villaggio
 {
 
-    public Guardia(String nome) { super(nome, BIANCA, null, 2, false); }
+    public Guardia(String nome)
+    {
+        super
+        (
+            nome, BIANCA,
+  "La prima notte riconosce le altre guardie, poi scopre dal moderatore il numero di criminali presenti in gioco.", 2, false
+        );
+    }
 
     @Override public boolean isGuardia() { return true; }
 
