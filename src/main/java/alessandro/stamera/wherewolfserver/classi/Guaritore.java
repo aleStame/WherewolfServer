b138ec7751c2a519972cbc7a236e0366932e6470
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-04 21:24:50 +0100

    VERDE: testMistico riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Guaritore.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Guaritore.java
@@ -5,6 +5,6 @@ import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 public final class Guaritore extends Villaggio
 {
 
-    public Guaritore() { super("Guaritore", BIANCA, null, 1, false); }
+    public Guaritore() { super("Guaritore", BIANCA, null, 1, true); }
 
 }
