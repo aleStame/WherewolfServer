dadcceaa54e8fbc37f59d0fdb854e45ffd735a3d
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-04 18:44:48 +0100

    REFACTORING: definizione del metodo di set up

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestAltraGuardia.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestAltraGuardia.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
@@ -8,8 +9,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class TestAltraGuardia
 {
 
-    @Test public void testNome() { assertThat(new AltraGuardia().getNome()).isEqualTo("Altra guardia"); }
+    private Ruolo ruolo;
 
-    @Test public void testAura() { assertThat(new AltraGuardia().getAura()).isEqualTo(BIANCA); }
+    @BeforeEach public void setUp() { ruolo = new AltraGuardia(); }
+
+    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Altra guardia"); }
+
+    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
 }
