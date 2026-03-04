274ca9555d71a5446da57da4998cb64775c680cd
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-04 18:31:50 +0100

    REFACTORING: definizione di un metodo di set up

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGuardiaPrincipale.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGuardiaPrincipale.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
@@ -8,8 +9,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class TestGuardiaPrincipale
 {
 
-    @Test public void testNome() { assertThat(new GuardiaPrincipale().getNome()).isEqualTo("Guardia"); }
+    private Ruolo ruolo;
 
-    @Test public void testAura() { assertThat(new GuardiaPrincipale().getAura()).isEqualTo(BIANCA); }
+    @BeforeEach public void setUp() { ruolo = new GuardiaPrincipale(); }
+
+    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Guardia"); }
+
+    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
 }
