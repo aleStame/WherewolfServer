cf679d44d6f4969cf1a46ca2ac39f6d4586884dc
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-04 22:35:36 +0100

    VERDE: testUtilizzoPotere riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Potere.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Potere.java
@@ -3,9 +3,16 @@ package alessandro.stamera.wherewolfserver.classi;
 public final class Potere extends Ruolo
 {
 
+    private boolean potereUtilizzato;
+
     public Potere(String nome, Fazione fazione, Aura aura, String descrizione, int lune, boolean mistico)
     {
         super(nome, fazione, aura, descrizione, lune, mistico);
+        potereUtilizzato = false;
     }
+
+    @Override public boolean isPotereUtilizzato() { return potereUtilizzato; }
+
+    @Override public void utilizzaPotere() { potereUtilizzato = true; }
 
 }
