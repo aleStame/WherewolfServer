29ad1a0716fcb1c14d3b7c6305c61aea419d3b82
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-04 22:37:50 +0100

    REFACTORING: definizione del metodo setUtilizzoPotere

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Potere.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Potere.java
@@ -8,11 +8,13 @@ public final class Potere extends Ruolo
     public Potere(String nome, Fazione fazione, Aura aura, String descrizione, int lune, boolean mistico)
     {
         super(nome, fazione, aura, descrizione, lune, mistico);
-        potereUtilizzato = false;
+        setUtilizzoPotere(false);
     }
 
     @Override public boolean isPotereUtilizzato() { return potereUtilizzato; }
 
-    @Override public void utilizzaPotere() { potereUtilizzato = true; }
+    @Override public void utilizzaPotere() { setUtilizzoPotere(true); }
+
+    private void setUtilizzoPotere(boolean potereUtilizzato) { this.potereUtilizzato = potereUtilizzato; }
 
 }
