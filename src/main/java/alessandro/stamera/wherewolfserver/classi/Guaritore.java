b4a8f926c0a6228ce78b6e3ad673be2bbed69f8f
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-05 10:21:45 +0100

    VERDE: testUtilizzoPotere riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Guaritore.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Guaritore.java
@@ -5,8 +5,18 @@ import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 public final class Guaritore extends Villaggio
 {
 
-    public Guaritore() { super("Guaritore", BIANCA, null, 1, true); }
+    private final Potere potere;
+
+    public Guaritore()
+    {
+        super("Guaritore", BIANCA, null, 1, true);
+        potere = new Potere();
+    }
 
     @Override public boolean isGuaritore() { return true; }
+
+    @Override public boolean isPotereUtilizzato() { return potere.isPotereUtilizzato(); }
+
+    @Override public void utilizzaPotere() { potere.utilizzaPotere(); }
 
 }
