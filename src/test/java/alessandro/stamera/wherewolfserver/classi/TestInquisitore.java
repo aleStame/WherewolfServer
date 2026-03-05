762cc9e44bc3f5b6595d1955d26cc56ba39c45f4
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-05 14:53:23 +0100

    VERDE DIRETTO: testInquisizione riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestInquisitore.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestInquisitore.java
@@ -16,4 +16,6 @@ public final class TestInquisitore
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
+    @Test public void testInquisizione() { assertThat(ruolo.isInquisizione()).isTrue(); }
+
 }
