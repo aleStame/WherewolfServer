bb3b3200f841a493bf30220bb38705c82233ea2a
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-05 10:44:58 +0100

    ROSSO: testFazione non riuscito

---
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Inquisizione.java
@@ -0,0 +1,11 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+public final class Inquisizione extends Ruolo
+{
+
+    public Inquisizione(String nome, Aura aura, String descrizione)
+    {
+        super(nome, null, aura, descrizione, 0, true);
+    }
+
+}
---
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestInquisizione.java
@@ -0,0 +1,15 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+import org.junit.jupiter.api.Test;
+import static alessandro.stamera.wherewolfserver.classi.Fazione.INQUISIZIONE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class TestInquisizione
+{
+
+    @Test public void testFazione()
+    {
+        assertThat(new Inquisizione(null, null, null).getFazione()).isEqualTo(INQUISIZIONE);
+    }
+
+}
