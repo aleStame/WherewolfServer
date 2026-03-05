914fa334baeba8ef2eabc4ec0efb8eea258cf166
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-05 18:20:23 +0100

    REFACTORING: definizione del metodo verificaStringa

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestInquisitore.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestInquisitore.java
@@ -12,14 +12,14 @@ public final class TestInquisitore
 
     @BeforeEach public void setUp() { ruolo = new Inquisitore(); }
 
-    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Inquisitore"); }
+    @Test public void testNome() { verificaStringa(ruolo.getNome(), "Inquisitore"); }
 
     @Test public void testDescrizione()
     {
         String descrizione =
             "La prima notte individua il Boia e il Templare e scopre quanti mistici sono in gioco. Se viene indicato da un mistico, lo " +
             "riconosce. Può segnalare un giocatore durante le accuse: se è un mistico, sarà accusato a prescindere dai voti ricevuti.";
-        assertThat(ruolo.getDescrizione()).isEqualTo(descrizione);
+        verificaStringa(ruolo.getDescrizione(), descrizione);
     }
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
@@ -31,5 +31,7 @@ public final class TestInquisitore
     @Test public void testInquisitore() { verificaVero(ruolo.isInquisitore()); }
 
     private void verificaVero(boolean valore) { assertThat(valore).isTrue(); }
+
+    private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
 }
