7c2e28607fd7a42cd7ac59d8ebb896c2aa8442a4
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-06 11:25:01 +0100

    REFACTORING: definizione del metodo verificaFalso

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestLadra.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestLadra.java
@@ -18,8 +18,10 @@ public final class TestLadra
 
     @Test public void testCriminale() { assertThat(ruolo.isCriminale()).isTrue(); }
 
-    @Test public void testAssassino() { assertThat(ruolo.isAssassino()).isFalse(); }
+    @Test public void testAssassino() { verificaFalso(ruolo.isAssassino()); }
 
-    @Test public void testCapoGilda() { assertThat(ruolo.isCapoGilda()).isFalse(); }
+    @Test public void testCapoGilda() { verificaFalso(ruolo.isCapoGilda()); }
+
+    private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
 
 }
