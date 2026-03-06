f074ba9784e4e7c57d079d17785f269cf35b61cc
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-06 18:15:02 +0100

    VERDE DIRETTO: testInquisizione riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestLeprecauno.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestLeprecauno.java
@@ -40,6 +40,8 @@ public final class TestLeprecauno
 
     @Test public void testGoblin() { verificaFalso(ruolo.isGoblin()); }
 
+    @Test public void testInquisizione() { verificaFalso(ruolo.isInquisizione()); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
