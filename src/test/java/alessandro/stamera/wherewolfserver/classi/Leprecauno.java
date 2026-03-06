1cb392e069adec619ca836100fcff92732e15026
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-06 17:02:00 +0100

    VERDE: testAura riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/Leprecauno.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/Leprecauno.java
@@ -1,10 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.NESSUNA;
 
 public final class Leprecauno extends Ruolo
 {
 
-    public Leprecauno() { super("Leprecauno", NESSUNA, null, null, 0, false); }
+    public Leprecauno() { super("Leprecauno", NESSUNA, BIANCA, null, 0, false); }
 
 }
