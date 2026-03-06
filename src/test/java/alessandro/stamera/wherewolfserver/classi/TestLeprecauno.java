3cea9b8a2189d0ce4d0e7c9031fee4636bde014d
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-06 16:44:53 +0100

    ROSSO: testFazione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestLeprecauno.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestLeprecauno.java
@@ -1,11 +1,14 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+import alessandro.stamera.wherewolfserver.classi.Fazione;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestLeprecauno
 {
 
     @Test public void testNome() { assertThat(new Leprecauno().getNome()).isEqualTo("Leprecauno"); }
+
+    @Test public void testFazione() { assertThat(new Leprecauno().getFazione()).isEqualTo(Fazione.NESSUNA); }
 
 }
