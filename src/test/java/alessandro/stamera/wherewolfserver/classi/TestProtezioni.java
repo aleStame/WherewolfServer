6e33ed649989b7ad0038274fcff1ef876367e0e0
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-06 12:50:38 +0100

    REFACTORING: definizione di un metodo di set up

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestProtezioni.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestProtezioni.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.Categoria.CREATURE_OMBRA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.LUPO_BRANCO;
@@ -14,18 +15,22 @@ import static alessandro.stamera.wherewolfserver.classi.Fazione.values;
 public final class TestProtezioni
 {
 
+    private Protezioni protezioni;
+
+    @BeforeEach public void setUp()
+    {
+        protezioni = new Protezioni();
+        protezioni.aggiungiProtezione(CREATURE_OMBRA);
+    }
+
     @Test public void testGiulietta()
     {
-        Protezioni protezioni = new Protezioni();
-        protezioni.aggiungiProtezione(CREATURE_OMBRA);
         for(Fazione fazione : new Fazione[] { LUPO_BRANCO, LUPO_SOLITARIO, VAMPIRO, NOSFERATU, NEGROMANTE, POSSEDUTO })
             assertThat(protezioni.isPresente(fazione)).isTrue();
     }
 
     @Test public void testLadra()
     {
-        Protezioni protezioni = new Protezioni();
-        protezioni.aggiungiProtezione(CREATURE_OMBRA);
         protezioni.perdiProtezioni();
         for(Fazione fazione : values()) assertThat(protezioni.isPresente(fazione)).isFalse();
     }
