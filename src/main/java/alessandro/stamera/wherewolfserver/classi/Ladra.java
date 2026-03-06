73f36c01611df3ffff23f1356ba22ef9d0a84c72
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-06 13:01:38 +0100

    VERDE: testUtilizzoPotere riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Ladra.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Ladra.java
@@ -1,12 +1,33 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+import static alessandro.stamera.wherewolfserver.classi.Categoria.CREATURE_OMBRA;
 
 public final class Ladra extends Criminale
 {
 
-    public Ladra() { super("Ladra", BIANCA, null); }
+    private final Protezioni protezioni;
+
+    private final Potere potere;
+
+    public Ladra()
+    {
+        super("Ladra", BIANCA, null);
+        protezioni = new Protezioni();
+        protezioni.aggiungiProtezione(CREATURE_OMBRA);
+        potere = new Potere();
+    }
 
     @Override public boolean isLadra() { return true; }
+
+    @Override public void utilizzaPotere()
+    {
+        protezioni.perdiProtezioni();
+        potere.utilizzaPotere();
+    }
+
+    @Override public boolean isPotereUtilizzato() { return potere.isPotereUtilizzato(); }
+
+    @Override public boolean isProtezionePresente(Fazione fazione) { return protezioni.isPresente(fazione); }
 
 }
