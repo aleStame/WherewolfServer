2fd6a5b6465331af6bb0ba7ebb31f1996385504b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-06 17:14:51 +0100

    VERDE: testDescrizione riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/Leprecauno.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/Leprecauno.java
@@ -6,6 +6,14 @@ import static alessandro.stamera.wherewolfserver.classi.Fazione.NESSUNA;
 public final class Leprecauno extends Ruolo
 {
 
-    public Leprecauno() { super("Leprecauno", NESSUNA, BIANCA, null, 0, false); }
+    public Leprecauno()
+    {
+        super
+        (
+    "Leprecauno", NESSUNA, BIANCA,
+"La prima notte riconosce le altre creature del Piccolo Popolo. Inoltre, è protetto da tutti i mistici",
+     0, false
+        );
+    }
 
 }
