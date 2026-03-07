bb5a065d13c9799b96ae7d27412c49a9044437e9
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-07 18:37:03 +0100

    VERDE: testProiezioni riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Eremita.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Eremita.java
@@ -1,15 +1,22 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+import static alessandro.stamera.wherewolfserver.classi.Categoria.CREATURE_OMBRA;
 
 public final class Eremita extends Villaggio
 {
 
+    private final Protezioni protezioni;
+
     public Eremita()
     {
         super("Eremita", BIANCA, "È protetto dalle creature dell'ombra", 1, false);
+        protezioni = new Protezioni();
+        protezioni.aggiungiProtezione(CREATURE_OMBRA);
     }
 
     @Override public boolean isEremita() { return true; }
+
+    @Override public boolean isProtetto(Fazione fazione) { return protezioni.isPresente(fazione); }
 
 }
