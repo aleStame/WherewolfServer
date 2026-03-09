a2580f0155b711172587823a924b663dfc4d5265
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-09 17:00:52 +0100

    REFACTORING: definizione di un metodo di set up

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestTratti.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestTratti.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,17 +8,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class TestTratti
 {
 
+    private Tratti tratti;
+
+    @BeforeEach public void setUp() { tratti = new Tratti(); }
+
     @ParameterizedTest @EnumSource(Tratto.class) public void testTrattoPresente(Tratto tratto)
     {
-        Tratti tratti = new Tratti();
         tratti.aggiungi(tratto);
         assertThat(tratti.isPresente(tratto)).isTrue();
     }
 
-    @ParameterizedTest @EnumSource(Tratto.class) public void testTrattoAssente(Tratto tratto)
-    {
-        Tratti tratti = new Tratti();
-        assertThat(tratti.isPresente(tratto)).isFalse();
-    }
+    @ParameterizedTest @EnumSource(Tratto.class)
+    public void testTrattoAssente(Tratto tratto) { assertThat(tratti.isPresente(tratto)).isFalse(); }
 
 }
