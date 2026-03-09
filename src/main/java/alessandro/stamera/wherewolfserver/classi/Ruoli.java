0abcf447162e01ceff0364f848762a5538e0172d
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-09 16:17:33 +0100

    ROSSO: testRuoli non riuscito

---
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Ruoli.java
@@ -0,0 +1,5 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+import java.util.Iterator;
+
+public final class Ruoli implements Iterable<Ruolo> { @Override public Iterator<Ruolo> iterator() { return null; } }
---
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestRuoli.java
@@ -0,0 +1,22 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class TestRuoli
+{
+
+    @Test public void testRuoli()
+    {
+        Ruolo[] soluzione = new Ruolo[]
+        {
+            new AltraGuardia(), new AngeloCustode(), new Assassino(), new Azzeccagarbugli(), new Bardo(), new Becchino(), new BoccaDiRosa(),
+            new Boia(), new Borgomastro(), new Bracconiere(), new Cacciatore(), new CacciatoreDiVampiri(), new CapoBranco(), new CapoGilda(),
+            new ContadinoEroe(), new ContadinoLupo(), new ContadinoMostro(), new ContadinoNormale(), new Eremita(), new Ghoul(), new GiovaneLupo(),
+            new Giulietta(), new Giullare(), new Goblin(), new GuardiaCorrotta(), new GuardiaPrincipale(), new Guaritore(), new Inquisitore(),
+            new Ladra(), new LupoBranco(), new LupoReietto(), new LupoSolitario()
+        };
+        assertThat(new Ruoli()).containsExactly(soluzione);
+    }
+
+}
