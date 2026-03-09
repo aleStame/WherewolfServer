9d4556b2a1900f9b21d0ded061b72911c79eb300
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-09 16:49:12 +0100

    ROSSO: testTrattoPresente non riuscito

---
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Tratti.java
@@ -0,0 +1,10 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+public final class Tratti
+{
+
+    public void aggiungi(Tratto tratto) { }
+
+    public boolean isPresente(Tratto tratto) { return false; }
+
+}
---
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestTratti.java
@@ -0,0 +1,17 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class TestTratti
+{
+
+    @ParameterizedTest @EnumSource(Tratto.class) public void testTrattoPresente(Tratto tratto)
+    {
+        Tratti tratti = new Tratti();
+        tratti.aggiungi(tratto);
+        assertThat(tratti.isPresente(tratto)).isTrue();
+    }
+
+}
