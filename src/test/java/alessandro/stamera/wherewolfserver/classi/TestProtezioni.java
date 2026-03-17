21760958375765bd95f8547811414f6b1faf17eb
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-17 18:07:03 +0100

    REFACTORING: definizione di un metodo di set up

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestProtezioni.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestProtezioni.java
@@ -1,9 +1,9 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestProtezioni
@@ -11,39 +11,42 @@ public final class TestProtezioni
 
     private Protezioni protezioni;
 
+    private RuoliFactory factory;
+
     @BeforeEach public void setUp()
     {
         protezioni = new Protezioni();
+        factory = new RuoliFactory();
     }
 
-    @ParameterizedTest @CsvSource({ "CAPO_BRANCO, LUPO_BRANCO, LUPO_SOLITARIO, LUPO_REIETTO, GIOVANE_LUPO" })
-    public void testCreatureOmbra(IstanzaRuolo istanza)
+    @ParameterizedTest @CsvSource({ "Capo branco, Lupo del branco, Lupo solitario, Lupo reietto, Giovane lupo" })
+    public void testCreatureOmbra(String nome)
     {
         protezioni.aggiungiProtezioneCreatureOmbra();
-        verificaPresenza(istanza);
+        verificaPresenza(factory.getRuolo(nome));
     }
 
-    @ParameterizedTest @CsvSource({ "CAPO_BRANCO, LUPO_BRANCO, LUPO_SOLITARIO, LUPO_REIETTO, GIOVANE_LUPO" })
-    public void testCappuccettoRosso(IstanzaRuolo istanza)
+    @ParameterizedTest @CsvSource({ "Capo branco, Lupo del branco, Lupo solitario, Lupo reietto, Giovane lupo" })
+    public void testCappuccettoRosso(String nome)
     {
         protezioni.aggiungiProtezioneLupi();
-        verificaPresenza(istanza);
+        verificaPresenza(factory.getRuolo(nome));
     }
 
-    @ParameterizedTest @EnumSource(IstanzaRuolo.class) public void testPerdiProtezioni(IstanzaRuolo istanza)
+    @Test public void testPerdiProtezioni()
     {
         protezioni.perdiProtezioni();
-        assertThat(isPresente(istanza)).isFalse();
+        for(int i = 0; i < factory.getNumeroRuoli(); i++) assertThat(isPresente(factory.getRuolo(factory.getNome(i)))).isFalse();
     }
 
-    @ParameterizedTest @CsvSource({ "GUARITORE, GOBLIN, LEPRECAUNO" }) public void testProtezioneMistici(IstanzaRuolo istanza)
+    @ParameterizedTest @CsvSource({ "Guaritore, Goblin, Leprecauno" }) public void testProtezioneMistici(String nome)
     {
         protezioni.aggiungiProtezioneMistici();
-        verificaPresenza(istanza);
+        verificaPresenza(factory.getRuolo(nome));
     }
 
-    private void verificaPresenza(IstanzaRuolo istanza) { assertThat(isPresente(istanza)).isTrue(); }
+    private void verificaPresenza(Ruolo ruolo) { assertThat(isPresente(ruolo)).isTrue(); }
 
-    private boolean isPresente(IstanzaRuolo istanza) { return protezioni.isPresente(istanza.getRuolo()); }
+    private boolean isPresente(Ruolo ruolo) { return protezioni.isPresente(ruolo); }
 
 }
