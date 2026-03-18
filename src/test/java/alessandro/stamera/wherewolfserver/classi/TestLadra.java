babd25f3fc701bd60a59c7df4ce3d0438953b856
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-19 00:15:31 +0100

    REFACTORING: definizione del metodo getRuolo

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestLadra.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestLadra.java
@@ -2,22 +2,23 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
-import static alessandro.stamera.wherewolfserver.classi.IstanzaRuolo.CAPO_BRANCO;
-import static alessandro.stamera.wherewolfserver.classi.IstanzaRuolo.LUPO_BRANCO;
-import static alessandro.stamera.wherewolfserver.classi.IstanzaRuolo.LUPO_SOLITARIO;
-import static alessandro.stamera.wherewolfserver.classi.IstanzaRuolo.LUPO_REIETTO;
-import static alessandro.stamera.wherewolfserver.classi.IstanzaRuolo.GIOVANE_LUPO;
-import static alessandro.stamera.wherewolfserver.classi.IstanzaRuolo.values;
 import static org.assertj.core.api.Assertions.assertThat;
-import static alessandro.stamera.wherewolfserver.classi.IstanzaRuolo.LADRA;
 
 public final class TestLadra
 {
 
+    private RuoliFactory factory;
+
     private Ruolo ruolo;
 
-    @BeforeEach public void setUp() { ruolo = LADRA.getRuolo(); }
+    @BeforeEach public void setUp()
+    {
+        factory = new RuoliFactory();
+        ruolo = factory.getRuolo("Ladra");
+    }
 
     @Test public void testNome() { verificaStringa(ruolo.getNome(), "Ladra"); }
 
@@ -39,14 +40,14 @@ public final class TestLadra
 
     @Test public void testLadra() { verificaVero(ruolo.isLadra()); }
 
-    @Test public void testUtilizzoPotere()
+    @ParameterizedTest @CsvSource({ "Capo branco, Lupo del branco, Giovane lupo, Lupo reietto, Lupo solitario" })
+    public void testUtilizzoPotere(String nome)
     {
         verificaFalso(isPotereUtilizzato());
-        for(IstanzaRuolo x : new IstanzaRuolo[] { CAPO_BRANCO, LUPO_BRANCO, LUPO_SOLITARIO, LUPO_REIETTO, GIOVANE_LUPO })
-            verificaVero(ruolo.isProtezionePresente(x.getRuolo()));
+        verificaVero(ruolo.isProtezionePresente(getRuolo(nome)));
         ruolo.utilizzaPotere();
         verificaVero(isPotereUtilizzato());
-        for(IstanzaRuolo x : values()) verificaFalso(ruolo.isProtezionePresente(x.getRuolo()));
+        for(int i = 0; i < factory.getNumeroRuoli(); i++) verificaFalso(ruolo.isProtezionePresente(getRuolo(factory.getNome(i))));
     }
 
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
@@ -56,5 +57,7 @@ public final class TestLadra
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
 
     private boolean isPotereUtilizzato() { return ruolo.isPotereUtilizzato(); }
+
+    private Ruolo getRuolo(String nome) { return factory.getRuolo(nome); }
 
 }
