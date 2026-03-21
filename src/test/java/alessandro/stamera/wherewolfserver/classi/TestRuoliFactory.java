a3445420abf8a735ad153d845c73b7c92eb630af
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-21 18:47:48 +0100

    REFACTORING: parametrizzazione del metodo testMistici

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestRuoliFactory.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestRuoliFactory.java
@@ -1,6 +1,5 @@
 package alessandro.stamera.wherewolfserver.classi;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
@@ -12,11 +11,8 @@ public final class TestRuoliFactory
     @ParameterizedTest @CsvSource({ "Capo branco", "Lupo del branco", "Giovane lupo", "Lupo reietto", "Lupo solitario" })
     public void testLupi(String nome) { verificaPresenza(FACTORY.getLupi(), getRuolo(nome)); }
 
-    @Test public void testMistici()
-    {
-        for(Ruolo mistico : new Ruolo[] { getRuolo("Goblin"), getRuolo("Guaritore"), getRuolo("Leprecauno") })
-            verificaPresenza(FACTORY.getMistici(), mistico);
-    }
+    @ParameterizedTest @CsvSource({ "Goblin, Guaritore, Leprecauno" })
+    public void testMistici(String nome) { verificaPresenza(FACTORY.getMistici(), getRuolo(nome)); }
 
     private void verificaPresenza(Ruolo[] ruoli, Ruolo ruolo) { assertThat(ruoli).contains(ruolo); }
 
