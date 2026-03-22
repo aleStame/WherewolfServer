9b5af7cd9e8b921b6b6ab3e2021d21d829b81d61
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-22 01:29:29 +0100

    VERDE: testLune riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Mago.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Mago.java
@@ -5,7 +5,7 @@ import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 public final class Mago extends Villaggio
 {
 
-    public Mago() { super("Mago", BIANCA, null, 0, false); }
+    public Mago() { super("Mago", BIANCA, null, 1, false); }
 
     public static Ruolo getInstance() { return new Mago(); }
 
