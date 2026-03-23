dde2ef5dcae7d03715014553a1c63b61850f5e4b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-23 15:04:25 +0100

    VERDE DIRETTO: testMago riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestCacciatore.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestCacciatore.java
@@ -53,6 +53,8 @@ public final class TestCacciatore
 
     @Test public void testGuaritore() { verificaFalso(ruolo.isGuaritore()); }
 
+    @Test public void testMago() { verificaFalso(ruolo.isMago()); }
+
     @Test public void testVillaggio() { verificaVero(ruolo.isVillaggio()); }
 
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
