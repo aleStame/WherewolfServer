85b1702e6bfd2b92a644a6124931b494b5ddffae
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-23 11:16:39 +0100

    VERDE DIRETTO: testEremita riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestMago.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestMago.java
@@ -41,6 +41,8 @@ public final class TestMago
 
     @Test public void testCappuccettoRosso() { verificaFalso(ruolo.isCappuccettoRosso()); }
 
+    @Test public void testEremita() { verificaFalso(ruolo.isEremita()); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
