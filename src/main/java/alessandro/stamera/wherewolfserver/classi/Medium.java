1046f978c49dad1c50c30b52dc332c8a83be0d3a
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-23 16:03:38 +0100

    VERDE: testMistico riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Medium.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Medium.java
@@ -9,7 +9,7 @@ public final class Medium extends Villaggio
     {
         super
         (
-    "Medium", BIANCA, "Dalla seconda notte, indica un giocatore eliminato e scopre se possedeva aura oscura.", 0, false
+    "Medium", BIANCA, "Dalla seconda notte, indica un giocatore eliminato e scopre se possedeva aura oscura.", 0, true
         );
     }
 
