9fcef9ef6e6f80460d06152ecff9650ea0563ec1
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-24 16:09:21 +0100

    VERDE DIRETTO: testControlloMedium riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadino.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestContadino.java
@@ -53,6 +53,8 @@ public final class TestContadino
 
     @Test public void testVillaggio() { verificaVero(ruolo.isVillaggio()); }
 
+    @Test public void testControlloMedium() { assertThat(ruolo.controlloMedium()).isEqualTo(BIANCA); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaVero(boolean valore) { assertThat(valore).isTrue(); }
