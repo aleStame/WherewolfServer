7dc2a02cecc65dfe22ed4ce3bb1df8b09c3cef7f
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-24 11:42:27 +0100

    REFACTORING: definizione del metodo verificaAuraNera

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoccaDiRosa.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBoccaDiRosa.java
@@ -17,7 +17,7 @@ public final class TestBoccaDiRosa
 
     @Test public void testNome() { verificaStringa(ruolo.getNome(), NOME); }
 
-    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(NERA); }
+    @Test public void testAura() { verificaAuraNera(ruolo.getAura()); }
 
     @Test public void testBoccaDiRosa() { verificaVero(ruolo.isBoccaDiRosa()); }
 
@@ -41,7 +41,9 @@ public final class TestBoccaDiRosa
 
     @Test public void testBorgomastro() { verificaFalso(ruolo.isBorgomastro()); }
 
-    @Test public void testControlloMedium() { assertThat(ruolo.controlloMedium()).isEqualTo(NERA); }
+    @Test public void testControlloMedium() { verificaAuraNera(ruolo.controlloMedium()); }
+
+    private void verificaAuraNera(Aura aura) { assertThat(aura).isEqualTo(NERA); }
 
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
