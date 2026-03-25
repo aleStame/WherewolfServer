fae5ddf8d10f6aebfa5353dc0c4910c8cce4f36a
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-25 18:12:04 +0100

    ROSSO: testDescrizione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestMegera.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestMegera.java
@@ -9,4 +9,13 @@ public final class TestMegera
 
     @Test public void testNome() { assertThat(FACTORY.getRuolo("Megera").getNome()).isEqualTo("Megera"); }
 
+    @Test public void testDescrizione()
+    {
+        String descrizione =
+            "La prima notte viene individuata da tutte le creature dell'ombra. Se viene indicata da un mistico, fino a che la Megera è in gioco, " +
+            "quel giocatore diventa Maledetto, riceverà sempre responsi negativi e non potrà più proteggere. Vince con qualsiasi creatura " +
+            "dell'ombra";
+        assertThat(FACTORY.getRuolo("Megera").getDescrizione()).isEqualTo(descrizione);
+    }
+
 }
