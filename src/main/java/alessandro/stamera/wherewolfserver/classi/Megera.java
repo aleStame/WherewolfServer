74b5b898f80ec24c9a158f9dc59ccf6372cb0526
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-25 18:14:47 +0100

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Megera.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Megera.java
@@ -3,7 +3,16 @@ package alessandro.stamera.wherewolfserver.classi;
 public class Megera extends Villaggio
 {
 
-    private Megera() { super("Megera", null, null, 0, false); }
+    private Megera()
+    {
+        super
+        (
+    "Megera", null,
+"La prima notte viene individuata da tutte le creature dell'ombra. Se viene indicata da un mistico, fino a che la Megera è in gioco, " +
+          "quel giocatore diventa Maledetto, riceverà sempre responsi negativi e non potrà più proteggere. Vince con qualsiasi creatura dell'ombra",
+     0, false
+        );
+    }
 
     public static Ruolo getInstance() { return new Megera(); }
 
