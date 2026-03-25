b02f7d59a9ce0b8b02327014a3a6dbc632ca8d56
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-25 18:46:14 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Megera.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Megera.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.NESSUNA;
 
 public final class Megera extends Ruolo
@@ -9,7 +10,7 @@ public final class Megera extends Ruolo
     {
         super
         (
-    "Megera", NESSUNA, null,
+    "Megera", NESSUNA, NERA,
 "La prima notte viene individuata da tutte le creature dell'ombra. Se viene indicata da un mistico, fino a che la Megera è in gioco, " +
           "quel giocatore diventa Maledetto, riceverà sempre responsi negativi e non potrà più proteggere. Vince con qualsiasi creatura dell'ombra",
      0, false
