efae717ba39e37c67dc40ff9b744110631ae8e70
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-25 18:18:27 +0100

    REFACTORING: definizione di un metodo di set up

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestMegera.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestMegera.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,7 +8,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class TestMegera
 {
 
-    @Test public void testNome() { assertThat(FACTORY.getRuolo("Megera").getNome()).isEqualTo("Megera"); }
+    private Ruolo ruolo;
+
+    @BeforeEach public void setUp() { ruolo = FACTORY.getRuolo("Megera"); }
+
+    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo("Megera"); }
 
     @Test public void testDescrizione()
     {
@@ -15,7 +20,7 @@ public final class TestMegera
             "La prima notte viene individuata da tutte le creature dell'ombra. Se viene indicata da un mistico, fino a che la Megera è in gioco, " +
             "quel giocatore diventa Maledetto, riceverà sempre responsi negativi e non potrà più proteggere. Vince con qualsiasi creatura " +
             "dell'ombra";
-        assertThat(FACTORY.getRuolo("Megera").getDescrizione()).isEqualTo(descrizione);
+        assertThat(ruolo.getDescrizione()).isEqualTo(descrizione);
     }
 
 }
