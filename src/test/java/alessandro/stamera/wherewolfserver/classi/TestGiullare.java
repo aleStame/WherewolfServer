3c40c66c21ff1d5ce8f6032826c35944112514a7
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-25 17:49:50 +0100

    REFACTORING: definizione del metodo verificaAuraBianca

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGiullare.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGiullare.java
@@ -29,7 +29,7 @@ public final class TestGiullare
 
     @Test public void testCategoria() { assertThat(ruolo.getCategoria()).isEqualTo(Categoria.NESSUNA); }
 
-    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
+    @Test public void testAura() { verificaAuraBianca(ruolo.getAura()); }
 
     @Test public void testLune() { assertThat(ruolo.getLune()).isEqualTo(1); }
 
@@ -51,7 +51,9 @@ public final class TestGiullare
 
     @Test public void testVillaggio() { verificaFalso(ruolo.isVillaggio()); }
 
-    @Test public void testControlloMedium() { assertThat(ruolo.controlloMedium()).isEqualTo(BIANCA); }
+    @Test public void testControlloMedium() { verificaAuraBianca(ruolo.controlloMedium()); }
+
+    private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
 
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
