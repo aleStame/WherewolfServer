00856eaac7823b3d25d46d95f6da6e6d4d8b78e0
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-25 10:33:57 +0100

    REFACTORING: definizione del metodo veriricaAuraBianca

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestLadra.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestLadra.java
@@ -26,7 +26,7 @@ public final class TestLadra
         verificaStringa(ruolo.getDescrizione(), descrizione);
     }
 
-    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
+    @Test public void testAura() { verificaAuraBianca(ruolo.getAura()); }
 
     @Test public void testCriminale() { verificaVero(ruolo.isCriminale()); }
 
@@ -47,9 +47,9 @@ public final class TestLadra
         for(Ruolo lupo : lupi) verificaFalso(isProtezionePresente(lupo));
     }
 
-    @Test public void testControlloMedium() { assertThat(ruolo.controlloMedium()).isEqualTo(BIANCA); }
+    @Test public void testControlloMedium() { verificaAuraBianca(ruolo.controlloMedium()); }
 
-    //private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
+    private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
 
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
