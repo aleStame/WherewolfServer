9a35641ac987776e2256340b4a97d91243d8783e
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-26 17:02:10 +0100

    VERDE: testMercante riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Mercante.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Mercante.java
@@ -7,6 +7,8 @@ public final class Mercante extends Citta
 
     public Mercante() { super("Mercante", BIANCA, null); }
 
+    @Override public boolean isMercante() { return true; }
+
     public static Ruolo getInstance() { return new Mercante(); }
 
 }
