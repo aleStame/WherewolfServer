654225cfc4a6f6713715befe56d9616f4c1e5f7d
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-26 15:39:29 +0100

    VERDE: testNome riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/IstanzaRuolo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/IstanzaRuolo.java
@@ -15,7 +15,7 @@ public enum IstanzaRuolo
     GUARDIA_CORROTTA(GuardiaCorrotta.getInstance()), GUARITORE(Guaritore.getInstance()), INQUISITORE(Inquisitore.getInstance()),
     LADRA(Ladra.getInstance()), LEPRECAUNO(Leprecauno.getInstance()), LUPO_BRANCO(LupoBranco.getInstance()),
     LUPO_REIETTO(LupoReietto.getInstance()), LUPO_SOLITARIO(LupoSolitario.getInstance()), MAGO(Mago.getInstance()),
-    MEDIUM(Medium.getInstance()), MEGERA(Megera.getInstance());
+    MEDIUM(Medium.getInstance()), MEGERA(Megera.getInstance()), MERCANTE(Mercante.getInstance());
 
     private final Ruolo ruolo;
 
--- src/main/java/alessandro/stamera/wherewolfserver/classi/Mercante.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Mercante.java
@@ -3,6 +3,8 @@ package alessandro.stamera.wherewolfserver.classi;
 public final class Mercante extends Citta
 {
 
-    public Mercante() { super(null, null, null); }
+    public Mercante() { super("Mercante", null, null); }
+
+    public static Ruolo getInstance() { return new Mercante(); }
 
 }
