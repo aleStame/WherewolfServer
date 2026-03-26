3a37305a8012c7629adba7375d4f6fd8b0ab72d9
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-26 15:50:28 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Mercante.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Mercante.java
@@ -1,9 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+
 public final class Mercante extends Citta
 {
 
-    public Mercante() { super("Mercante", null, null); }
+    public Mercante() { super("Mercante", BIANCA, null); }
 
     public static Ruolo getInstance() { return new Mercante(); }
 
