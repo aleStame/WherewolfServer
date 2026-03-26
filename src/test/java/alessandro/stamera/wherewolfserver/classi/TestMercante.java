60dc7f45b31bb87fcc62001a92d4ad064cd80922
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-26 16:00:38 +0100

    REFACTORING: definizione del metodo verificaAuraBianca

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestMercante.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestMercante.java
@@ -18,8 +18,10 @@ public final class TestMercante
 
     @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo(NOME); }
 
-    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
+    @Test public void testAura() { verificaAuraBianca(ruolo.getAura()); }
 
-    @Test public void testControlloMedium() { assertThat(ruolo.controlloMedium()).isEqualTo(BIANCA); }
+    @Test public void testControlloMedium() { verificaAuraBianca(ruolo.controlloMedium()); }
+
+    private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
 
 }
