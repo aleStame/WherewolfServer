7288356a610d7a86107f9431efbdac9016d5ced1
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-26 17:50:32 +0100

    VERDE: testMonaco riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Monaco.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Monaco.java
@@ -1,9 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+
 public final class Monaco extends Villaggio
 {
 
-    public Monaco() { super("Monaco", null, null, 0, true); }
+    public Monaco() { super("Monaco", BIANCA, null, 0, true); }
 
     public static Ruolo getInstance() { return new Monaco(); }
 
