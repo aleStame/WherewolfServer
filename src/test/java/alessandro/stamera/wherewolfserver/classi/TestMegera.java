06bddd855d73ffcb80042432c43e390601e261d7
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-26 14:53:18 +0100

    ROSSO: testMegera non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestMegera.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestMegera.java
@@ -50,6 +50,8 @@ public final class TestMegera
 
     @Test public void testLupo() { verificaFalso(ruolo.isLupo()); }
 
+    @Test public void testMegera() { assertThat(ruolo.isMegera()).isTrue(); }
+
     @Test public void testVillaggio() { verificaFalso(ruolo.isVillaggio()); }
 
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
