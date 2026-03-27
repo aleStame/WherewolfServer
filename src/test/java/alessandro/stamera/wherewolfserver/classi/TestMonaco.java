dcc16a830d4fb57fc2338e89f7bc03129f7a03a5
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-27 11:14:02 +0100

    ROSSO: testMistico non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestMonaco.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestMonaco.java
@@ -30,6 +30,8 @@ public final class TestMonaco
 
     @Test public void testLune() { assertThat(ruolo.getLune()).isEqualTo(1); }
 
+    @Test public void testMistico() { assertThat(ruolo.isMistico()).isFalse(); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
 }
