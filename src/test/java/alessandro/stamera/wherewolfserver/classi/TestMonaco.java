f92f5ec11158931953a13b82e28fa6c84c11cdb8
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-27 11:08:01 +0100

    ROSSO: testLune non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestMonaco.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestMonaco.java
@@ -28,6 +28,8 @@ public final class TestMonaco
         verificaStringa(ruolo.getDescrizione(), descrizione);
     }
 
+    @Test public void testLune() { assertThat(ruolo.getLune()).isEqualTo(1); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
 }
