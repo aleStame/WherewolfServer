9d7461638937a34f13a01439cf20096d30ef4b72
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-27 10:58:59 +0100

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Monaco.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Monaco.java
@@ -5,7 +5,15 @@ import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 public final class Monaco extends Villaggio
 {
 
-    public Monaco() { super("Monaco", BIANCA, null, 0, true); }
+    public Monaco()
+    {
+        super
+        (
+    "Monaco", BIANCA,
+"La prima notte scopre almeno due ruoli che non sono presenti nel gioco, fra quelli che sono tra i ruoli possibili per quella partita.",
+     0, true
+        );
+    }
 
     public static Ruolo getInstance() { return new Monaco(); }
 
