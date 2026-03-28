9b5f650551a3646d752cda728ecb6a5c1950782c
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-28 11:59:07 +0100

    VERDE DIRETTO: testBardo riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestMonaco.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestMonaco.java
@@ -32,6 +32,8 @@ public final class TestMonaco
 
     @Test public void testMistico() { assertThat(ruolo.isMistico()).isFalse(); }
 
+    @Test public void testBardo() { assertThat(ruolo.isBardo()).isFalse(); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
 }
