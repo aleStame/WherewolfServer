1927632dba1b2e326c45eb35809dab6f80e32c89
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-28 18:28:28 +0100

    ROSSO: testFazione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestNegromante.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestNegromante.java
@@ -1,6 +1,8 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+
+import static alessandro.stamera.wherewolfserver.classi.Fazione.NEGROMANTE;
 import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,5 +12,7 @@ public final class TestNegromante
     private static final String NOME = "Negromante";
 
     @Test public void testNome() { assertThat(FACTORY.getRuolo(NOME).getNome()).isEqualTo(NOME); }
+
+    @Test public void testFazione() { assertThat(FACTORY.getRuolo(NOME).getFazione()).isEqualTo(NEGROMANTE); }
 
 }
