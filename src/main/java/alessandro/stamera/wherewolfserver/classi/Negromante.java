cea41c016697d3ec70b82faf3ca789d260ae3d97
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-28 19:05:34 +0100

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Negromante.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Negromante.java
@@ -1,11 +1,12 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.NEGROMANTE;
 
 public final class Negromante extends Ruolo
 {
 
-    private Negromante() { super("Negromante", NEGROMANTE, null, null, 0, false); }
+    private Negromante() { super("Negromante", NEGROMANTE, BIANCA, null, 0, false); }
 
     public static Ruolo getInstance() { return new Negromante(); }
 
