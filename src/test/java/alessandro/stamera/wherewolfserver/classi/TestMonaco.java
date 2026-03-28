4c4181a3248b8e21508a8aaf1625b5b4e415af50
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-03-28 12:08:10 +0100

    VERDE DIRETTO: testBracconiere riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestMonaco.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestMonaco.java
@@ -36,6 +36,8 @@ public final class TestMonaco
 
     @Test public void testBecchino() { verificaFalso(ruolo.isBecchino()); }
 
+    @Test public void testBracconiere() { verificaFalso(ruolo.isBracconiere()); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
