2148fa706fc7c42cad4bf2a0999ade376219558e
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-01 10:58:10 +0200

    ROSSO: testAura non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestNosferatu.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestNosferatu.java
@@ -2,6 +2,7 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
 import static alessandro.stamera.wherewolfserver.classi.Categoria.CREATURE_OMBRA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.NOSFERATU;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,5 +22,7 @@ public final class TestNosferatu
     @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(NOSFERATU); }
 
     @Test public void testCategoria() { assertThat(ruolo.getCategoria()).isEqualTo(CREATURE_OMBRA); }
+
+    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(NERA); }
 
 }
