ff522d6485690d685ec7e47646f18b625d80102b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-01 10:46:12 +0200

    ROSSO: testFazione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestNosferatu.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestNosferatu.java
@@ -1,6 +1,8 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+
+import static alessandro.stamera.wherewolfserver.classi.Fazione.NOSFERATU;
 import static org.assertj.core.api.Assertions.assertThat;
 import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
 
@@ -10,5 +12,7 @@ public final class TestNosferatu
     private static final String NOME = "Nosferatu";
 
     @Test public void testNome() { assertThat(FACTORY.getRuolo(NOME).getNome()).isEqualTo(NOME); }
+
+    @Test public void testFazione() { assertThat(FACTORY.getRuolo(NOME).getFazione()).isEqualTo(NOSFERATU); }
 
 }
