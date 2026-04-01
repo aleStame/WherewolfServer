374f1677057c95812d35ac70484cb6929f145d2e
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-01 11:18:49 +0200

    VERDE: testCreatureOmbra riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Nosferatu.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Nosferatu.java
@@ -2,11 +2,16 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.NOSFERATU;
+import static alessandro.stamera.wherewolfserver.classi.Tratto.CREATURA_OMBRA;
 
 public final class Nosferatu extends Ruolo
 {
 
-    private Nosferatu() { super("Nosferatu", NOSFERATU, NERA, null, 0, false); }
+    private Nosferatu()
+    {
+        super("Nosferatu", NOSFERATU, NERA, null, 0, false);
+        aggiungiTratti(CREATURA_OMBRA);
+    }
 
     public static Ruolo getInstance() { return new Nosferatu(); }
 
