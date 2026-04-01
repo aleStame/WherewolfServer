321368315a55e054204eba62c78f481dcba425e2
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-01 10:40:16 +0200

    VERDE: testNome riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Nosferatu.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Nosferatu.java
@@ -3,6 +3,8 @@ package alessandro.stamera.wherewolfserver.classi;
 public final class Nosferatu extends Ruolo
 {
 
-    public Nosferatu() { super(null, null, null, null, 0, true); }
+    private Nosferatu() { super("Nosferatu", null, null, null, 0, true); }
+
+    public static Ruolo getInstance() { return new Nosferatu(); }
 
 }
