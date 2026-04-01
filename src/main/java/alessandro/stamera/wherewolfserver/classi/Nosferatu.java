def6614a0d649d0c17ccdbc0db95962b5b2ba007
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-01 11:00:37 +0200

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Nosferatu.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Nosferatu.java
@@ -1,11 +1,12 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.NOSFERATU;
 
 public final class Nosferatu extends Ruolo
 {
 
-    private Nosferatu() { super("Nosferatu", NOSFERATU, null, null, 0, true); }
+    private Nosferatu() { super("Nosferatu", NOSFERATU, NERA, null, 0, true); }
 
     public static Ruolo getInstance() { return new Nosferatu(); }
 
