44ab02c8f8f6dd101d992a949d67dc8d53129e62
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-02 11:42:29 +0200

    VERDE: testAttaccoLupiRomeo riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/ContadinoMostro.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/ContadinoMostro.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.EsitoAttacco.FALLITO;
 import static alessandro.stamera.wherewolfserver.classi.EsitoAttacco.MORTO;
 
 public final class ContadinoMostro extends Contadino
@@ -15,7 +16,12 @@ public final class ContadinoMostro extends Contadino
 
     @Override public EsitoAttacco attaccoNosferatu() { return MORTO; }
 
-    @Override public EsitoAttacco attaccoLupi(Ruolo ruolo) { return MORTO; }
+    @Override public EsitoAttacco attaccoLupi(Ruolo ruolo)
+    {
+        EsitoAttacco esito = MORTO;
+        if(isRomeo()) esito = FALLITO;
+        return esito;
+    }
 
     public static Ruolo getInstance() { return new ContadinoMostro(); }
 
