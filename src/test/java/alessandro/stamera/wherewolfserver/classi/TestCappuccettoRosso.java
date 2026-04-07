7f2467ac53766da3acb26909bb5f2646d4392427
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-07 17:20:24 +0200

    VERDE DIRETTO: testNonna riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestCappuccettoRosso.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestCappuccettoRosso.java
@@ -66,6 +66,8 @@ public final class TestCappuccettoRosso
 
     @Test public void testMonaco() { verificaFalso(ruolo.isMonaco()); }
 
+    @Test public void testNonna() { verificaFalso(ruolo.isNonna()); }
+
     @Test public void testControlloMedium() { verificaAuraBianca(ruolo.controlloMedium()); }
 
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
