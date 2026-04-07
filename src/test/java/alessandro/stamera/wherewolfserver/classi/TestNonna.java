d365a9cf387978f2406d029cd71145d89cb13c96
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-07 16:47:42 +0200

    VERDE DIRETTO: testMonaco riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestNonna.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestNonna.java
@@ -57,6 +57,8 @@ public final class TestNonna
 
     @Test public void testMedium() { verificaFalso(ruolo.isMedium()); }
 
+    @Test public void testMonaco() { verificaFalso(ruolo.isMonaco()); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
