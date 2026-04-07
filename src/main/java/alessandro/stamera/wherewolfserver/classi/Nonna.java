a54ea75fa53a3e130d916dedac4afd47db653f10
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-07 15:22:34 +0200

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Nonna.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Nonna.java
@@ -5,7 +5,16 @@ import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 public final class Nonna extends Villaggio
 {
 
-    private Nonna() { super("Nonna", BIANCA, null, 1, false); }
+    private Nonna()
+    {
+        super
+        (
+    "Nonna", BIANCA,
+"Non ci sono più creature dell'ombra ed almeno un giocatore è in gioco. Non ci sono Criminali in gioco a fine partita o, se almeno un " +
+          "Criminale è ancora in gioco, lo è anche almeno uno tra Guardia e Altra guardia",
+     1, false
+        );
+    }
 
     public static Ruolo getInstance() { return new Nonna(); }
 
