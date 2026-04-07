8b0be784bcbbf8b93d846a9bb6278f60344f9e6b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-07 14:53:42 +0200

    VERDE: testControlloLune riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Nonna.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Nonna.java
@@ -5,7 +5,7 @@ import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 public final class Nonna extends Villaggio
 {
 
-    private Nonna() { super("Nonna", BIANCA, null, 0, true); }
+    private Nonna() { super("Nonna", BIANCA, null, 1, true); }
 
     public static Ruolo getInstance() { return new Nonna(); }
 
