4c56ebf3ac058814b05d7246b6048332d5bf66e6
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-08 23:18:12 +0200

    VERDE DIRETTO: testGuardia riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestOste.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestOste.java
@@ -48,6 +48,8 @@ public final class TestOste
 
     @Test public void testEremita() { verificaFalso(ruolo.isEremita()); }
 
+    @Test public void testGuardia() { verificaFalso(ruolo.isGuardia()); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
