a5392d2800b2c017c3e8b265cf729126bc1809c8
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-08 11:16:22 +0200

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Oratore.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Oratore.java
@@ -1,9 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+
 public final class Oratore extends Citta
 {
 
-    private Oratore() { super("Oratore", null, null); }
+    private Oratore() { super("Oratore", BIANCA, null); }
 
     public static Ruolo getInstance() { return new Oratore(); }
 
