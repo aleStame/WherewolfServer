2b6e3612869a0fc942191a2634e64e85f27afd8a
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-08 21:48:51 +0200

    ROSSO: testDescrizione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestOste.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestOste.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
@@ -10,8 +11,18 @@ public final class TestOste
 
     private static final String NOME = "Oste";
 
-    @Test public void testNome() { assertThat(FACTORY.getRuolo(NOME).getNome()).isEqualTo(NOME); }
+    private Ruolo ruolo;
 
-    @Test public void testAura() { assertThat(FACTORY.getRuolo(NOME).getAura()).isEqualTo(BIANCA); }
+    @BeforeEach public void setUp() { ruolo = FACTORY.getRuolo(NOME); }
+
+    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo(NOME); }
+
+    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
+
+    @Test public void testDescrizione()
+    {
+        String descrizione = "Ogni mattino se la Veggente quella notte ha scoperto un'aura oscura, il moderatore lo comunica pubblicamente";
+        assertThat(ruolo.getDescrizione()).isEqualTo(descrizione);
+    }
 
 }
