b29f5f811b0faf8a78aadd0ad6810d394af71976
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-09 15:46:39 +0200

    VERDE DIRETTO: testMegera riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPazzo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPazzo.java
@@ -51,6 +51,8 @@ public final class TestPazzo
 
     @Test public void testLupo() { verificaFalso(ruolo.isLupo()); }
 
+    @Test public void testMegera() { verificaFalso(ruolo.isMegera()); }
+
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
 
     private void verificaStringa(String descrizione, String risultato) { assertThat(descrizione).isEqualTo(risultato); }
