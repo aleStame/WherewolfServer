eaf61abc70aa2de6156d7c80b67fb51517846448
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-09 11:43:17 +0200

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Pazzo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Pazzo.java
@@ -1,9 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+
 public final class Pazzo extends Ruolo
 {
 
-    private Pazzo() { super("Pazzo", null, null, null, 0, true); }
+    private Pazzo() { super("Pazzo", null, BIANCA, null, 0, true); }
 
     public static Ruolo getInstance() { return new Pazzo(); }
 
