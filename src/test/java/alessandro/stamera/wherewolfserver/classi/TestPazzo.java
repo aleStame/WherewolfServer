621a74a2b24d8cbdb503fe7ae57180eb2bcff548
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-09 11:40:49 +0200

    ROSSO: testAura non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPazzo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPazzo.java
@@ -1,6 +1,7 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,5 +11,7 @@ public final class TestPazzo
     private static final String NOME = "Pazzo";
 
     @Test public void testNome() { assertThat(FACTORY.getRuolo(NOME).getNome()).isEqualTo(NOME); }
+
+    @Test public void testBianca() { assertThat(FACTORY.getRuolo(NOME).getAura()).isEqualTo(BIANCA); }
 
 }
