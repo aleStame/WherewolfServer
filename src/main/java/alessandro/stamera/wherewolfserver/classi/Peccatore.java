c9a520740ce0811ab66be61a32ca57324c321e63
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-09 17:40:04 +0200

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Peccatore.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Peccatore.java
@@ -1,9 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
+
 public final class Peccatore extends Villaggio
 {
 
-    private Peccatore() { super("Peccatore", null, null, 0, true); }
+    private Peccatore() { super("Peccatore", NERA, null, 0, true); }
 
     public static Ruolo getInstance() { return new Peccatore(); }
 
