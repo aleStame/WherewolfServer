0b9e50357cf344bf5a0e3b792c1d689ea3e71eeb
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-09 12:13:02 +0200

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Pazzo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Pazzo.java
@@ -5,7 +5,16 @@ import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 public final class Pazzo extends Ruolo
 {
 
-    private Pazzo() { super("Pazzo", Fazione.NESSUNA, BIANCA, null, 0, true); }
+    private Pazzo()
+    {
+        super
+        (
+    "Pazzo", Fazione.NESSUNA, BIANCA,
+"Se viene eliminato perché ucciso dai lupi mannari, la notte successiva il loro attacco sarà annullato. Vince se viene eliminato " +
+          "dall'attacco dei lupi mannari",
+     0, true
+        );
+    }
 
     public static Ruolo getInstance() { return new Pazzo(); }
 
