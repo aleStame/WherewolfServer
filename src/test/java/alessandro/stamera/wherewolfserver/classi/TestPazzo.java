5fbe60f55b62a3c7151c6dafd815f429bab2f2ca
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-09 15:04:34 +0200

    VERDE DIRETTO: testGiullare riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPazzo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPazzo.java
@@ -43,6 +43,8 @@ public final class TestPazzo
 
     @Test public void testGhoul() { verificaFalso(ruolo.isGhoul()); }
 
+    @Test public void testGiullare() { verificaFalso(ruolo.isGiullare()); }
+
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
 
     private void verificaStringa(String descrizione, String risultato) { assertThat(descrizione).isEqualTo(risultato); }
