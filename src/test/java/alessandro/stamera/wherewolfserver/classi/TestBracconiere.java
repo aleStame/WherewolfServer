8f47171003afeeb70f7972b3cc5619deb764ef5d
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-09 10:27:17 +0200

    VERDE DIRETTO: testOste riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBracconiere.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBracconiere.java
@@ -61,6 +61,8 @@ public final class TestBracconiere
 
     @Test public void testNonna() { verificaFalso(ruolo.isNonna()); }
 
+    @Test public void testOste() { verificaFalso(ruolo.isOste()); }
+
     @Test public void testVillaggio() { verificaVero(ruolo.isVillaggio()); }
 
     @Test public void testControlloMedium() { verificaAuraBianca(ruolo.controlloMedium()); }
