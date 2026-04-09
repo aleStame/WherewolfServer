75a5f7b01bd4f4c280c25c0901b8d5bd157b9cff
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-09 15:01:42 +0200

    VERDE DIRETTO: testGhoul riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPazzo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPazzo.java
@@ -41,6 +41,8 @@ public final class TestPazzo
 
     @Test public void testCriminale() { verificaFalso(ruolo.isCriminale()); }
 
+    @Test public void testGhoul() { verificaFalso(ruolo.isGhoul()); }
+
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
 
     private void verificaStringa(String descrizione, String risultato) { assertThat(descrizione).isEqualTo(risultato); }
