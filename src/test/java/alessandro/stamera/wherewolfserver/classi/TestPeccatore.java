d02efe977f9b28b90088ea4174c0fc03c9a0968c
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-10 19:24:37 +0200

    VERDE DIRETTO: testCitta riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPeccatore.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPeccatore.java
@@ -45,6 +45,8 @@ public final class TestPeccatore
 
     @Test public void testCappuccettoRosso() { verificaFalso(ruolo.isCappuccettoRosso()); }
 
+    @Test public void testCitta() { verificaFalso(ruolo.isCitta()); }
+
     @Test public void testContadino() { verificaFalso(ruolo.isContadino()); }
 
     @Test public void testEremita() { verificaFalso(ruolo.isEremita()); }
