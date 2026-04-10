3ae0b770905b7475e3f59ea92dbc9419318d20b5
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-10 18:44:13 +0200

    VERDE DIRETTO: testNonna riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPeccatore.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPeccatore.java
@@ -59,6 +59,8 @@ public final class TestPeccatore
 
     @Test public void testMonaco() { verificaFalso(ruolo.isMonaco()); }
 
+    @Test public void testNonna() { verificaFalso(ruolo.isNonna()); }
+
     private void verificaStringa(String valore, String descrizione) { assertThat(valore).isEqualTo(descrizione); }
 
     private void verificaAuraNera(Aura aura) { assertThat(aura).isEqualTo(NERA); }
