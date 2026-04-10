d11e8420b7caec9ab532e9fab11be567d68abcb4
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-10 18:41:00 +0200

    VERDE DIRETTO: testMonaco riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPeccatore.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPeccatore.java
@@ -57,6 +57,8 @@ public final class TestPeccatore
 
     @Test public void testMedium() { verificaFalso(ruolo.isMedium()); }
 
+    @Test public void testMonaco() { verificaFalso(ruolo.isMonaco()); }
+
     private void verificaStringa(String valore, String descrizione) { assertThat(valore).isEqualTo(descrizione); }
 
     private void verificaAuraNera(Aura aura) { assertThat(aura).isEqualTo(NERA); }
