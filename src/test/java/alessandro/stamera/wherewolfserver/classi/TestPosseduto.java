f14a4867e40e48f140a85cbf2ec8f68648de9c2f
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-11 14:25:56 +0200

    ROSSO: testFazione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPosseduto.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPosseduto.java
@@ -1,6 +1,7 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.Test;
+import static alessandro.stamera.wherewolfserver.classi.Fazione.POSSEDUTO;
 import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,5 +11,7 @@ public final class TestPosseduto
     private static final String NOME = "Posseduto";
 
     @Test public void testNome() { assertThat(FACTORY.getRuolo(NOME).getNome()).isEqualTo(NOME); }
+
+    @Test public void testFazione() { assertThat(FACTORY.getRuolo(NOME).getFazione()).isEqualTo(POSSEDUTO); }
 
 }
