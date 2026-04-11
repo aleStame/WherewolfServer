1c4ce80d46f7a1e5d0ecd642d0415705572c1d91
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-11 14:39:25 +0200

    ROSSO: testAura non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPosseduto.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPosseduto.java
@@ -2,6 +2,7 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static alessandro.stamera.wherewolfserver.classi.Aura.NERA;
 import static alessandro.stamera.wherewolfserver.classi.Categoria.CREATURE_OMBRA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.POSSEDUTO;
 import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
@@ -21,5 +22,7 @@ public final class TestPosseduto
     @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(POSSEDUTO); }
 
     @Test public void testCategoria() { assertThat(ruolo.getCategoria()).isEqualTo(CREATURE_OMBRA); }
+
+    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(NERA); }
 
 }
