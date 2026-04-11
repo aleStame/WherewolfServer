44e80682e86ebb0c0e2f49640275ee35b7a26eb3
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-11 14:48:24 +0200

    VERDE DIRETTO: testControlloMedium riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPosseduto.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPosseduto.java
@@ -25,4 +25,6 @@ public final class TestPosseduto
 
     @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(NERA); }
 
+    @Test public void testControlloMedium() { assertThat(ruolo.controlloMedium()).isEqualTo(NERA); }
+
 }
