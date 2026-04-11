bdc48da4cc19edad63319d76fcdfb3cd48576997
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-11 17:19:39 +0200

    ROSSO: testFazione non riuscito

---
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/PiccoloPopolo.java
@@ -0,0 +1,8 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+public final class PiccoloPopolo extends Ruolo
+{
+
+    public PiccoloPopolo(String nome, Aura aura, String descrizione) { super(nome, null, aura, descrizione, 1, false); }
+
+}
---
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPiccoloPopolo.java
@@ -0,0 +1,11 @@
+package alessandro.stamera.wherewolfserver.classi;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class TestPiccoloPopolo
+{
+
+    @Test public void testFazione() { assertThat(new PiccoloPopolo(null, null, null).getFazione()).isEqualTo(Fazione.NESSUNA); }
+
+}
