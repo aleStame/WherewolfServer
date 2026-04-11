6d50bc4439b4fdce11685ebd7733164c6b117f6e
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-11 14:28:24 +0200

    VERDE: testFazione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Posseduto.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Posseduto.java
@@ -1,9 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Fazione.POSSEDUTO;
+
 public final class Posseduto extends Ruolo
 {
 
-    private Posseduto() { super("Posseduto", null, null, null, 0, true); }
+    private Posseduto() { super("Posseduto", POSSEDUTO, null, null, 0, true); }
 
     public static Ruolo getInstance() { return new Posseduto(); }
 
