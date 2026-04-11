6b3401c5aba1367034962234406f8c8c7599f40b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-11 11:16:53 +0200

    ROSSO: testPeccatore non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPeccatore.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPeccatore.java
@@ -79,6 +79,8 @@ public final class TestPeccatore
 
     @Test public void testPazzo() { verificaFalso(ruolo.isPazzo()); }
 
+    @Test public void testPeccatore() { assertThat(ruolo.isPeccatore()).isTrue(); }
+
     @Test public void testVillaggio() { assertThat(ruolo.isVillaggio()).isTrue(); }
 
     private void verificaStringa(String valore, String descrizione) { assertThat(valore).isEqualTo(descrizione); }
