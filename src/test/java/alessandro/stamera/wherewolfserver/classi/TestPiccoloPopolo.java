44483379d3f180f89cb927a23615b1b6a7d1f7ae
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-11 17:25:31 +0200

    VERDE DIRETTO: testCategoria riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPiccoloPopolo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPiccoloPopolo.java
@@ -8,4 +8,6 @@ public final class TestPiccoloPopolo
 
     @Test public void testFazione() { assertThat(new PiccoloPopolo(null, null, null).getFazione()).isEqualTo(Fazione.NESSUNA); }
 
+    @Test public void testCategoria() { assertThat(new PiccoloPopolo(null, null, null).getCategoria()).isEqualTo(Categoria.NESSUNA); }
+
 }
