44982d3ec6a16a7491b69a34a4e7ccdac4590128
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-11 11:56:34 +0200

    VERDE DIRETTO: testPeccatore riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestGuaritore.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestGuaritore.java
@@ -56,6 +56,8 @@ public final class TestGuaritore
 
     @Test public void testOste() { verificaFalso(ruolo.isOste()); }
 
+    @Test public void testPeccatore() { verificaFalso(ruolo.isPeccatore()); }
+
     @Test public void testVillaggio() { verificaVero(ruolo.isVillaggio()); }
 
     @Test public void testUtilizzoPotere()
