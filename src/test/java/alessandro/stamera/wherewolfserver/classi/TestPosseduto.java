8b00e5712f74dffb88ecc5033be12f494a18471d
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-11 14:35:41 +0200

    VERDE DIRETTO: testCategoria riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPosseduto.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPosseduto.java
@@ -2,6 +2,7 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static alessandro.stamera.wherewolfserver.classi.Categoria.CREATURE_OMBRA;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.POSSEDUTO;
 import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,5 +19,7 @@ public final class TestPosseduto
     @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo(NOME); }
 
     @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(POSSEDUTO); }
+
+    @Test public void testCategoria() { assertThat(ruolo.getCategoria()).isEqualTo(CREATURE_OMBRA); }
 
 }
