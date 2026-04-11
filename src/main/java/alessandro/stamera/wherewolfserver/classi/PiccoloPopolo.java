fb8383d15ff7932999da48eb92df036d65ce99aa
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-11 17:21:31 +0200

    VERDE: testFazione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/PiccoloPopolo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/PiccoloPopolo.java
@@ -1,8 +1,10 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Fazione.NESSUNA;
+
 public final class PiccoloPopolo extends Ruolo
 {
 
-    public PiccoloPopolo(String nome, Aura aura, String descrizione) { super(nome, null, aura, descrizione, 1, false); }
+    public PiccoloPopolo(String nome, Aura aura, String descrizione) { super(nome, NESSUNA, aura, descrizione, 1, false); }
 
 }
