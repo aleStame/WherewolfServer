12d5965ec23b22d9443d1fe67ccfd43856408eb5
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-11 14:31:47 +0200

    REFACTORING: definizione di un metodo di set up

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPosseduto.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPosseduto.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.Fazione.POSSEDUTO;
 import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
@@ -10,8 +11,12 @@ public final class TestPosseduto
 
     private static final String NOME = "Posseduto";
 
-    @Test public void testNome() { assertThat(FACTORY.getRuolo(NOME).getNome()).isEqualTo(NOME); }
+    private Ruolo ruolo;
 
-    @Test public void testFazione() { assertThat(FACTORY.getRuolo(NOME).getFazione()).isEqualTo(POSSEDUTO); }
+    @BeforeEach public void setUp() { ruolo = FACTORY.getRuolo(NOME); }
+
+    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo(NOME); }
+
+    @Test public void testFazione() { assertThat(ruolo.getFazione()).isEqualTo(POSSEDUTO); }
 
 }
