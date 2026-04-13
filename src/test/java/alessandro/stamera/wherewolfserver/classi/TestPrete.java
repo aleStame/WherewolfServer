6acef08af16747d14c81f17b732fcc87e07a8092
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-13 22:38:15 +0200

    VERDE DIRETTO: testEremita riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPrete.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPrete.java
@@ -47,6 +47,8 @@ public final class TestPrete
 
     @Test public void testContadino() { verificaFalso(ruolo.isContadino()); }
 
+    @Test public void testEremita() { verificaFalso(ruolo.isEremita()); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
