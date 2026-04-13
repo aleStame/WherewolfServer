8db765c0df9f7c4b7bcfdcb6b5db246648c6ce06
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-13 09:51:02 +0200

    VERDE: testProtezioneMistici riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/PiccoloPopolo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/PiccoloPopolo.java
@@ -1,10 +1,15 @@
 package alessandro.stamera.wherewolfserver.classi;
 
 import static alessandro.stamera.wherewolfserver.classi.Fazione.NESSUNA;
+import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
 
 public final class PiccoloPopolo extends Ruolo
 {
 
-    public PiccoloPopolo(String nome, Aura aura, String descrizione) { super(nome, NESSUNA, aura, descrizione, 1, true); }
+    public PiccoloPopolo(String nome, Aura aura, String descrizione)
+    {
+        super(nome, NESSUNA, aura, descrizione, 1, true);
+        aggiungiProtezione(FACTORY.getRuolo("Guaritore"), FACTORY.getRuolo("Mago"), FACTORY.getRuolo("Megera"));
+    }
 
 }
