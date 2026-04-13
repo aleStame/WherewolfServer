ed5e8b64d7c9a50dcf535f8f1c08eb47c1098c1c
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-13 17:00:42 +0200

    VERDE DIRETTO: testPiccoloPopolo riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestLupo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestLupo.java
@@ -57,6 +57,8 @@ public final class TestLupo
 
     @Test public void testPazzo() { verificaFalso(ruolo.isPazzo()); }
 
+    @Test public void testPiccoloPopolo() { verificaFalso(ruolo.isPiccoloPopolo()); }
+
     @Test public void testVillaggio() { verificaFalso(ruolo.isVillaggio()); }
 
     @Test public void testControlloMedium() { verificaAuraNera(ruolo.controlloMedium()); }
