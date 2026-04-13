c8df14bb06da5e6fae742e8c3e8818adb715a9c2
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-13 22:25:42 +0200

    VERDE DIRETTO: testCacciatore riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPrete.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPrete.java
@@ -39,6 +39,8 @@ public final class TestPrete
 
     @Test public void testBracconiere() { verificaFalso(ruolo.isBracconiere()); }
 
+    @Test public void testCacciatore() { verificaFalso(ruolo.isCacciatore()); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
