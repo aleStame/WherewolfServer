26df64e2a0b16359a58fb6ae7a76a5b46889072b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-13 17:50:53 +0200

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Prete.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Prete.java
@@ -1,9 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+
 public final class Prete extends Villaggio
 {
 
-    private Prete() { super("Prete", null, null, 0, true); }
+    private Prete() { super("Prete", BIANCA, null, 0, true); }
 
     public static Ruolo getInstance() { return new Prete(); }
 
