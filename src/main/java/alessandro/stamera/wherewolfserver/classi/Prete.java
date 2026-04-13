323a25cf928e974d9fd92eb3affe3d08a2a99dd6
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-13 18:02:36 +0200

    VERDE: testDescrizione non riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Prete.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Prete.java
@@ -5,7 +5,16 @@ import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 public final class Prete extends Villaggio
 {
 
-    private Prete() { super("Prete", BIANCA, null, 0, true); }
+    private Prete()
+    {
+        super
+        (
+    "Prete", BIANCA,
+"La prima notte individua il Peccatore e scopre se Bocca di rosa e il Posseduto sono in gioco. È protetto dal Posseduto e, se viene " +
+          "attaccato, viene avvisato e gli fa perdere tutti i poteri.",
+     0, true
+        );
+    }
 
     public static Ruolo getInstance() { return new Prete(); }
 
