ff0c724fc0a8b9e54b3072589b08a3b001f91abf
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-13 22:32:03 +0200

    VERDE DIRETTO: testCappuccettoRosso riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPrete.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPrete.java
@@ -43,6 +43,8 @@ public final class TestPrete
 
     @Test public void testCacciatoreDiVampiri() { verificaFalso(ruolo.isCacciatoreDiVampiri()); }
 
+    @Test public void testCappuccettoRosso() { verificaFalso(ruolo.isCappuccettoRosso()); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
