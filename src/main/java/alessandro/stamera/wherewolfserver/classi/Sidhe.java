05cd0aab6419b58dd2eb0cf27e74b8e3a61ed28b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-14 14:27:29 +0200

    VERDE: testNome non riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Sidhe.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Sidhe.java
@@ -3,6 +3,8 @@ package alessandro.stamera.wherewolfserver.classi;
 public final class Sidhe extends PiccoloPopolo
 {
 
-    private Sidhe() { super(null, null, null); }
+    private Sidhe() { super("Sidhe", null, null); }
+
+    public static Ruolo getInstance() { return new Sidhe(); }
 
 }
