7ab2f88143d557baccb1fa018c00b9079c44cebd
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-14 16:44:51 +0200

    VERDE: testAura riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Spia.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Spia.java
@@ -1,9 +1,11 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
+
 public final class Spia extends Criminale
 {
 
-    private Spia() { super("Spia", null, null); }
+    private Spia() { super("Spia", BIANCA, null); }
 
     public static Ruolo getInstance() { return new Spia(); }
 
