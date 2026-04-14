960d2948548fa5ccea4f8e54f0819b0fa9194f4d
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-14 15:08:54 +0200

    ROSSO: testProtezioneMistici non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestSidhe.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestSidhe.java
@@ -2,8 +2,12 @@ package alessandro.stamera.wherewolfserver.classi;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
+import static alessandro.stamera.wherewolfserver.classi.Tratto.PROTETTO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestSidhe
@@ -25,6 +29,12 @@ public final class TestSidhe
     {
         String descrizione = "La prima notte riconosce le altre creature del Piccolo Popolo (Goblin, Leprecauno). È protetta da tutti i Mistici.";
         assertThat(ruolo.getDescrizione()).isEqualTo(descrizione);
+    }
+
+    @ParameterizedTest @CsvSource({ "Guaritore, Mago, Megera" }) public void testProtezioneMistici(String nome)
+    {
+        assertThat(ruolo.isProtezionePresente(FACTORY.getRuolo(nome))).isTrue();
+        assertThat(ruolo.isTrattoPresente(PROTETTO)).isTrue();
     }
 
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
