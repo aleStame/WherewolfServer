fd2093586bbf75a636e3f058d7b04d179a16ef99
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-14 17:24:35 +0200

    VERDE: testSpia riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Spia.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Spia.java
@@ -7,6 +7,8 @@ public final class Spia extends Criminale
 
     private Spia() { super("Spia", BIANCA, null); }
 
+    @Override public boolean isSpia() { return true; }
+
     public static Ruolo getInstance() { return new Spia(); }
 
 }
