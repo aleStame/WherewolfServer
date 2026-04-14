583f3c9dae394621648e658eadfc4ddb74d887f7
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-14 14:47:46 +0200

    VERDE: testDescrizione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Sidhe.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Sidhe.java
@@ -5,7 +5,14 @@ import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 public final class Sidhe extends PiccoloPopolo
 {
 
-    private Sidhe() { super("Sidhe", BIANCA, null); }
+    private Sidhe()
+    {
+        super
+        (
+    "Sidhe", BIANCA,
+"La prima notte riconosce le altre creature del Piccolo Popolo (Goblin, Leprecauno). È protetta da tutti i Mistici."
+        );
+    }
 
     public static Ruolo getInstance() { return new Sidhe(); }
 
