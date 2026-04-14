2ac4198e025c2cc28e156f11ea37722dab6460e7
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-14 14:51:34 +0200

    REFACTORING: definizione della costante NOME

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestSidhe.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestSidhe.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.Aura.BIANCA;
 import static alessandro.stamera.wherewolfserver.classi.Partita.FACTORY;
@@ -10,13 +11,18 @@ public final class TestSidhe
 
     private static final String NOME = "Sidhe";
 
-    @Test public void testNome() { assertThat(FACTORY.getRuolo(NOME).getNome()).isEqualTo(NOME); }
+    private Ruolo ruolo;
 
-    @Test public void testAura() { assertThat(FACTORY.getRuolo(NOME).getAura()).isEqualTo(BIANCA); }
+    @BeforeEach public void setUp() { ruolo = FACTORY.getRuolo(NOME); }
+
+    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo(NOME); }
+
+    @Test public void testAura() { assertThat(ruolo.getAura()).isEqualTo(BIANCA); }
 
     @Test public void testDescrizione()
     {
-        assertThat(FACTORY.getRuolo(NOME).getDescrizione()).isEqualTo("La prima notte riconosce le altre creature del Piccolo Popolo (Goblin, Leprecauno). È protetta da tutti i Mistici.");
+        String descrizione = "La prima notte riconosce le altre creature del Piccolo Popolo (Goblin, Leprecauno). È protetta da tutti i Mistici.";
+        assertThat(ruolo.getDescrizione()).isEqualTo(descrizione);
     }
 
 }
