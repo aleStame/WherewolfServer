bcd8cd01a9b18de50db4411ad1f6118abad0950b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-15 15:48:52 +0200

    VERDE DIRETTO: testBracconiere riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestSensitiva.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestSensitiva.java
@@ -50,6 +50,8 @@ public final class TestSensitiva
 
     @Test public void testBecchino() { verificaFalso(ruolo.isBecchino()); }
 
+    @Test public void testBracconiere() { verificaFalso(ruolo.isBracconiere()); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
