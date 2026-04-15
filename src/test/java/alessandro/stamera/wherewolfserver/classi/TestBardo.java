f5080b45c6abace775f0123d045f89f9c1227472
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-15 16:26:26 +0200

    VERDE DIRETTO: testSensitiva riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBardo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBardo.java
@@ -66,6 +66,8 @@ public final class TestBardo
 
     @Test public void testPrete() { verificaFalso(ruolo.isPrete()); }
 
+    @Test public void testSensitiva() { verificaFalso(ruolo.isSensitiva()); }
+
     @Test public void testVillaggio() { verificaVero(ruolo.isVillaggio()); }
 
     @Test public void testControlloMedium() { verificaAuraBianca(ruolo.controlloMedium()); }
