029db47b477f86310aa25fecf11bcc98bb198606
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-15 16:32:20 +0200

    VERDE DIRETTO: testSensitiva riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestBracconiere.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestBracconiere.java
@@ -67,6 +67,8 @@ public final class TestBracconiere
 
     @Test public void testPrete() { verificaFalso(ruolo.isPrete()); }
 
+    @Test public void testSensitiva() { verificaFalso(ruolo.isSensitiva()); }
+
     @Test public void testVillaggio() { verificaVero(ruolo.isVillaggio()); }
 
     @Test public void testControlloMedium() { verificaAuraBianca(ruolo.controlloMedium()); }
