4433dff9bbaa0604a22502fc61adf6b80496c6cc
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-15 16:05:10 +0200

    VERDE DIRETTO: testMedium riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestSensitiva.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestSensitiva.java
@@ -64,6 +64,8 @@ public final class TestSensitiva
 
     @Test public void testMago() { verificaFalso(ruolo.isMago()); }
 
+    @Test public void testMedium() { verificaFalso(ruolo.isMedium()); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
