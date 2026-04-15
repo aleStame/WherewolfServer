3e23291d37edbccf80f17e9dc0640655e8cac92b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-15 11:04:59 +0200

    ROSSO: testMistico non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestSensitiva.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestSensitiva.java
@@ -32,6 +32,8 @@ public final class TestSensitiva
 
     @Test public void testLune() { assertThat(ruolo.getLune()).isEqualTo(1); }
 
+    @Test public void testMistico() { assertThat(ruolo.isMistico()).isTrue(); }
+
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
 
     private void verificaAuraBianca(Aura aura) { assertThat(aura).isEqualTo(BIANCA); }
