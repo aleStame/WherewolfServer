704cc047b356e7387ca0e227107182fab117c5d5
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-15 17:04:30 +0200

    VERDE DIRETTO: testSensitiva riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestMonaco.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestMonaco.java
@@ -64,6 +64,8 @@ public final class TestMonaco
 
     @Test public void testPrete() { verificaFalso(ruolo.isPrete()); }
 
+    @Test public void testSensitiva() { verificaFalso(ruolo.isSensitiva()); }
+
     @Test public void testVillaggio() { verificaVero(ruolo.isVillaggio()); }
 
     private void verificaStringa(String valore, String risultato) { assertThat(valore).isEqualTo(risultato); }
