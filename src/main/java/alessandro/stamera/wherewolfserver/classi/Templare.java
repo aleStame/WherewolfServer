783af44b832fca78623b76ff3d8f303bfac79d68
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-16 11:24:22 +0200

    VERDE: testTemplare riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Templare.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Templare.java
@@ -7,6 +7,8 @@ public final class Templare extends Inquisizione
 
     private Templare() { super("Templare", BIANCA, null); }
 
+    @Override public boolean isTemplare() { return true; }
+
     public static Ruolo getInstance() { return new Templare(); }
 
 }
