ea2180bed1fd770fc349473097fa8a0bec17200e
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-17 12:23:33 +0200

    REFACTORING: definizione del metodo creaBallottaggio

--- src/main/java/alessandro/stamera/wherewolfserver/classi/GiocatoriVivi.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/GiocatoriVivi.java
@@ -5,17 +5,21 @@ public final class GiocatoriVivi extends Giocatori
 
     public Giocatori getBallottaggio()
     {
+        Giocatori ballottaggio = creaBallottaggio();
+        annullaVoti();
+        return ballottaggio;
+    }
+
+    private Giocatori creaBallottaggio()
+    {
         Giocatori ballottaggio = new Ballottaggio();
         for(int i = 0; i < 2; i++)
         {
             String nome = getNomeGiocatore(i);
             if(getNumeroVoti(nome) > 0) ballottaggio.aggiungiGiocatore(nome, getRuolo(nome));
         }
-        annullaVoti();
         ballottaggio.annullaVoti();
         return ballottaggio;
     }
-
-    //private Giocatori creaBallottaggio() {}
 
 }
