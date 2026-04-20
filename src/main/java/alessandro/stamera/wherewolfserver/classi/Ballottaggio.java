c5dd884e13d51a508b295c2a00133a76f564adfa
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-20 17:30:11 +0200

    VERDE: testAmatoPresente riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/Ballottaggio.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/Ballottaggio.java
@@ -3,6 +3,11 @@ package alessandro.stamera.wherewolfserver.classi;
 public final class Ballottaggio extends Giocatori
 {
 
-    public boolean isAmatoPresente() { return false; }
+    public boolean isAmatoPresente()
+    {
+        boolean esito = false;
+        for(int i = 0; i < getNumeroGiocatori() && !esito; i++) esito = getRuolo(getNomeGiocatore(i)).isAmato();
+        return esito;
+    }
 
 }
