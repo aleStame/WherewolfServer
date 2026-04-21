94605f1c617825c135b07911980e3be5a3ae2280
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-21 18:24:31 +0200

    REFACTORING: definizione del metodo segnalazioneAngeloCustode

--- src/test/java/alessandro/stamera/wherewolfserver/classi/TestPartita.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/TestPartita.java
@@ -52,7 +52,7 @@ public final class TestPartita
     {
         int posizione = 2;
         String nome = getNomeGiocatoreEsempio(posizione);
-        partita.segnalazioneAngeloCustode(nome);
+        segnalazioneAngeloCustode(nome);
         incrementaVoti(posizione, 3);
         terminaVotazioni();
         verificaAccusato(getNomeGiocatoreEsempio(0));
@@ -62,7 +62,7 @@ public final class TestPartita
     @Test public void testAngeloCustodeAccusatoPresente()
     {
         String nome = getNomeGiocatoreEsempio(1);
-        partita.segnalazioneAngeloCustode(nome);
+        segnalazioneAngeloCustode(nome);
         for(int i = 0; i < 3; i++) incrementaVoti(i, 2);
         terminaVotazioni();
         String[] soluzioni = new String[] { getNomeGiocatoreEsempio(2), getNomeGiocatoreEsempio(0) };
@@ -81,7 +81,7 @@ public final class TestPartita
     @Test public void testAttaccoAmatoAssassino()
     {
         String nomeAngeloCustode = getNomeGiocatoreEsempio(0), nomeAmato = getNomeGiocatoreEsempio(3);
-        partita.segnalazioneAngeloCustode(nomeAmato);
+        segnalazioneAngeloCustode(nomeAmato);
         partita.attaccoAssassino(nomeAmato);
         assertThat(partita.isEliminato(nomeAngeloCustode)).isTrue();
         assertThat(partita.isVivo(nomeAmato)).isTrue();
@@ -98,5 +98,7 @@ public final class TestPartita
     private String getNomeGiocatoreEsempio(int posizione) { return ESEMPI_GIOCATORI[posizione][0]; }
 
     private boolean isAccusato(String nome) { return partita.isAccusato(nome); }
+
+    private void segnalazioneAngeloCustode(String nome) { partita.segnalazioneAngeloCustode(nome); }
 
 }
