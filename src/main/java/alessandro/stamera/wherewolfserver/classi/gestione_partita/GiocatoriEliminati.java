1ed16e2e6fc8f3be67e3a9e503d873111ab964a1
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-27 18:31:56 +0200

    REFACTORING: definizione del metodo isBardo

--- src/main/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/GiocatoriEliminati.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/GiocatoriEliminati.java
@@ -6,8 +6,10 @@ public final class GiocatoriEliminati extends Giocatori
     public boolean isBardoPresente()
     {
         boolean esito = false;
-        for(int i = 0; i < getNumeroGiocatori() && !esito; i++) esito = (getRuolo(getNomeGiocatore(i)).isBardo());
+        for(int i = 0; i < getNumeroGiocatori() && !esito; i++) esito = isBardo(i);
         return esito;
     }
+
+    private boolean isBardo(int posizione) { return getRuolo(getNomeGiocatore(posizione)).isBardo(); }
 
 }
