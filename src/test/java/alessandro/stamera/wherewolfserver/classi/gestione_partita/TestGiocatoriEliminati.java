82f4982dd9cdd343c0eaa1b1457bc88ebd0d56ef
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-27 18:19:03 +0200

    REFACTORING: definizione di un metodo di set up

--- src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestGiocatoriEliminati.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestGiocatoriEliminati.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi.gestione_partita;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.gestione_partita.Partita.FACTORY;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,17 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class TestGiocatoriEliminati
 {
 
+    private GiocatoriEliminati giocatori;
+
+    @BeforeEach public void setUp() { giocatori = new GiocatoriEliminati(); }
+
     @Test public void testBardoPresente()
     {
-        GiocatoriEliminati giocatori = new GiocatoriEliminati();
         giocatori.aggiungiGiocatore("Pino", FACTORY.getRuolo("Bardo"));
         assertThat(giocatori.isBardoPresente()).isTrue();
     }
 
-    @Test public void testBardoAssente()
-    {
-        GiocatoriEliminati giocatori = new GiocatoriEliminati();
-        assertThat(giocatori.isBardoPresente()).isFalse();
-    }
+    @Test public void testBardoAssente() { assertThat(giocatori.isBardoPresente()).isFalse(); }
 
 }
