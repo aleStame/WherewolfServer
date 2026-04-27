e278786499630ddf093d0c3ec5d5808866f27bbf
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-27 18:09:37 +0200

    VERDE: testBardoPresente riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/GiocatoriEliminati.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/GiocatoriEliminati.java
@@ -3,6 +3,11 @@ package alessandro.stamera.wherewolfserver.classi.gestione_partita;
 public final class GiocatoriEliminati extends Giocatori
 {
 
-    public boolean isBardoPresente() { return false; }
+    public boolean isBardoPresente()
+    {
+        boolean esito = false;
+        for(int i = 0; i < getNumeroGiocatori() && !esito; i++) esito = (getRuolo(getNomeGiocatore(i)).isBardo());
+        return esito;
+    }
 
 }
