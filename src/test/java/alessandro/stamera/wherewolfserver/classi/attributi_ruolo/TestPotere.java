3fa3ac0bc62fb6aa3aca070018f00b757d2b976b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-04-29 15:28:25 +0200

    REFACTORING: definizione del metodo verificaFalso

--- src/test/java/alessandro/stamera/wherewolfserver/classi/attributi_ruolo/TestPotere.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/attributi_ruolo/TestPotere.java
@@ -9,11 +9,13 @@ public final class TestPotere
     @Test public void testUtilizzoPotere()
     {
         Potere potere = new Potere();
-        assertThat(potere.isPotereUtilizzato()).isFalse();
+        verificaFalso(potere.isPotereUtilizzato());
         potere.utilizzaPotere();
         assertThat(potere.isPotereUtilizzato()).isTrue();
         potere.riabilitaPotere();
-        assertThat(potere.isPotereUtilizzato()).isFalse();
+        verificaFalso(potere.isPotereUtilizzato());
     }
+
+    private void verificaFalso(boolean valore) { assertThat(valore).isFalse(); }
 
 }
