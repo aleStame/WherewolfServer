90da786347e0e5ea693b82532cfc73493041c3bf
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-05-06 21:54:52 +0200

    ROSSO: testSconfitta non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/lupi/TestCapoBranco.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/lupi/TestCapoBranco.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoPartita.VITTORIA;
+import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoPartita.SCONFITTA;
 import static alessandro.stamera.wherewolfserver.classi.gestione_partita.Partita.FACTORY;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -49,6 +50,12 @@ public final class TestCapoBranco
     {
         Partita partita = new Partita(new String[][] { { "Noemi", "Capo branco" }, { "Elisa", "Lupo del branco" }, { "Damiano", "Pazzo" } });
         assertThat(ruolo.getEsitoPartita(partita)).isEqualTo(VITTORIA);
+    }
+
+    @Test public void testSconfitta()
+    {
+        Partita partita = new Partita(new String[][] { { "Manfredi", "Azzeccagarbugli" }, { "Massimo", "Giullare" }, { "Luana", "Guaritore" } });
+        assertThat(ruolo.getEsitoPartita(partita)).isEqualTo(SCONFITTA);
     }
 
     private void verificaStringa(String valore, String descrizione) { assertThat(valore).isEqualTo(descrizione); }
