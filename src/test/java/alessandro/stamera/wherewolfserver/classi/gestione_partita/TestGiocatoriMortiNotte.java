1f39d6a816eba172d098c05befd82b443c68266b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-05-09 15:15:46 +0200

    REFACTORING: definizione di un metodo di set up

--- src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestGiocatoriMortiNotte.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestGiocatoriMortiNotte.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi.gestione_partita;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoAttacco.RIUSCITO;
 import static alessandro.stamera.wherewolfserver.classi.gestione_partita.Partita.FACTORY;
@@ -8,20 +9,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class TestGiocatoriMortiNotte
 {
 
+    private GiocatoriMortiNotte giocatori;
+
+    @BeforeEach public void setUp() { giocatori = new GiocatoriMortiNotte(); }
+
     @Test public void testProgenieNosferatu()
     {
         String nome = "Marco";
-        GiocatoriMortiNotte giocatori = new GiocatoriMortiNotte();
         giocatori.aggiungiGiocatore(nome, FACTORY.getRuolo("Prete"));
         assertThat(giocatori.progenizzazioneNosferatu(nome)).isEqualTo(RIUSCITO);
     }
     @Test public void testLupo()
     {
         String[][] giocatori = new String[][] { { "Katia", "Nosferatu" }, { "Valeria", "Giovane lupo" } };
-        GiocatoriMortiNotte morti = new GiocatoriMortiNotte();
-        for(String[] giocatore : giocatori) morti.aggiungiGiocatore(giocatore[0], FACTORY.getRuolo(giocatore[1]));
-        assertThat(morti.isLupo(giocatori[0][0])).isFalse();
-        assertThat(morti.isLupo(giocatori[1][0])).isTrue();
+        for(String[] giocatore : giocatori) this.giocatori.aggiungiGiocatore(giocatore[0], FACTORY.getRuolo(giocatore[1]));
+        assertThat(this.giocatori.isLupo(giocatori[0][0])).isFalse();
+        assertThat(this.giocatori.isLupo(giocatori[1][0])).isTrue();
     }
 
 
