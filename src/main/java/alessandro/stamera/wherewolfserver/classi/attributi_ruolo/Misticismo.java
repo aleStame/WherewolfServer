d15457bf8a69d33ace5a48f5b99012a938af78b6
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-05-20 00:44:58 +0200

    VERDE: testStringaMistico riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/attributi_ruolo/Misticismo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/attributi_ruolo/Misticismo.java
@@ -1,4 +1,14 @@
 package alessandro.stamera.wherewolfserver.classi.attributi_ruolo;
 
-public enum Misticismo {
+public enum Misticismo
+{
+
+    MISTICO("Mistico"), NON_MISTICO("Non mistico");
+
+    private final String messaggio;
+
+    Misticismo(String messaggio) { this.messaggio = messaggio; }
+
+    @Override public String toString() { return messaggio; }
+
 }
