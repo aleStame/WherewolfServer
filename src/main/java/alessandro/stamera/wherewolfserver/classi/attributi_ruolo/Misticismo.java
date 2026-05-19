ac4b78df1afa297b4f90ad9056f5b53ba48d0414
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-05-20 00:55:46 +0200

    ROSSO: testRicerca non riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/attributi_ruolo/Misticismo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/attributi_ruolo/Misticismo.java
@@ -1,5 +1,8 @@
 package alessandro.stamera.wherewolfserver.classi.attributi_ruolo;
 
+import java.util.Arrays;
+import static java.util.Arrays.stream;
+
 public enum Misticismo
 {
 
@@ -10,5 +13,11 @@ public enum Misticismo
     Misticismo(String messaggio) { this.messaggio = messaggio; }
 
     @Override public String toString() { return messaggio; }
+
+    public static Misticismo getMisticismo(String descrizione)
+    {
+        return null;
+        //return stream(values()).filter(valore -> valore.toString().equals(descrizione)).findFirst().get();
+    }
 
 }
--- src/test/java/alessandro/stamera/wherewolfserver/classi/attributi_ruolo/TestMisticismo.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/attributi_ruolo/TestMisticismo.java
@@ -2,6 +2,7 @@ package alessandro.stamera.wherewolfserver.classi.attributi_ruolo;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Misticismo.getMisticismo;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestMisticismo
@@ -10,7 +11,7 @@ public final class TestMisticismo
     @ParameterizedTest @CsvSource({ "MISTICO, Mistico", "NON_MISTICO, Non mistico" })
     public void testStringaMistico(Misticismo misticismo, String messaggio) { assertThat(misticismo.toString()).isEqualTo(messaggio); }
 
-    /*@ParameterizedTest @CsvSource({ "Bianca, BIANCA", "Nera, NERA" })
-    public void testRicerca(String descrizione, Aura aura) { assertThat(getAura(descrizione)).isEqualTo(aura); }*/
+    @ParameterizedTest @CsvSource({ "Bianca, BIANCA", "Nera, NERA" })
+    public void testRicerca(String descrizione, Misticismo misticismo) { assertThat(getMisticismo(descrizione)).isEqualTo(misticismo); }
 
 }
