0fcc523755c86a71dcced8f2fd0f7896a1a9f779
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-05-20 17:46:59 +0200

    ROSSO: testAttaccoNegromante non riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/classi_generiche/Ruolo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/classi_generiche/Ruolo.java
@@ -329,6 +329,8 @@ public class Ruolo
 
     public void annullaSegnalazioneOratore() { setSegnalazioneOratore(false); }
 
+    public EsitoAttacco attaccoNegromante() { return null; }
+
     private void setSegnalazioneOratore(boolean segnalatoOratore) { this.segnalatoOratore = segnalatoOratore; }
 
     private boolean isVittoriaAmanti(Partita partita) { return isRomeo() && partita.isGiuliettaViva(); }
