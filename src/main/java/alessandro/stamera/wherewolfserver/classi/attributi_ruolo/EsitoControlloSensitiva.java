553c1f0cb2e034e3c8bd619702bea87783b5471c
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-05-21 11:51:16 +0200

    VERDE: testStringaSensitiva riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/attributi_ruolo/EsitoControlloSensitiva.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/attributi_ruolo/EsitoControlloSensitiva.java
@@ -1,4 +1,14 @@
 package alessandro.stamera.wherewolfserver.classi.attributi_ruolo;
 
-public enum EsitoControlloSensitiva {
+public enum EsitoControlloSensitiva
+{
+
+    VILLAGGIO("Villaggio"), NON_VILLAGGIO("Non villaggio");
+
+    private final String messaggio;
+
+    EsitoControlloSensitiva(String messaggio) { this.messaggio = messaggio; }
+
+    @Override public String toString() { return messaggio; }
+
 }
