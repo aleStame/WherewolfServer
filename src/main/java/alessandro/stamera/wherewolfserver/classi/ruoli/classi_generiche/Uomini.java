607b96642375fa4489dd0de1f0fb395531f9a313
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-05-26 22:42:47 +0200

    VERDE: testCategoria riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/classi_generiche/Uomini.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/classi_generiche/Uomini.java
@@ -1,7 +1,9 @@
 package alessandro.stamera.wherewolfserver.classi.ruoli.classi_generiche;
 
 import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Aura;
+import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Categoria;
 import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Fazione;
+import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Categoria.UOMINI;
 
 public class Uomini extends Ruolo
 {
@@ -10,5 +12,7 @@ public class Uomini extends Ruolo
     {
         super(nome, fazione, aura, descrizione, lune, mistico);
     }
+
+    @Override public Categoria getCategoria() { return UOMINI; }
 
 }
--- src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/classi_generiche/TestUomini.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/classi_generiche/TestUomini.java
@@ -3,11 +3,17 @@ package alessandro.stamera.wherewolfserver.classi.ruoli.classi_generiche;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Categoria.UOMINI;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.mock;
 
 public final class TestUomini
 {
 
-    @Test public void testCategoria() { assertThat(mock(Uomini.class).getCategoria()).isEqualTo(UOMINI); }
+    @Test public void testCategoria()
+    {
+        Uomini ruolo = mock(Uomini.class);
+        doCallRealMethod().when(ruolo).getCategoria();
+        assertThat(ruolo.getCategoria()).isEqualTo(UOMINI);
+    }
 
 }
