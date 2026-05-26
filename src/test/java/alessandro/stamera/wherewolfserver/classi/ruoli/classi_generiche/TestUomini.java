84c2b09053ac31f7fcb6dda1641b4d6e52373727
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-05-27 00:08:28 +0200

    REFACTORING: mocking dei test

--- src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/classi_generiche/TestUomini.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/classi_generiche/TestUomini.java
@@ -3,32 +3,32 @@ package alessandro.stamera.wherewolfserver.classi.ruoli.classi_generiche;
 import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoPartita;
 import alessandro.stamera.wherewolfserver.classi.gestione_partita.Partita;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.stream.Stream;
-
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Categoria.UOMINI;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoPartita.*;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoPartita.SCONFITTA;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.mock;
 
-public final class TestUomini
+@ExtendWith(MockitoExtension.class) public final class TestUomini
 {
+
+    @Mock Uomini ruolo;
 
     @Test public void testCategoria()
     {
-        Uomini ruolo = mock(Uomini.class);
         doCallRealMethod().when(ruolo).getCategoria();
         assertThat(ruolo.getCategoria()).isEqualTo(UOMINI);
     }
 
     @ParameterizedTest @MethodSource("getEsempiEsitiPartita") public void testEsitoPartita(Partita partita, EsitoPartita esito)
     {
-        Uomini ruolo = mock(Uomini.class);
         doCallRealMethod().when(ruolo).getEsitoPartita(partita);
         assertThat(ruolo.getEsitoPartita(partita)).isEqualTo(esito);
     }
