83fc47bd6a28dcdef625939818e9a0fb774e57d1
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-05-27 16:46:34 +0200

    VERDE: testEsitoPartita non riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/classi_generiche/CreatureOmbra.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/classi_generiche/CreatureOmbra.java
@@ -2,8 +2,12 @@ package alessandro.stamera.wherewolfserver.classi.ruoli.classi_generiche;
 
 import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Aura;
 import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Categoria;
+import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoPartita;
 import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Fazione;
+import alessandro.stamera.wherewolfserver.classi.gestione_partita.Partita;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Categoria.CREATURE_OMBRA;
+import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoPartita.NON_FINITO;
+import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoPartita.SCONFITTA;
 
 public class CreatureOmbra extends Ruolo
 {
@@ -14,5 +18,12 @@ public class CreatureOmbra extends Ruolo
     }
 
     @Override public Categoria getCategoria() { return CREATURE_OMBRA; }
+
+    @Override public EsitoPartita getEsitoPartita(Partita partita)
+    {
+        EsitoPartita esito = super.getEsitoPartita(partita);
+        if(esito == NON_FINITO && partita.isNoCreatureOmbra()) esito = SCONFITTA;
+        return esito;
+    }
 
 }
