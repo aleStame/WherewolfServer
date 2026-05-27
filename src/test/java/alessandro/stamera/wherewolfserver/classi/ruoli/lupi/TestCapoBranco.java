7c9df0e1ecbd711f87d81058f49de60620f6fe0e
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-05-27 17:28:26 +0200

    ROSSO: testEsitoPartita non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/lupi/TestCapoBranco.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/lupi/TestCapoBranco.java
@@ -1,15 +1,24 @@
 package alessandro.stamera.wherewolfserver.classi.ruoli.lupi;
 
 import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoAttacco;
+import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoPartita;
 import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Tratto;
+import alessandro.stamera.wherewolfserver.classi.gestione_partita.Partita;
 import alessandro.stamera.wherewolfserver.classi.ruoli.classi_generiche.Ruolo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoAttacco.MORTO;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoAttacco.RIUSCITO;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoAttacco.FALLITO;
+import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoPartita.SCONFITTA;
+import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoPartita.VITTORIA;
 import static alessandro.stamera.wherewolfserver.classi.gestione_partita.Partita.FACTORY;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -54,6 +63,26 @@ public final class TestCapoBranco
     public void testAttaccoAltriLupi(String nome) { verificaAttaccoLupi(FACTORY.getRuolo(nome), FALLITO); }
 
     @Test public void testGildata() { verificaAttacco(ruolo.gildata(), MORTO); }
+
+    @ParameterizedTest @MethodSource("getEsempiEsitoPartita")
+    public void testEsitoPartita(Partita partita, EsitoPartita esito) { assertThat(ruolo.getEsitoPartita(partita)).isEqualTo(esito); }
+
+    private static Stream<Arguments> getEsempiEsitoPartita()
+    {
+        String[] capoBranco = { "Luisa", "Capo branco" }, lupoBranco = { "Maria", "Lupo del branco" }, lupoReietto = { "Mario", "Lupo reietto" };
+        Partita[] partite = new Partita[]
+        {
+            new Partita(new String[][] { capoBranco, lupoReietto }), new Partita(new String[][] { lupoBranco, lupoReietto }),
+            new Partita(new String[][] { { "Nathan", "Giovane lupo" }, lupoReietto }),
+            new Partita(new String[][] { { "Giancarlo", "Lupo solitario" }, lupoReietto }), new Partita(new String[][] { capoBranco, lupoBranco })
+
+        };
+        return Stream.of
+        (
+            Arguments.of(partite[0], VITTORIA), Arguments.of(partite[1], VITTORIA), Arguments.of(partite[2], SCONFITTA),
+            Arguments.of(partite[3], SCONFITTA), Arguments.of(partite[4], VITTORIA)
+        );
+    }
 
     private void verificaAttaccoLupi(Ruolo lupo, EsitoAttacco esito) { verificaAttacco(ruolo.attaccoLupi(lupo), esito); }
 
