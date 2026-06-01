5a9388062a2b280bdffea0be53cffaaafd7ffc98
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-01 16:41:56 +0200

    VERDE DIRETTO: testNonFazioneNosferatu riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestGiocatoriVivi.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestGiocatoriVivi.java
@@ -724,6 +724,24 @@ public final class TestGiocatoriVivi
         verificaVero(giocatori.isFazioneNosferatu(nome));
     }
 
+    @ParameterizedTest @CsvSource
+    (
+        {
+            "Altra guardia", "Angelo custode", "Assassino", "Azzeccagarbugli", "Bardo", "Becchino", "Bocca di rosa", "Boia", "Borgomastro",
+            "Bracconiere", "Cacciatore", "Cacciatore di vampiri", "Capo branco", "Capo gilda", "Cappuccetto rosso", "Contadino eroe",
+            "Contadino discendente dei lupi", "Contadino mostro", "Contadino normale", "Eremita", "Ghoul", "Giovane lupo", "Giulietta", "Giullare",
+            "Goblin", "Guardia", "Guardia corrotta,", "Guaritore", "Inquisitore", "Ladra", "Leprecauno", "Lupo del branco", "Lupo reietto",
+            "Lupo solitario", "Mago", "Medium", "Megera", "Mercante", "Monaco", "Negromante", "Nonna", "Oratore", "Oste", "Pazzo", "Peccatore",
+            "Posseduto", "Prete", "Sidhe", "Spia", "Sensitiva", "Templare"
+        }
+    )
+    public void testNonFazioneNosferatu(String nomeRuolo)
+    {
+        String nome = "Gioele";
+        aggiungiGiocatore(nome, nomeRuolo);
+        verificaFalso(giocatori.isFazioneNosferatu(nome));
+    }
+
     private boolean isAmatoPresente() { return giocatori.isAmatoPresente(); }
 
     private boolean isLupoBrancoPresente() { return giocatori.isLupoBrancoPresente(); }
