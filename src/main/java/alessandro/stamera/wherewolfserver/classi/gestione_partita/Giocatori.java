21827a3304f07e0789b5c09fb66326708713d6e1
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-02 16:32:52 +0200

    VERDE: testNonRomeo riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/Giocatori.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/Giocatori.java
@@ -94,9 +94,9 @@ public class Giocatori
 
     public boolean isContadinoMostro(String nome) { return getRuolo(nome).isContadinoMostro(); }
 
-    public void romeizzazione(String nome) { }
+    public void romeizzazione(String nome) { getRuolo(nome).romeizzazione(); }
 
-    public boolean isRomeo(String nome) { return true; }
+    public boolean isRomeo(String nome) { return getRuolo(nome).isRomeo(); }
 
     private int getPosizioneContadinoMostro()
     {
