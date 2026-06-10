5c5a5b7d61a788d25ea0f09d8505e2bb0b732e94
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-10 17:25:18 +0200

    VERDE: testEsitoPartitaDopoAttacco riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/contadini/ContadinoLupo.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/contadini/ContadinoLupo.java
@@ -2,6 +2,9 @@ package alessandro.stamera.wherewolfserver.classi.ruoli.contadini;
 
 import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoAttacco;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoAttacco.*;
+import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.EsitoPartita;
+import alessandro.stamera.wherewolfserver.classi.fazioni.Lupo;
+import alessandro.stamera.wherewolfserver.classi.gestione_partita.Partita;
 import alessandro.stamera.wherewolfserver.classi.ruoli.classi_generiche.Ruolo;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Fazione.LUPO_BRANCO;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Tratto.CREATURA_OMBRA;
@@ -29,6 +32,13 @@ public final class ContadinoLupo extends Contadino
     }
 
     @Override public boolean isLupo() { return isTrattoPresente(LUPO_MANNARO); }
+
+    @Override public EsitoPartita getEsitoPartita(Partita partita)
+    {
+        EsitoPartita esito = super.getEsitoPartita(partita);
+        if(isLupo()) esito = new Lupo(getNome(), getDescrizione(), getLune()).getEsitoPartita(partita);
+        return esito;
+    }
 
     public static Ruolo getInstance() { return new ContadinoLupo(); }
 
