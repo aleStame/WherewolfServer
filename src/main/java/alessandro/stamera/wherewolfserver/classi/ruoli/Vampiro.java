07d7ae4ef7e813010ade2d11437e9b03f1e6a89f
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-13 11:05:45 +0200

    VERDE: testNome riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/Vampiro.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/Vampiro.java
@@ -1,5 +1,13 @@
 package alessandro.stamera.wherewolfserver.classi.ruoli;
 
-public final class Vampiro
+import alessandro.stamera.wherewolfserver.classi.ruoli.classi_generiche.CreatureOmbra;
+import alessandro.stamera.wherewolfserver.classi.ruoli.classi_generiche.Ruolo;
+
+public final class Vampiro extends CreatureOmbra
 {
+
+    private Vampiro() { super("Vampiro", null, null, null, 0, true); }
+
+    public static Ruolo getInstance() { return new Vampiro(); }
+
 }
