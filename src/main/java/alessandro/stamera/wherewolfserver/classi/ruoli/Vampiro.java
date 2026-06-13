4f05856c2cd6cad775884eb771fb5833ca7bc9a7
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-13 17:51:02 +0200

    VERDE: testLune riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/Vampiro.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/Vampiro.java
@@ -15,7 +15,7 @@ public final class Vampiro extends CreatureOmbra
 "La prima notte riconosce il Ghoul. Dalla seconda notte, può indicare un giocatore che viene avvisato. Se è il Cacciatore di vampiri o " +
           "un lupo, il Vampiro viene ucciso. Se è un mistico, non accade nulla. Altrimenti, quel giocatore lo riconosce e diventa una progenie " +
           "vampirica con aura oscura e fazione Vampiro. Inoltre, la prima notte individua la Megera.",
-     0, true
+     2, true
         );
     }
 
