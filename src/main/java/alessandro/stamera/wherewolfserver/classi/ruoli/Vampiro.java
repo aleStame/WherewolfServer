34a22298ee0f9ecbb4164768ff2986f6fb47f2df
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-13 11:49:13 +0200

    VERDE: testFazione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/Vampiro.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/ruoli/Vampiro.java
@@ -2,6 +2,7 @@ package alessandro.stamera.wherewolfserver.classi.ruoli;
 
 import alessandro.stamera.wherewolfserver.classi.ruoli.classi_generiche.CreatureOmbra;
 import alessandro.stamera.wherewolfserver.classi.ruoli.classi_generiche.Ruolo;
+import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.Fazione.VAMPIRO;
 
 public final class Vampiro extends CreatureOmbra
 {
@@ -10,7 +11,7 @@ public final class Vampiro extends CreatureOmbra
     {
         super
         (
-    "Vampiro", null, null,
+    "Vampiro", VAMPIRO, null,
 "La prima notte riconosce il Ghoul. Dalla seconda notte, può indicare un giocatore che viene avvisato. Se è il Cacciatore di vampiri o " +
           "un lupo, il Vampiro viene ucciso. Se è un mistico, non accade nulla. Altrimenti, quel giocatore lo riconosce e diventa una progenie " +
           "vampirica con aura oscura e fazione Vampiro. Inoltre, la prima notte individua la Megera.",
