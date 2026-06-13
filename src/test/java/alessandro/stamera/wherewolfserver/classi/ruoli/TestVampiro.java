5f295aee0869b8890fb6669c20f61827cdb6543b
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-13 11:17:00 +0200

    ROSSO: testDescrizione non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/TestVampiro.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/TestVampiro.java
@@ -11,4 +11,13 @@ public final class TestVampiro
 
     @Test public void testNome() { assertThat(FACTORY.getRuolo(NOME).getNome()).isEqualTo(NOME); }
 
+    @Test public void testDescrizione()
+    {
+        String descrizione =
+            "La prima notte riconosce il Ghoul. Dalla seconda notte, può indicare un giocatore che viene avvisato. Se è il Cacciatore di vampiri " +
+            "o un lupo, il Vampiro viene ucciso. Se è un mistico, non accade nulla. Altrimenti, quel giocatore lo riconosce e diventa una " +
+            "progenie vampirica con aura oscura e fazione Vampiro. Inoltre, la prima notte individua la Megera.";
+        assertThat(FACTORY.getRuolo(NOME).getDescrizione()).isEqualTo(descrizione);
+    }
+
 }
