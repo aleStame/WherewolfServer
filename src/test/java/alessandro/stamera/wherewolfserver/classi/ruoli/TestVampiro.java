3e461f791fafefe9a456a6d0544eba35a723a214
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-13 11:24:26 +0200

    REFACTORING: definizione di un metodo di set up

--- src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/TestVampiro.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/TestVampiro.java
@@ -1,5 +1,7 @@
 package alessandro.stamera.wherewolfserver.classi.ruoli;
 
+import alessandro.stamera.wherewolfserver.classi.ruoli.classi_generiche.Ruolo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.gestione_partita.Partita.FACTORY;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,7 +11,11 @@ public final class TestVampiro
 
     private static final String NOME = "Vampiro";
 
-    @Test public void testNome() { assertThat(FACTORY.getRuolo(NOME).getNome()).isEqualTo(NOME); }
+    private Ruolo ruolo;
+
+    @BeforeEach public void setUp() { ruolo = FACTORY.getRuolo(NOME); }
+
+    @Test public void testNome() { assertThat(ruolo.getNome()).isEqualTo(NOME); }
 
     @Test public void testDescrizione()
     {
@@ -17,7 +23,7 @@ public final class TestVampiro
             "La prima notte riconosce il Ghoul. Dalla seconda notte, può indicare un giocatore che viene avvisato. Se è il Cacciatore di vampiri " +
             "o un lupo, il Vampiro viene ucciso. Se è un mistico, non accade nulla. Altrimenti, quel giocatore lo riconosce e diventa una " +
             "progenie vampirica con aura oscura e fazione Vampiro. Inoltre, la prima notte individua la Megera.";
-        assertThat(FACTORY.getRuolo(NOME).getDescrizione()).isEqualTo(descrizione);
+        assertThat(ruolo.getDescrizione()).isEqualTo(descrizione);
     }
 
 }
