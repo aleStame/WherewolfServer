6050e4f8b3ac52699a7bef9debe72041e9b2b7c0
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-22 14:46:26 +0200

    VERDE: testAttaccoVampiro non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestPartita.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestPartita.java
@@ -1099,9 +1099,8 @@ public final class TestPartita
         {
             "Altra guardia", "Angelo custode", "Assassino", "Azzeccagarbugli", "Bardo", "Becchino", "Bocca di rosa", "Boia", "Borgomastro",
             "Bracconiere", "Cacciatore", "Cappuccetto rosso", "Contadino eroe", "Contadino discendente dei lupi", "Contadino normale", "Ghoul",
-            "Giulietta", "Giullare", "Goblin", "Guardia", "Guardia corrotta", "Guaritore", "Inquisitore", "Ladra", "Leprecauno", "Mago", "Medium",
-            "Megera", "Mercante", "Monaco", "Negromante", "Nonna", "Nosferatu", "Oratore", "Oste", "Pazzo", "Peccatore", "Posseduto", "Prete",
-            "Sidhe", "Spia", "Sensitiva", "Templare", "Vampiro"
+            "Giulietta", "Giullare", "Guardia", "Guardia corrotta", "Inquisitore", "Ladra", "Mercante", "Monaco", "Nonna", "Nosferatu", "Oratore",
+            "Oste", "Pazzo", "Peccatore", "Posseduto", "Prete", "Spia", "Templare", "Vampiro"
         }
     )
     public void testCriminalizzazioneProgenieVampiro(String nomeRuolo)
