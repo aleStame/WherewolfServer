5c31e135c751040a2ee71a41d057189bda3aae57
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-29 23:31:52 +0200

    VERDE: testAvvisoMorteAngeloCustode riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneProgenizzazioneNonRiuscita.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneProgenizzazioneNonRiuscita.java
@@ -3,9 +3,16 @@ package alessandro.stamera.wherewolfserver.classi.eccezioni;
 public final class EccezioneProgenizzazioneNonRiuscita extends IllegalStateException
 {
 
-    public EccezioneProgenizzazioneNonRiuscita(String nomeCacciatoreDiVampiri, String nomeAngeloCustode, String nomeVampiroAmato)
+    public EccezioneProgenizzazioneNonRiuscita
+    (
+        String nomeCacciatoreDiVampiri, String nomeAngeloCustode, String nomeVampiroAmato
+    )
     {
-
+        super
+        (
+            "Il tentativo di vampirizzazione del Cacciatore di vampiri (" + nomeCacciatoreDiVampiri + ") causa la morte dell'Angelo custode (" +
+            nomeAngeloCustode + ") del Vampiro amato (" + nomeVampiroAmato + ").\nAvvisa " + nomeAngeloCustode + " della sua morte."
+        );
     }
 
 }
--- src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneProgenizzazioneNonRiuscita.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneProgenizzazioneNonRiuscita.java
@@ -10,7 +10,7 @@ public final class TestEccezioneProgenizzazioneNonRiuscita
     {
         String messaggio =
             "Il tentativo di vampirizzazione del Cacciatore di vampiri (Aldo) causa la morte dell'Angelo custode (Giovanni) del Vampiro amato (" +
-            "Giacomo).\nAvvisa Francesco della sua morte.";
+            "Giacomo).\nAvvisa Giovanni della sua morte.";
         EccezioneProgenizzazioneNonRiuscita eccezione =
             new EccezioneProgenizzazioneNonRiuscita("Aldo", "Giovanni", "Giacomo");
         assertThat(eccezione.getMessage()).isEqualTo(messaggio);
