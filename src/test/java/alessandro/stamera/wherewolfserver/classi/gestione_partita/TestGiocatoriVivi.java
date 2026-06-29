39b679262b21bad10d74b960c2d080983cb19a84
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-29 16:31:41 +0200

    VERDE DIRETTO: testAttaccoAmatoRomeoAssassino riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestGiocatoriVivi.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestGiocatoriVivi.java
@@ -110,6 +110,27 @@ public final class TestGiocatoriVivi
         inizializzaGiocatori(new String[][] { { "Enzo", "Angelo custode" }, { "Barbara", "Assassino" }, { nomeVittima, nomeRuolo } });
         segnalazioneAngeloCustode(nomeVittima);
         verificaAttaccoAssassino(nomeVittima, ANGELO_CUSTODE_MORTO);
+        ripristina(nomeVittima);
+    }
+
+    @ParameterizedTest @CsvSource
+    (
+        {
+            "Altra guardia", "Azzeccagarbugli", "Bardo", "Becchino", "Bocca di rosa", "Boia", "Borgomastro", "Bracconiere", "Cacciatore",
+            "Cacciatore di vampiri", "Capo branco", "Capo gilda", "Cappuccetto rosso", "Contadino eroe", "Contadino discendente dei lupi",
+            "Contadino normale", "Eremita", "Ghoul", "Giovane lupo", "Giulietta", "Giullare", "Goblin", "Guardia", "Guardia corrotta", "Guaritore", "Inquisitore", "Ladra", "Leprecauno", "Lupo del branco", "Lupo reietto", "Lupo solitario", "Mago",
+            "Medium", "Megera", "Mercante", "Monaco", "Negromante", "Nonna", "Nosferatu", "Oratore", "Oste", "Pazzo", "Peccatore", "Posseduto",
+            "Prete", "Sidhe", "Spia", "Sensitiva", "Templare", "Vampiro"
+        }
+    )
+    public void testAttaccoAmatoRomeoAssassino(String nomeRuolo)
+    {
+        String nomeVittima = "Maddalena";
+        inizializzaGiocatori(new String[][] { { "Enzo", "Angelo custode" }, { "Barbara", "Assassino" }, { nomeVittima, nomeRuolo } });
+        giocatori.romeizzazione(nomeVittima);
+        segnalazioneAngeloCustode(nomeVittima);
+        verificaAttaccoAssassino(nomeVittima, ANGELO_CUSTODE_MORTO);
+        ripristina(nomeVittima);
     }
 
     @Test public void testSegnalazioneAzzeccagarbugliAngeloCustode()
