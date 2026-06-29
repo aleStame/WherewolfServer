9d14feba0c4e4830af8363a2583c1963a4b46165
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-29 17:21:30 +0200

    VERDE DIRETTO: testAttaccoAmatoAssassino riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestPartita.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestPartita.java
@@ -100,24 +100,23 @@ public final class TestPartita
     @ParameterizedTest @CsvSource
     (
         {
-            "Altra guardia", "Azzeccagarbugli", "Bardo", "Becchino", "Bocca di rosa", "Borgomastro", "Bracconiere", "Cacciatore",
+            "Altra guardia", "Azzeccagarbugli", "Bardo", "Becchino", "Bocca di rosa", "Boia", "Borgomastro", "Bracconiere", "Cacciatore",
             "Cacciatore di vampiri", "Capo branco", "Capo gilda", "Cappuccetto rosso", "Contadino eroe", "Contadino discendente dei lupi",
-            "Contadino normale", "Eremita", "Ghoul", "Giovane lupo", "Giulietta", "Giullare", "Goblin", "Guardia", "Guardia corrotta", "Guaritore",
-            "Inquisitore", "Ladra", "Leprecauno", "Lupo del branco", "Lupo reietto", "Lupo solitario", "Mago", "Medium", "Megera", "Mercante",
-            "Monaco", "Negromante", "Nonna", "Nosferatu", "Oratore", "Oste", "Pazzo", "Peccatore", "Posseduto", "Prete", "Sidhe", "Spia",
-            "Sensitiva", "Templare", "Vampiro"
+            "Contadino normale", "Eremita", "Ghoul", "Giovane lupo", "Giulietta", "Giullare", "Goblin", "Guardia", "Guardia corrotta", "Guaritore", "Inquisitore", "Ladra", "Leprecauno", "Lupo del branco", "Lupo reietto", "Lupo solitario", "Mago",
+            "Medium", "Megera", "Mercante", "Monaco", "Negromante", "Nonna", "Nosferatu", "Oratore", "Oste", "Pazzo", "Peccatore", "Posseduto",
+            "Prete", "Sidhe", "Spia", "Sensitiva", "Templare", "Vampiro"
         }
     )
     public void testAttaccoAmatoAssassino(String nomeRuolo)
     {
-        String nomeAngelo = "Enzo", nomeAmato = "Maddalena";
-        inizializzaPartita(new String[][] { { nomeAngelo, "Angelo custode" }, { "Barbara", "Assassino" }, { nomeAmato, nomeRuolo } });
-        segnalazioneAngeloCustode(nomeAmato);
-        attaccoAssassino(nomeAmato);
+        String nomeAngelo = "Enzo", nomeVittima = "Maddalena";
+        inizializzaPartita(new String[][] { { nomeAngelo, "Angelo custode" }, { "Barbara", "Assassino" }, { nomeVittima, nomeRuolo } });
+        segnalazioneAngeloCustode(nomeVittima);
+        attaccoAssassino(nomeVittima);
         terminaNotte();
         verificaEliminazione(nomeAngelo);
-        verificaNonEliminati(nomeAmato);
-        ripristinaGiocatoreVivo(nomeAmato);
+        verificaNonEliminati(nomeVittima);
+        ripristinaGiocatoreVivo(nomeVittima);
     }
 
     @ParameterizedTest @CsvSource
