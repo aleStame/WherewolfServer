8967176efc6786419c20c224b7ff8439971de757
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-06-29 23:57:55 +0200

    REFACTORING: definizione del metodo verificaMessaggio

--- src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneProgenizzazioneNonRiuscita.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneProgenizzazioneNonRiuscita.java
@@ -6,23 +6,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class TestEccezioneProgenizzazioneNonRiuscita
 {
 
+    private EccezioneProgenizzazioneNonRiuscita eccezione;
+
     @Test public void testAvvisoMorteAngeloCustode()
     {
         String messaggio =
             "Il tentativo di vampirizzazione del Cacciatore di vampiri (Aldo) causa la morte dell'Angelo custode (Giovanni) del Vampiro amato (" +
             "Giacomo).\nAvvisa Giovanni della sua morte.";
-        EccezioneProgenizzazioneNonRiuscita eccezione =
-            new EccezioneProgenizzazioneNonRiuscita("Aldo", "Giovanni", "Giacomo");
-        assertThat(eccezione.getMessage()).isEqualTo(messaggio);
+        eccezione = new EccezioneProgenizzazioneNonRiuscita("Aldo", "Giovanni", "Giacomo");
+        verificaMessaggio(messaggio);
     }
 
     @Test public void testAvvisoMorteGhoul()
     {
         String messaggio =
             "Il tentativo di vampirizzazione del Cacciatore di vampiri (Katia) causa la morte del Ghoul (Valeria).\nAvvisa Valeria della sua morte";
-        EccezioneProgenizzazioneNonRiuscita eccezione =
-            new EccezioneProgenizzazioneNonRiuscita("Katia", "Valeria");
-        assertThat(eccezione.getMessage()).isEqualTo(messaggio);
+        eccezione = new EccezioneProgenizzazioneNonRiuscita("Katia", "Valeria");
+        verificaMessaggio(messaggio);
     }
+
+    private void verificaMessaggio(String messaggio) { assertThat(eccezione.getMessage()).isEqualTo(messaggio); }
 
 }
