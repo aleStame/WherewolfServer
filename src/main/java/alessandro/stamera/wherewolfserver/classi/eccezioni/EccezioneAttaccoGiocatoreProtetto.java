b1dfd0aabebaa37d82a7c9802d89b9ab761d96e1
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-01 15:38:26 +0200

    VERDE: testErroreAttaccoAmato riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneAttaccoGiocatoreProtetto.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneAttaccoGiocatoreProtetto.java
@@ -3,6 +3,13 @@ package alessandro.stamera.wherewolfserver.classi.eccezioni;
 public class EccezioneAttaccoGiocatoreProtetto extends IllegalStateException
 {
 
-    public EccezioneAttaccoGiocatoreProtetto(boolean isRomeo, String vittima) { }
+    private final String nomeVittima;
+
+    public EccezioneAttaccoGiocatoreProtetto(boolean isRomeo, String nomeVittima) { this.nomeVittima = nomeVittima; }
+
+    @Override public String getMessage()
+    {
+        return nomeVittima + " non muore perché protetto dalla Strega.\nAvvisa i lupi della sua mancata morte.";
+    }
 
 }
