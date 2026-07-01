564701d7245278ad3a4e595ec7e683d11d873be9
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-01 16:36:44 +0200

    REFACTORING: parametrizzazione dei test

--- src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneAttaccoGiocatoreProtetto.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneAttaccoGiocatoreProtetto.java
@@ -1,30 +1,23 @@
 package alessandro.stamera.wherewolfserver.classi.eccezioni;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestEccezioneAttaccoGiocatoreProtetto
 {
 
-    private EccezioneAttaccoGiocatoreProtetto eccezione;
-
-    @Test public void testErroreAttaccoProtetto()
+    @ParameterizedTest @CsvSource
+    (
+        {
+            "false, Gianluigi, 'Gianluigi non muore perché protetto dalla Strega.\nAvvisa i lupi della sua mancata morte.'",
+            "true, Antongiulio, 'Antongiulio non muore perché Romeo.\nAvvisa i lupi della sua mancata morte.'"
+        }
+    )
+    public void testMessaggioErrore(boolean isRomeo, String nomeVittima, String messaggio)
     {
-        generaEccezione(false, "Gianluigi");
-        verificaMessaggio("Gianluigi non muore perché protetto dalla Strega.\nAvvisa i lupi della sua mancata morte.");
+        EccezioneAttaccoGiocatoreProtetto eccezione = new EccezioneAttaccoGiocatoreProtetto(isRomeo, nomeVittima);
+        assertThat(eccezione.getMessage()).isEqualTo(messaggio);
     }
-
-    @Test public void testErroreAttaccoRomeo()
-    {
-        generaEccezione(true, "Antongiulio");
-        verificaMessaggio("Antongiulio non muore perché Romeo.\nAvvisa i lupi della sua mancata morte.");
-    }
-
-    private void generaEccezione(boolean isRomeo, String nomeVittima)
-    {
-        eccezione = new EccezioneAttaccoGiocatoreProtetto(isRomeo, nomeVittima);
-    }
-
-    private void verificaMessaggio(String messaggio) { assertThat(eccezione.getMessage()).isEqualTo(messaggio); }
 
 }
