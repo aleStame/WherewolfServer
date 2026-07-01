09286d2330ebf59279e257c61fb32da5ece7bc55
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-01 15:41:50 +0200

    ROSSO: testErroreAttaccoRomeo non riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneAttaccoGiocatoreProtetto.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneAttaccoGiocatoreProtetto.java
@@ -6,10 +6,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class TestEccezioneAttaccoGiocatoreProtetto
 {
 
-    @Test public void testErroreAttaccoAmato()
+    @Test public void testErroreAttaccoProtetto()
     {
         EccezioneAttaccoGiocatoreProtetto eccezione = new EccezioneAttaccoGiocatoreProtetto(false, "Gianluigi");
         String messaggio = "Gianluigi non muore perché protetto dalla Strega.\nAvvisa i lupi della sua mancata morte.";
+        assertThat(eccezione.getMessage()).isEqualTo(messaggio);
+    }
+
+    @Test public void testErroreAttaccoRomeo()
+    {
+        EccezioneAttaccoGiocatoreProtetto eccezione = new EccezioneAttaccoGiocatoreProtetto(true, "Antongiulio");
+        String messaggio = "Antongiulio non muore perché Romeo.\nAvvisa i lupi della sua mancata morte.";
         assertThat(eccezione.getMessage()).isEqualTo(messaggio);
     }
 
