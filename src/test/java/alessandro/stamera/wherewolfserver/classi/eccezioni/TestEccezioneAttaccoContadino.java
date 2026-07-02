765ee4841bb0ed886ddcab770a42b36ea2765134
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-02 13:42:10 +0200

    VERDE DIRETTO: testContadinoMostro riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneAttaccoContadino.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneAttaccoContadino.java
@@ -2,6 +2,7 @@ package alessandro.stamera.wherewolfserver.classi.eccezioni;
 
 import org.junit.jupiter.api.Test;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.TipoContadino.EROE;
+import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.TipoContadino.MOSTRO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestEccezioneAttaccoContadino
@@ -12,6 +13,14 @@ public final class TestEccezioneAttaccoContadino
         EccezioneAttaccoContadino eccezione = new EccezioneAttaccoContadino(EROE, "Nadia", "Elena");
         String messaggio =
             "L'attacco al Contadino eroe (Nadia) causa la morte anche del lupo attaccante (Elena).\nAvvisa entrambi i giocatori della loro morte.";
+        assertThat(eccezione.getMessage()).isEqualTo(messaggio);
+    }
+
+    @Test public void testContadinoMostro()
+    {
+        EccezioneAttaccoContadino eccezione = new EccezioneAttaccoContadino(MOSTRO, "Nadia", "Elena");
+        String messaggio =
+            "L'attacco al Contadino mostro (Nadia) causa la morte anche del lupo attaccante (Elena).\nAvvisa entrambi i giocatori della loro morte.";
         assertThat(eccezione.getMessage()).isEqualTo(messaggio);
     }
 
