052d49215135502a08a6b3ec5d980e3f7bee2dc0
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-02 18:11:47 +0200

    VERDE: testMessaggioErrore riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneNonnaBeccata.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneNonnaBeccata.java
@@ -3,6 +3,13 @@ package alessandro.stamera.wherewolfserver.classi.eccezioni;
 public final class EccezioneNonnaBeccata extends IllegalArgumentException
 {
 
-    public EccezioneNonnaBeccata(String nomeLupo, String tipoLupo, String nomeNonna) { }
+    public EccezioneNonnaBeccata(String nomeLupo, String tipoLupo, String nomeNonna)
+    {
+        super
+        (
+            "Il " + tipoLupo + " (" + nomeLupo + ") ha beccato la Nonna (" + nomeNonna + ").\nSveglia " + nomeNonna + " e avvisa i due giocatori " +
+            "che " + nomeLupo+ " è eliminato e che " + nomeNonna + " è il " + tipoLupo + "."
+        );
+    }
 
 }
