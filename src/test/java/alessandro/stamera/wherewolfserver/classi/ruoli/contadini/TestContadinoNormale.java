40733d8e108284d3a754d7195c74611e90b83b32
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-02 15:30:44 +0200

    VERDE DIRETTO: testTipoContadino riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/contadini/TestContadinoNormale.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/ruoli/contadini/TestContadinoNormale.java
@@ -1,5 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi.ruoli.contadini;
 
+import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.TipoContadino.NORMALE;
 import static alessandro.stamera.wherewolfserver.classi.gestione_partita.Partita.FACTORY;
 import static org.assertj.core.api.Assertions.assertThat;
 import alessandro.stamera.wherewolfserver.classi.ruoli.classi_generiche.Ruolo;
@@ -24,6 +25,8 @@ public final class TestContadinoNormale
     @Test public void testContadinoEroe() { verificaFalso(ruolo.isContadinoMostro()); }
 
     @Test public void testContadinoLupo() { verificaFalso(ruolo.isContadinoLupo()); }
+
+    @Test public void testTipoContadino() { assertThat(ruolo.getTipoContadino()).isEqualTo(NORMALE); }
 
     private void verificaVero(boolean valore) { assertThat(valore).isTrue(); }
 
