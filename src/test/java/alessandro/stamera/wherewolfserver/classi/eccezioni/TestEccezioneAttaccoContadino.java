6f7fcce728eaa1793ba82de0d52315286dd9fa0e
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-02 13:53:20 +0200

    REFACTORING: compattazione dei due test in un unico test parametrico

--- src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneAttaccoContadino.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneAttaccoContadino.java
@@ -1,6 +1,10 @@
 package alessandro.stamera.wherewolfserver.classi.eccezioni;
 
-import org.junit.jupiter.api.Test;
+import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.TipoContadino;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.TipoContadino.EROE;
 import static alessandro.stamera.wherewolfserver.classi.attributi_ruolo.TipoContadino.MOSTRO;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,20 +12,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class TestEccezioneAttaccoContadino
 {
 
-    @Test public void testContadinoEroe()
+    @ParameterizedTest @MethodSource("getEsempiAttacchi")
+    public void testAttaccoContadino(TipoContadino tipoContadino, String messaggio)
     {
-        EccezioneAttaccoContadino eccezione = new EccezioneAttaccoContadino(EROE, "Nadia", "Elena");
-        String messaggio =
-            "L'attacco al Contadino eroe (Nadia) causa la morte anche del lupo attaccante (Elena).\nAvvisa entrambi i giocatori della loro morte.";
-        assertThat(eccezione.getMessage()).isEqualTo(messaggio);
+        assertThat(new EccezioneAttaccoContadino(tipoContadino, "Nadia", "Elena").getMessage()).isEqualTo(messaggio);
     }
 
-    @Test public void testContadinoMostro()
+    private static Stream<Arguments> getEsempiAttacchi()
     {
-        EccezioneAttaccoContadino eccezione = new EccezioneAttaccoContadino(MOSTRO, "Nadia", "Elena");
-        String messaggio =
-            "L'attacco al Contadino mostro (Nadia) causa la morte anche del lupo attaccante (Elena).\nAvvisa entrambi i giocatori della loro morte.";
-        assertThat(eccezione.getMessage()).isEqualTo(messaggio);
+        return Stream.of
+        (
+            Arguments.of
+            (
+    EROE,
+               "L'attacco al Contadino eroe (Nadia) causa la morte anche del lupo attaccante (Elena).\nAvvisa entrambi i giocatori della loro morte."
+            ),
+            Arguments.of
+            (
+                MOSTRO,
+                "L'attacco al Contadino mostro (Nadia) causa la morte anche del lupo attaccante (Elena).\nAvvisa entrambi i giocatori della " +
+                "loro morte."
+            )
+        );
     }
 
 }
