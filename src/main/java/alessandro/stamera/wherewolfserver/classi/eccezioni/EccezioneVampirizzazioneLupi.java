1acb8356905406aaabd78734c0a04e678454305e
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-03 12:36:13 +0200

    VERDE: testVampirizzazioneLupo riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneVampirizzazioneLupi.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneVampirizzazioneLupi.java
@@ -3,6 +3,9 @@ package alessandro.stamera.wherewolfserver.classi.eccezioni;
 public class EccezioneVampirizzazioneLupi extends IllegalArgumentException
 {
 
-    public EccezioneVampirizzazioneLupi(String tipoLupo, String nomeLupo, String nomeVampiro) { }
+    public EccezioneVampirizzazioneLupi(String tipoLupo, String nomeLupo, String nomeVampiro)
+    {
+      super("Impossibile vampirizzare il " + tipoLupo + " (" + nomeLupo + ").\nAvvisa il Vampiro (" + nomeVampiro + ") della sua morte.");
+    }
 
 }
