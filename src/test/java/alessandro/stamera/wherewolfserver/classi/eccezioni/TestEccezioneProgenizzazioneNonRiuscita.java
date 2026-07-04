f73ccc95b3ff83b30d12e31fcdad6e35adce4752
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-04 14:43:50 +0200

    REFACTORING: parametrizzazione del metodo testAvvisoMorteAngeloCustode riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneProgenizzazioneNonRiuscita.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneProgenizzazioneNonRiuscita.java
@@ -1,9 +1,10 @@
 package alessandro.stamera.wherewolfserver.classi.eccezioni;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
-
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class TestEccezioneProgenizzazioneNonRiuscita
@@ -11,15 +12,10 @@ public final class TestEccezioneProgenizzazioneNonRiuscita
 
     private EccezioneProgenizzazioneNonRiuscita eccezione;
 
-    @Test public void testAvvisoMorteAngeloCustode()
+    @ParameterizedTest @MethodSource("getEsempiPartitaMorteAngeloCustode")
+    public void testAvvisoMorteAngeloCustode(String nomeRuolo, String messaggio)
     {
-        String messaggio =
-            "Il tentativo di vampirizzazione del Cacciatore di vampiri (Aldo) causa la morte dell'Angelo custode (Giovanni) del Vampiro amato (" +
-            "Giacomo).\nAvvisa Giovanni della sua morte.";
-        eccezione = new EccezioneProgenizzazioneNonRiuscita
-        (
-"Cacciatore di vampiri", "Aldo", "Giovanni", "Giacomo"
-        );
+        eccezione = new EccezioneProgenizzazioneNonRiuscita(nomeRuolo, "Aldo", "Giovanni", "Giacomo");
         verificaMessaggio(messaggio);
     }
 
@@ -37,6 +33,24 @@ public final class TestEccezioneProgenizzazioneNonRiuscita
     {
         eccezione = new EccezioneProgenizzazioneNonRiuscita(nomeRuolo, "Katia", "Valeria");
         verificaMessaggio(messaggio);
+    }
+
+    private static Stream<Arguments> getEsempiPartitaMorteAngeloCustode()
+    {
+        String[][] stringhe =
+        {
+            {
+                "Cacciatore di vampiri",
+                "Il tentativo di vampirizzazione del Cacciatore di vampiri (Aldo) causa la morte dell'Angelo custode (Giovanni) del Vampiro amato "
+                + "(Giacomo).\nAvvisa Giovanni della sua morte."
+            },
+            {
+                "Contadino mostro",
+                "Il tentativo di vampirizzazione del Contadino mostro (Aldo) causa la morte dell'Angelo custode (Giovanni) del Vampiro amato " +
+                "(Giacomo).\nAvvisa Giovanni della sua morte."
+            }
+        };
+        return Stream.of(Arguments.of(stringhe[0][0], stringhe[0][1]), Arguments.of(stringhe[1][0], stringhe[1][1]));
     }
 
     private void verificaMessaggio(String messaggio) { assertThat(eccezione.getMessage()).isEqualTo(messaggio); }
