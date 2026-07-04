1358be427660222dba0d13c39ed1e2cda640f8e2
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-04 17:49:09 +0200

    REFACTORING: ridefinizione del metodo getMessage

--- src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneProgenizzazioneNonRiuscita.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneProgenizzazioneNonRiuscita.java
@@ -1,27 +1,53 @@
 package alessandro.stamera.wherewolfserver.classi.eccezioni;
 
+import java.util.Optional;
+
 public final class EccezioneProgenizzazioneNonRiuscita extends IllegalArgumentException
 {
+
+    private final String ruoloVittima, nomeVittima;
+
+    private final Optional<String> nomeAngeloCustode, nomeVampiroAmato, nomeGhoul;
 
     public EccezioneProgenizzazioneNonRiuscita
     (
         String ruoloVittima, String nomeVittima, String nomeAngeloCustode, String nomeVampiroAmato
     )
     {
-        super
-        (
-            "Il tentativo di vampirizzazione del " + ruoloVittima + " (" + nomeVittima + ") causa la morte dell'Angelo custode (" + nomeAngeloCustode
-            + ") del Vampiro amato (" + nomeVampiroAmato + ").\nAvvisa " + nomeAngeloCustode + " della sua morte."
-        );
+        this.ruoloVittima = ruoloVittima;
+        this.nomeVittima = nomeVittima;
+        this.nomeAngeloCustode = Optional.of(nomeAngeloCustode);
+        this.nomeVampiroAmato = Optional.of(nomeVampiroAmato);
+        this.nomeGhoul = Optional.empty();
     }
 
     public EccezioneProgenizzazioneNonRiuscita(String ruoloVittima, String nomeVittima, String nomeGhoul)
     {
-        super
-        (
-            "Il tentativo di vampirizzazione del " + ruoloVittima + " (" + nomeVittima + ") causa la morte del Ghoul (" + nomeGhoul + ").\nAvvisa " +
-            nomeGhoul + " della sua morte."
-        );
+        this.ruoloVittima = ruoloVittima;
+        this.nomeVittima = nomeVittima;
+        this.nomeAngeloCustode = Optional.empty();
+        this.nomeVampiroAmato = Optional.empty();
+        this.nomeGhoul = Optional.of(nomeGhoul);
+    }
+
+    @Override public String getMessage()
+    {
+        String messaggio;
+        if(nomeAngeloCustode.isPresent() && nomeVampiroAmato.isPresent())
+        {
+            String nomeAngeloCustode = this.nomeAngeloCustode.get();
+            messaggio =
+                "Il tentativo di vampirizzazione del " + ruoloVittima + " (" + nomeVittima + ") causa la morte dell'Angelo custode (" +
+                nomeAngeloCustode + ") del Vampiro amato (" + nomeVampiroAmato.get() + ").\nAvvisa " + nomeAngeloCustode + " della sua morte.";
+        }
+        else
+        {
+            String nomeGhoul = this.nomeGhoul.get();
+            messaggio =
+                "Il tentativo di vampirizzazione del " + ruoloVittima + " (" + nomeVittima + ") causa la morte del Ghoul (" + nomeGhoul +
+                ").\nAvvisa " + nomeGhoul + " della sua morte.";
+        }
+        return messaggio;
     }
 
 }
