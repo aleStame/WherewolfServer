25dbf955e51ba1ba515cb21a20e3eb006948b747
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-06 17:31:19 +0200

    VERDE DIRETTO: testMessaggioErrore riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneMorteProgenizzatore.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneMorteProgenizzatore.java
@@ -10,17 +10,27 @@ public final class TestEccezioneMorteProgenizzatore
     @ParameterizedTest @CsvSource
     (
         {
-            "'Capo branco', 'Impossibile vampirizzare il Capo branco (Giuliano).\nAvvisa il Vampiro (Michele) della sua morte.'",
-            "'Lupo del branco', 'Impossibile vampirizzare il Lupo del branco (Giuliano).\nAvvisa il Vampiro (Michele) della sua morte.'",
-            "'Lupo reietto', 'Impossibile vampirizzare il Lupo reietto (Giuliano).\nAvvisa il Vampiro (Michele) della sua morte.'",
-            "'Lupo solitario', 'Impossibile vampirizzare il Lupo solitario (Giuliano).\nAvvisa il Vampiro (Michele) della sua morte.'",
-            "'Contadino discendente dei lupi', 'Impossibile vampirizzare il Contadino discendente dei lupi (Giuliano).\nAvvisa il Vampiro " +
-            "(Michele) della sua morte.'"
+            "'Capo branco', Vampiro, 'Impossibile vampirizzare il Capo branco (Giuliano).\nAvvisa il Vampiro (Michele) della sua morte.'",
+            "'Capo branco', Nosferatu, 'Impossibile progenizzare il Capo branco (Giuliano).\nAvvisa il Nosferatu (Michele) della sua morte.'",
+            "'Lupo del branco', Vampiro, 'Impossibile vampirizzare il Lupo del branco (Giuliano).\nAvvisa il Vampiro (Michele) della sua morte.'",
+            "'Lupo del branco', Nosferatu, 'Impossibile progenizzare il Lupo del branco (Giuliano).\nAvvisa il Nosferatu (Michele) della sua " +
+            "morte.'",
+            "'Lupo reietto', Vampiro, 'Impossibile vampirizzare il Lupo reietto (Giuliano).\nAvvisa il Vampiro (Michele) della sua morte.'",
+            "'Lupo reietto', Nosferatu, 'Impossibile progenizzare il Lupo reietto (Giuliano).\nAvvisa il Nosferatu (Michele) della sua morte.'",
+            "'Lupo solitario', Vampiro, 'Impossibile vampirizzare il Lupo solitario (Giuliano).\nAvvisa il Vampiro (Michele) della sua morte.'",
+            "'Lupo solitario', Nosferatu, 'Impossibile progenizzare il Lupo solitario (Giuliano).\nAvvisa il Nosferatu (Michele) della sua " +
+            "morte.'",
+            "'Contadino discendente dei lupi', Vampiro, 'Impossibile vampirizzare il Contadino discendente dei lupi (Giuliano).\nAvvisa il " +
+            "Vampiro (Michele) della sua morte.'",
+            "'Contadino discendente dei lupi', Nosferatu, 'Impossibile progenizzare il Contadino discendente dei lupi (Giuliano).\nAvvisa il " +
+            "Nosferatu (Michele) della sua morte.'"
         }
     )
-    public void testVampirizzazioneLupo(String tipoLupo, String messaggio)
+    public void testMessaggioErrore(String tipoLupo, String progenizzatore, String messaggio)
     {
-        assertThat(new EccezioneMorteProgenizzatore("Vampiro", tipoLupo, "Giuliano", "Michele").getMessage()).isEqualTo(messaggio);
+        EccezioneMorteProgenizzatore eccezione =
+            new EccezioneMorteProgenizzatore(progenizzatore, tipoLupo, "Giuliano", "Michele");
+        assertThat(eccezione.getMessage()).isEqualTo(messaggio);
     }
 
 }
