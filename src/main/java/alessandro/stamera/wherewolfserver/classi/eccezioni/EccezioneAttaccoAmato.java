2bd1c8fe1d822a32949d070dfa256c3fb612202c
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-16 16:03:59 +0200

    VERDE: testMessaggio riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneAttaccoAmato.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneAttaccoAmato.java
@@ -3,6 +3,22 @@ package alessandro.stamera.wherewolfserver.classi.eccezioni;
 public final class EccezioneAttaccoAmato extends IllegalStateException
 {
 
-    public EccezioneAttaccoAmato(String tipoLupo, String nomeLupo, String ruoloAmato, String nomeAmato, String nomeAngeloCustode) { }
+    private final String tipoLupo, nomeLupo, ruoloAmato, nomeAmato, nomeAngeloCustode;
+
+    public EccezioneAttaccoAmato(String tipoLupo, String nomeLupo, String ruoloAmato, String nomeAmato, String nomeAngeloCustode)
+    {
+        this.tipoLupo = tipoLupo;
+        this.nomeLupo = nomeLupo;
+        this.ruoloAmato = ruoloAmato;
+        this.nomeAmato = nomeAmato;
+        this.nomeAngeloCustode = nomeAngeloCustode;
+    }
+
+    @Override public String getMessage()
+    {
+        return
+            "Il " + tipoLupo + " (" + nomeLupo + ") non può attaccare il " + ruoloAmato + " amato (" + nomeAmato + ").\n Avvisa l'Angelo custode "
+            + "(" + nomeAngeloCustode + ") della sua morte.";
+    }
 
 }
