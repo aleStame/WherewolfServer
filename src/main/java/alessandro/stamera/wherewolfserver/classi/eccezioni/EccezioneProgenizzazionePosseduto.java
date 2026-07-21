3f7ba8edfdf00c1819acaa6402d6ef536635c025
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-21 17:31:58 +0200

    ROSSO: testMessaggioErrore non riuscito

---
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneProgenizzazionePosseduto.java
@@ -0,0 +1,9 @@
+package alessandro.stamera.wherewolfserver.classi.eccezioni;
+
+public class EccezioneProgenizzazionePosseduto extends RuntimeException
+{
+
+    public EccezioneProgenizzazionePosseduto(String ruoloProgenizzatore, String nomeProgenizzatore, String nomePosseduto)
+    { }
+
+}
