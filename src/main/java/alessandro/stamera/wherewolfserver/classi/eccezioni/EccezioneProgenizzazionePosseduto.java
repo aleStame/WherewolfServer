405f74927b200f18fc6632c0581e60c384f5c56a
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-21 17:51:04 +0200

    REFACTORING: definizione del metodo getVerbo

--- src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneProgenizzazionePosseduto.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneProgenizzazionePosseduto.java
@@ -1,6 +1,6 @@
 package alessandro.stamera.wherewolfserver.classi.eccezioni;
 
-public class EccezioneProgenizzazionePosseduto extends RuntimeException
+public class EccezioneProgenizzazionePosseduto extends IllegalArgumentException
 {
 
     private final String ruoloProgenizzatore, nomeProgenizzatore, nomePosseduto;
@@ -14,15 +14,20 @@ public class EccezioneProgenizzazionePosseduto extends RuntimeException
 
     @Override public String getMessage()
     {
+        return
+            "Il " + ruoloProgenizzatore + " (" + nomeProgenizzatore + ") non può " + getVerbo() + " il Posseduto (" + nomePosseduto + ").\n" +
+            nomeProgenizzatore + " diventerà il Posseduto e " + nomePosseduto + " che morirà.";
+    }
+
+    private String getVerbo()
+    {
         String verbo = "";
         switch(ruoloProgenizzatore)
         {
             case "Nosferatu" -> verbo = "progenizzare";
             case "Vampiro" -> verbo = "vampirizzare";
         }
-        return
-            "Il " + ruoloProgenizzatore + " (" + nomeProgenizzatore + ") non può " + verbo + " il Posseduto (" + nomePosseduto + ").\n" +
-            nomeProgenizzatore + " diventerà il Posseduto e " + nomePosseduto + " che morirà.";
+        return verbo;
     }
 
 }
