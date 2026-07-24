a45fb88d61ca8bd0b596c960fe457cb57bd5ec52
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-24 17:18:45 +0200

    REFACTORING: definizione del metodo nosferatizzazione

--- src/main/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/Partita.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/Partita.java
@@ -189,12 +189,7 @@ public final class Partita
         String nomeNosferatu = vivi.getNomeNosferatu();
         switch(attaccoNosferatu(nome))
         {
-            case RIUSCITO ->
-            {
-                boolean esito = mortiNotte.isAngeloCustode(nome);
-                risorgiGiocatore(nome);
-                if(esito) vivi.resettaAmato();
-            }
+            case RIUSCITO -> nosferatizzazione(nome);
             case MORTO -> morteNosferatu(nome);
             case GHOUL_MORTO -> gestioneMorteGhoul(nome, nomeNosferatu);
             case TROVATO_POSSEDUTO -> gestionePosseduto(nome, nomeNosferatu);
@@ -266,6 +261,13 @@ public final class Partita
             case FALLITO -> throw new IllegalStateException("Scegli un'altra persona da attaccare.");
             case RIUSCITO -> maledettiNegromante[getPosizioneLiberaMaledettiNegromante()] = nome;
         }
+    }
+
+    private void nosferatizzazione(String nome)
+    {
+        boolean esito = mortiNotte.isAngeloCustode(nome);
+        risorgiGiocatore(nome);
+        if(esito) vivi.resettaAmato();
     }
 
     private EsitoAttacco attaccoNosferatu(String nomeVittima)
