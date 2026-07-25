790ce7cef00df01894809510ea8e926060cd9574
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-25 15:13:22 +0200

    ROSSO: testMessaggio non riuscito

---
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneCappuccettoRosso.java
@@ -0,0 +1,7 @@
+package alessandro.stamera.wherewolfserver.classi.eccezioni;
+
+public class EccezioneCappuccettoRosso extends RuntimeException {
+    public EccezioneCappuccettoRosso(String message) {
+        super(message);
+    }
+}
