f2f9e98e6988154094d61f6bc6d36bd86e6d1f4c
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-07-31 02:09:49 +0200

    VERDE: testProgenieNosferatu riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/GiocatoriMortiNotte.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/GiocatoriMortiNotte.java
@@ -6,8 +6,7 @@ import alessandro.stamera.wherewolfserver.classi.attributi_ruolo.TipoContadino;
 public final class GiocatoriMortiNotte extends Giocatori
 {
 
-    public EsitoAttacco progenizzazioneNosferatu(String nome) { //return getRuolo(nome).attaccoNosferatu();
-        return null; }
+    public EsitoAttacco progenizzazioneNosferatu(String nome) { return getGiocatore(nome).progenizzazioneNosferatu(); }
 
     public boolean isLupo(String nome) { return getRuolo(nome).isLupo(); }
 
