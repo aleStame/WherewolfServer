9a747ae57d1aac4d2fae2a608820f181fc3321a9
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-08-03 00:05:31 +0200

    VERDE DIRETTO: testNonBracconiere riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestGiocatore.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestGiocatore.java
@@ -159,6 +159,23 @@ public final class TestGiocatore
         verificaVero(giocatore.isBracconiere());
     }
 
+    @ParameterizedTest @CsvSource
+    (
+        {
+            "Altra guardia", "Angelo custode", "Assassino", "Azzeccagarbugli", "Bardo", "Becchino", "Bocca di rosa", "Boia", "Borgomastro",
+            "Cacciatore", "Cacciatore di vampiri", "Capo branco", "Capo gilda", "Cappuccetto rosso", "Contadino eroe",
+            "Contadino discendente dei lupi", "Contadino mostro", "Contadino normale", "Eremita", "Ghoul", "Giovane lupo", "Giulietta", "Giullare",
+            "Goblin", "Guardia", "Guardia corrotta", "Guaritore", "Inquisitore", "Ladra", "Leprecauno", "Lupo del branco", "Lupo reietto",
+            "Lupo solitario", "Mago", "Medium", "Megera", "Mercante", "Monaco", "Negromante", "Nonna", "Nosferatu", "Oratore", "Oste", "Pazzo",
+            "Peccatore", "Posseduto", "Prete", "Sidhe", "Spia", "Strega", "Sensitiva", "Templare", "Vampiro"
+        }
+    )
+    public void testNonBracconiere(String nomeLupo)
+    {
+        cambiaRuolo(nomeLupo);
+        assertThat(giocatore.isBracconiere()).isFalse();
+    }
+
     private void verificaNonInquisito() { assertThat(isInquisito()).isFalse(); }
 
     private boolean isInquisito() { return giocatore.isInquisito(); }
