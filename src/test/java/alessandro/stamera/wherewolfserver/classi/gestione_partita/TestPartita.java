ae2cca37039fd2190b59ea52ba37d03111c0c3a7
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-08-25 17:16:14 +0200

    VERDE DIRETTO: testAttaccoLupoReiettoAmatoNonna riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestPartita.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestPartita.java
@@ -1782,24 +1782,18 @@ public final class TestPartita
         verificaProtezioneCacciatore(tipoLupo, nomeCacciatore);
     }
 
-    /*
-    *
-    *
-
-    @Test public void testAttaccoLupoBrancoAmatoNonna()
+    @Test public void testAttaccoLupoReiettoAmatoNonna()
     {
-        String nomeLupo = "Ciro", tipoLupo = "Lupo del branco", nomeVittima = "Federica", nomeCacciatore = "Giorgia";
+        String nomeLupo = "Ciro", tipoLupo = "Lupo reietto", nomeVittima = "Federica", nomeCacciatore = "Lucia";
         inizializzaPartita(new String[][] { { nomeLupo, tipoLupo }, { nomeVittima, "Nonna" }, { nomeCacciatore, "Cacciatore" } });
         segnalazioneAngeloCustode(nomeLupo);
         String messaggio =
-            "Il Lupo del branco (Ciro) ha beccato la Nonna (Federica).\nSveglia Federica e avvisa i due giocatori che Ciro è eliminato e che " +
-            "Federica è il Lupo del branco.";
+            "Il Lupo reietto (Ciro) ha beccato la Nonna (Federica).\nSveglia Federica e avvisa i due giocatori che Ciro è eliminato e che " +
+            "Federica è il Lupo reietto.";
         verificaAttaccoNonna(tipoLupo, nomeVittima, messaggio, nomeLupo);
-        verificaLupoBranco(nomeVittima);
+        verificaVero(partita.isLupoReietto(nomeVittima));
         verificaProtezioneCacciatore(tipoLupo, nomeCacciatore);
     }
-    *
-    * */
 
     @ParameterizedTest @CsvSource
     (
