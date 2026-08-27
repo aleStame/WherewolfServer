8fb18afc98e7380ad7e07b0090d7ede8dab435ab
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-08-27 15:21:49 +0200

    VERDE: testGildataFallita riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneGildata.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneGildata.java
@@ -3,7 +3,8 @@ package alessandro.stamera.wherewolfserver.classi.eccezioni;
 public final class EccezioneGildata extends RuntimeException
 {
 
-    public EccezioneGildata(String message) {
-        super(message);
+    public EccezioneGildata(String nomeVittima) {
+        super("Impossibile criminalizzare " + nomeVittima + ".");
     }
+
 }
