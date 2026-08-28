88b3f5a55edf46b305df96d821af7ee26dd77b6e
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-08-28 23:34:04 +0200

    REFACTORING: definizione del metodo verificaMessaggioErrore

--- src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneCacciatore.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneCacciatore.java
@@ -6,16 +6,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class TestEccezioneCacciatore
 {
 
+    private EccezioneCacciatore eccezione;
+
     @Test public void testMessaggioCacciatore()
     {
-        String messaggio = "Milo è il Cacciatore ed è protetto dall'attacco del lupo ex Nonna.\nAvvisa i lupi dell'attacco fallito.";
-        assertThat(new EccezioneCacciatore("Milo").getMessage()).isEqualTo(messaggio);
+        eccezione = new EccezioneCacciatore("Milo");
+        verificaMessaggioErrore("Milo è il Cacciatore ed è protetto dall'attacco del lupo ex Nonna.\nAvvisa i lupi dell'attacco fallito.");
     }
 
     @Test public void testMessaggioCacciatoreLupoNonna()
     {
-        String messaggio = "Milo è l'ultimo lupo rimasto in gioco.\nAvvisalo dell'attacco fallito al Cacciatore (Lucio).";
-        assertThat(new EccezioneCacciatore("Milo", "Lucio").getMessage()).isEqualTo(messaggio);
+        eccezione = new EccezioneCacciatore("Milo", "Lucio");
+        verificaMessaggioErrore("Milo è l'ultimo lupo rimasto in gioco.\nAvvisalo dell'attacco fallito al Cacciatore (Lucio).");
     }
+
+    private void verificaMessaggioErrore(String messaggio) { assertThat(eccezione.getMessage()).isEqualTo(messaggio); }
 
 }
