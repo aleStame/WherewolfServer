246c2da872375f6bdc4ba11e691e3490f889f520
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-08-28 12:46:22 +0200

    VERDE: testEliminaProtezione riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/attributi_ruolo/Tratti.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/attributi_ruolo/Tratti.java
@@ -71,7 +71,7 @@ public final class Tratti
 
     public void eliminaTratti(Tratto... tratti) { this.tratti.removeAll(stream(tratti).toList()); }
 
-    public void perdiProtezione(Ruolo ruolo) { }
+    public void perdiProtezione(Ruolo ruolo) { protezioni.perdiProtezione(ruolo); }
 
     private void proteggi() { if(!tratti.contains(PROTETTO)) aggiungi(PROTETTO); }
 
