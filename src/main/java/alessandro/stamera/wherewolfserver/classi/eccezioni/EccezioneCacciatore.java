e7375802ee4a2cf001cb73325a6769d40ea469ba
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-08-28 22:41:32 +0200

    VERDE: testMessaggioCacciatore riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneCacciatore.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneCacciatore.java
@@ -3,8 +3,9 @@ package alessandro.stamera.wherewolfserver.classi.eccezioni;
 public final class EccezioneCacciatore extends IllegalStateException
 {
 
-    public EccezioneCacciatore(String message) {
-        super(message);
+    public EccezioneCacciatore(String nomeCacciatore)
+    {
+        super(nomeCacciatore + " è il Cacciatore ed è protetto dall'attacco del lupo ex Nonna.\nAvvisa i lupi dell'attacco fallito.");
     }
 
 }
