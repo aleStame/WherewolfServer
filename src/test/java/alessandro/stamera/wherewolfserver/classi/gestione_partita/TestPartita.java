977d81fc197c84a89a909731a4f9640552c2028f
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-08-29 17:08:57 +0200

    VERDE DIRETTO: testAttaccoStregatoAssassino riuscito

--- src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestPartita.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/gestione_partita/TestPartita.java
@@ -184,6 +184,27 @@ public final class TestPartita
         verificaAttaccoAssassinoAmato(nomeVittima, nomeAssassino, nomeAngelo);
     }
 
+    @ParameterizedTest @CsvSource
+    (
+        {
+            "Altra guardia", "Angelo custode", "Azzeccagarbugli", "Bardo", "Becchino", "Bocca di rosa", "Boia", "Borgomastro", "Bracconiere",
+            "Cacciatore", "Cacciatore di vampiri", "Capo branco", "Capo gilda", "Cappuccetto rosso", "Contadino eroe",
+            "Contadino discendente dei lupi", "Contadino normale", "Eremita", "Ghoul", "Giovane lupo", "Giulietta", "Giullare", "Goblin",
+            "Guardia", "Guardia corrotta", "Guaritore", "Inquisitore", "Ladra", "Leprecauno", "Lupo del branco", "Lupo reietto", "Lupo solitario",
+            "Mago", "Medium", "Megera", "Mercante", "Monaco", "Negromante", "Nonna", "Nosferatu", "Oratore", "Oste", "Pazzo", "Peccatore",
+            "Posseduto", "Prete", "Sidhe", "Spia", "Sensitiva", "Templare", "Vampiro"
+        }
+    )
+    public void testAttaccoStregatoAssassino(String nomeRuolo)
+    {
+        String nomeVittima = "Maddalena";
+        inizializzaPartita(new String[][] { { "Barbara", "Assassino" }, { nomeVittima, nomeRuolo } });
+        protezioneStrega(nomeVittima);
+        attaccoAssassino(nomeVittima);
+        terminaNotte();
+        verificaEliminazione(nomeVittima);
+    }
+
     private void verificaAttaccoAssassinoAmato(String nomeVittima, String nomeAssassino, String nomeAngelo)
     {
         segnalazioneAngeloCustode(nomeVittima);
