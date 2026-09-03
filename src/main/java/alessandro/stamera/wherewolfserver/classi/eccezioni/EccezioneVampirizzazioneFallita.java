99af9388d5737209556ec6bd0d6799bcbcc6b5bd
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-09-03 11:42:21 +0200

    VERDE: testAvvisoErroreRomeo riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneVampirizzazioneFallita.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneVampirizzazioneFallita.java
@@ -3,8 +3,9 @@ package alessandro.stamera.wherewolfserver.classi.eccezioni;
 public class EccezioneVampirizzazioneFallita extends IllegalStateException
 {
 
-    public EccezioneVampirizzazioneFallita(String nomeRomeo, String nomeVampiro) {
-        super("message");
+    public EccezioneVampirizzazioneFallita(String nomeRomeo, String nomeVampiro)
+    {
+        super("Impossibile vampirizzare " + nomeRomeo + " perché Romeo.\nAvvisa il Vampiro (" + nomeVampiro + ") della mancata vampirizzazione.");
     }
 
 }
