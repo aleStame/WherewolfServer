78b2258a790f69159f299ccef45ba756449c4ba9
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-09-04 14:40:02 +0200

    VERDE: testAvvisoErrore riuscito

--- src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneVampirizzazioneFallita.java
+++ src/main/java/alessandro/stamera/wherewolfserver/classi/eccezioni/EccezioneVampirizzazioneFallita.java
@@ -1,21 +1,24 @@
 package alessandro.stamera.wherewolfserver.classi.eccezioni;
 
 import java.util.Optional;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
 
 public class EccezioneVampirizzazioneFallita extends IllegalStateException
 {
 
     private final String nomeVittima, nomeVampiro;
+
     private final Optional<String> ruoloProtettore;
 
     public EccezioneVampirizzazioneFallita(String nomeVittima, String nomeVampiro)
     {
-        this(nomeVittima, nomeVampiro, Optional.empty());
+        this(nomeVittima, nomeVampiro, empty());
     }
 
     public EccezioneVampirizzazioneFallita(String nomeVittima, String nomeVampiro, String ruoloProtettore)
     {
-        this(nomeVittima, nomeVampiro, Optional.of(ruoloProtettore));
+        this(nomeVittima, nomeVampiro, of(ruoloProtettore));
     }
 
     private EccezioneVampirizzazioneFallita(String nomeVittima, String nomeVampiro, Optional<String> ruoloProtettore)
@@ -28,8 +31,16 @@ public class EccezioneVampirizzazioneFallita extends IllegalStateException
     @Override public String getMessage()
     {
         return
-            "Impossibile vampirizzare " + nomeVittima + " perché .\nAvvisa il Vampiro (" + nomeVampiro +
+            "Impossibile vampirizzare " + nomeVittima + " " + getFraseMotivazione() + ".\nAvvisa il Vampiro (" + nomeVampiro +
             ") della mancata vampirizzazione.";
+    }
+
+    private String getFraseMotivazione()
+    {
+        String risultato = "perché ";
+        if(ruoloProtettore.isPresent()) risultato += getFraseProtettore(ruoloProtettore.get());
+        else risultato += "protetto dall'attacco del Vampiro";
+        return risultato;
     }
 
     private String getFraseProtettore(String protettore)
