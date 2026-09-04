c2173f5206ff35830c87ec9cd58f19f0d2a87c60
Alessandro Stamera <alessandro.stamera@studenti.unimi.it>
2026-09-04 14:21:36 +0200

    REFACTORING: definizione del metodo verificaMessaggioErrore

--- src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneVampirizzazioneFallita.java
+++ src/test/java/alessandro/stamera/wherewolfserver/classi/eccezioni/TestEccezioneVampirizzazioneFallita.java
@@ -6,17 +6,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class TestEccezioneVampirizzazioneFallita
 {
 
+    private EccezioneVampirizzazioneFallita eccezione;
+
     @Test public void testAvvisoErroreRomeo()
     {
-        String messaggio = "Impossibile vampirizzare Mario perché Romeo.\nAvvisa il Vampiro (Lorenzo) della mancata vampirizzazione.";
-        assertThat(new EccezioneVampirizzazioneFallita("Mario", "Lorenzo", "Giulietta").getMessage()).isEqualTo(messaggio);
+        eccezione = new EccezioneVampirizzazioneFallita("Mario", "Lorenzo", "Giulietta");
+        verificaMessaggioErrore("Impossibile vampirizzare Mario perché Romeo.\nAvvisa il Vampiro (Lorenzo) della mancata vampirizzazione.");
     }
 
     @Test public void testAvvisoErroreStrega()
     {
+        eccezione = new EccezioneVampirizzazioneFallita("Mario", "Lorenzo", "Strega");
         String messaggio =
             "Impossibile vampirizzare Mario perché protetto dalla Strega.\nAvvisa il Vampiro (Lorenzo) della mancata vampirizzazione.";
-        assertThat(new EccezioneVampirizzazioneFallita("Mario", "Lorenzo", "Strega").getMessage()).isEqualTo(messaggio);
+        verificaMessaggioErrore(messaggio);
     }
+
+    private void verificaMessaggioErrore(String messaggio) { assertThat(eccezione.getMessage()).isEqualTo(messaggio); }
 
 }
